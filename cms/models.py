6c2029c6558835796f8c58025e11351b5e2b477f
divio <divio@92ec8317-e278-4cfe-9719-05471b91e6e2>
2009-03-05 12:53:34 +0000

    cleanup
    git-svn-id: http://svn.django-cms.org/branches/django-cms2@275 92ec8317-e278-4cfe-9719-05471b91e6e2

--- cms/models.py
+++ cms/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.contrib.auth.models import User, Group
 from django.utils.translation import ugettext_lazy as _
 from django.core.urlresolvers import reverse
-from django.core.exceptions import ImproperlyConfigured
 from django.contrib.sites.models import Site
 from django.shortcuts import get_object_or_404
 
