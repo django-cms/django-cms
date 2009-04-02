feb6f3b176e2abf0b59fa8629e4ee9cd19ee32b6
Simon Meers <DrMeers@gmail.com>
2009-04-02 17:27:26 +1100

    Modified plugin_editor template to display summary of plugin content beside plugin type. Each plugin can define __unicode__ to provide a short textual content summary, which Picture, Text and Flash now do

--- cms/plugins/text/models.py
+++ cms/plugins/text/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin
 from django.conf import settings
+from django.utils.html import strip_tags
+from django.utils.text import truncate_words
 
 if 'reversion' in settings.INSTALLED_APPS:
     import reversion
@@ -10,7 +12,8 @@ if 'reversion' in settings.INSTALLED_APPS:
 class Text(CMSPlugin):
     """A block of content, tied to a page, for a particular language"""
     body = models.TextField(_("body"))
-
+    def __unicode__(self):
+        return u"%s" % strip_tags(truncate_words((self.body), 3))
 
 if 'reversion' in settings.INSTALLED_APPS:        
     reversion.register(Text, follow=["cmsplugin_ptr"])
