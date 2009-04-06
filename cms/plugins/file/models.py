2c4857f45be7ca4690188d88c9515b78a50a0dbc
divio <divio@92ec8317-e278-4cfe-9719-05471b91e6e2>
2009-04-06 07:57:07 +0000

    Added default file_icon repository in cms/media/cms/images/file_icons, and updated CMS_FILE_ICON_PATH and CMS_FILE_ICON_EXTENSIONS plugin-specific settings. Also added cms.plugins.file to example project INSTALLED_APPS
    Signed-off-by: Patrick Lauber <patrick.lauber@divio.ch>
    
    git-svn-id: http://svn.django-cms.org/branches/django-cms2@304 92ec8317-e278-4cfe-9719-05471b91e6e2

--- cms/plugins/file/models.py
+++ cms/plugins/file/models.py
@@ -5,7 +5,8 @@ from cms.models import CMSPlugin
 from os.path import join, basename, splitext, exists
 import math
 
-from cms import settings
+from cms import settings as cms_settings
+from django.conf import settings
 
 if 'reversion' in settings.INSTALLED_APPS:
     import reversion
@@ -16,25 +17,29 @@ class File(CMSPlugin):
 	
 	Default template displays download link with icon (if available) and file size.
 	
-	Icons are searched for within <CMS_MEDIA_ROOT>/<CMS_ICON_PATH> 
-	(CMS_ICON_PATH is a plugin-specific setting which defaults to "images/icons")
+	Icons are searched for within <MEDIA_ROOT>/<CMS_FILE_ICON_PATH> 
+	(CMS_FILE_ICON_PATH is a plugin-specific setting which defaults to "<CMS_MEDIA_PATH>/images/file_icons")
 	with filenames of the form <file_ext>.<icon_ext>, where <file_ext> is the extension
-	of the file itself, and <icon_ext> is one of <CMS_ICON_EXTENSIONS>
+	of the file itself, and <icon_ext> is one of <CMS_FILE_ICON_EXTENSIONS>
 	(another plugin specific setting, which defaults to ('gif', 'png'))
 	
-	The search is currently performed within get_icon_url; this is probably a performance
-	concern.
+	This could be updated to use the mimetypes library to determine the type of file rather than
+	storing a separate icon for each different extension.
+	
+	The icon search is currently performed within get_icon_url; this is probably a performance concern.
 	"""
 	file = models.FileField(_("file"), upload_to=CMSPlugin.get_media_path)
 	title = models.CharField(_("title"), max_length=255, null=True, blank=True)
-	ICON_EXTENSIONS = getattr(settings, "CMS_ICON_EXTENSIONS", ('gif', 'png'))
-	ICON_PATH = getattr(settings, "CMS_ICON_PATH", join("images", "icons"))
+	# CMS_ICON_EXTENSIONS and CMS_ICON_PATH are assumed to be plugin-specific, and not included in cms.settings
+	# -- they are therefore imported from django.conf.settings
+	ICON_EXTENSIONS = getattr(settings, "CMS_FILE_ICON_EXTENSIONS", ('gif', 'png'))
+	ICON_PATH = getattr(settings, "CMS_FILE_ICON_PATH", join(cms_settings.CMS_MEDIA_PATH, "images", "file_icons"))
 	def get_icon_url(self):
 		base = join(self.ICON_PATH, self.get_ext())
 		for ext in self.ICON_EXTENSIONS:
 			relative = "%s.%s" % (base, ext)
-			if exists(join(settings.CMS_MEDIA_ROOT, relative)): 
-				return join(settings.CMS_MEDIA_URL, relative)
+			if exists(join(settings.MEDIA_ROOT, relative)): 
+				return join(settings.MEDIA_URL, relative)
 		return None
 	def get_file_name(self):
 		return basename(self.file.path)
