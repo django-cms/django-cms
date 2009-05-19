281436591f551ba32a0fbfe2ea7d55c3462787dd
Luke Plant <L.Plant.98@cantab.net>
2009-05-20 01:28:32 +0800

    Fix bug with mutating TextForm, and make it easier to subclass TextPlugin
    Signed-off-by: Patrick Lauber <patrick.lauber@divio.ch>

--- cms/plugins/text/cms_plugins.py
+++ cms/plugins/text/cms_plugins.py
@@ -13,19 +13,35 @@ class TextPlugin(CMSPluginBase):
     name = _("Text")
     form = TextForm
     render_template = "cms/plugins/text.html"
-    
+
+    def get_editor_widget(self, request, plugins, objects):
+        """
+        Returns the Django form Widget to be used for
+        the text area
+        """
+        return WYMEditor(installed_plugins=plugins, objects=objects)
+
+    def get_form_class(self, request, plugins, objects):
+        """
+        Returns a subclass of Form to be used by this plugin
+        """
+        # We avoid mutating the Form declared above by subclassing
+        class TextPluginForm(self.form):
+            pass
+
+        widget = self.get_editor_widget(request, plugins, objects)
+        TextPluginForm.declared_fields["body"] = CharField(widget=widget, required=False)
+        return TextPluginForm
+
     def get_form(self, request, obj=None, **kwargs):
-        form = self.form
-        objects = []
+        objects = [] # not actually used, so we don't bother to calculate
         plugins = plugin_pool.get_text_enabled_plugins(self.placeholder)
-        widget = WYMEditor(installed_plugins=plugins, objects=objects)
-        form.declared_fields["body"] = CharField(widget=widget, required=False)
-        
+        form = self.get_form_class(request, plugins, objects)
         kwargs['form'] = form # override standard form
         return super(TextPlugin, self).get_form(request, obj, **kwargs)
-    
+
     def render(self, context, instance, placeholder):
         return {'body':plugin_tags_to_user_html(instance.body, context, placeholder), 
                 'placeholder':placeholder}
-    
+
 plugin_pool.register_plugin(TextPlugin)
