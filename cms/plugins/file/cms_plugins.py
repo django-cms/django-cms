20986cfb6736720b44d206d0348e5ef489fd6c10
Simon Meers <DrMeers@gmail.com>
2009-05-30 15:55:41 +0800

    Made File plugins text_enabled
    Signed-off-by: Patrick Lauber <patrick.lauber@divio.ch>

--- cms/plugins/file/cms_plugins.py
+++ cms/plugins/file/cms_plugins.py
@@ -3,13 +3,20 @@ from cms.plugin_base import CMSPluginBase
 from django.utils.translation import ugettext_lazy as _
 from models import File
 from django.conf import settings
+from cms.settings import CMS_MEDIA_URL
 
 class FilePlugin(CMSPluginBase):
     model = File
     name = _("File")
     render_template = "cms/plugins/file.html"
+    text_enabled = True
     
     def render(self, context, instance, placeholder):      
         return {'instance':instance, 'placeholder':placeholder, 'MEDIA_URL': settings.MEDIA_URL}
+
+    def icon_src(self, instance):
+        file_icon = instance.get_icon_url()
+        if file_icon: return file_icon
+        return CMS_MEDIA_URL + u"images/plugins/file.png"
     
 plugin_pool.register_plugin(FilePlugin)
