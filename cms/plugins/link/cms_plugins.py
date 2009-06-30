a056371d5629b0dcfc78893aff2c52ceb17c2ad1
pcicman <pcicman@gmail.com>
2009-06-30 15:53:57 +0200

    Bugfixing:
    - add page in link plugin is not displayed anymore

--- cms/plugins/link/cms_plugins.py
+++ cms/plugins/link/cms_plugins.py
@@ -1,15 +1,27 @@
+from django.utils.translation import ugettext_lazy as _
+from django import forms
+from models import Link
+from cms.settings import CMS_MEDIA_URL
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
-from django.utils.translation import ugettext_lazy as _
-from models import Link
-from cms.plugins.text.forms import TextForm
-from cms.settings import CMS_MEDIA_URL
+from cms.models import Page
+
+
+class LinkPluginForm(forms.ModelForm):
+    # change field, so we don't see add page icon in admin
+    page_link = forms.ModelChoiceField(Page.objects, label=_('Page'), required=False)
+    
+    class Meta:
+        model = Link
+
 
 class LinkPlugin(CMSPluginBase):
     model = Link
     name = _("Link")
     render_template = "cms/plugins/link.html"
     text_enabled = True
+    
+    form = LinkPluginForm
     
     def render(self, context, instance, placeholder):
         if instance.url:
