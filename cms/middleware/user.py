ec149d1b3ec888d7ccde8e12844baac398eac646
Patrick Lauber <patrick.lauber@divio.ch>
2009-07-06 13:14:18 +0200

    typo

--- cms/middleware/user.py
+++ cms/middleware/user.py
@@ -1,7 +1,7 @@
 """This is ugly, but seems there's no other way how to do what we need for
 permission system.
 
-This middleware is required only when CMS_PERMISSION.
+This middleware is required only when CMS_PERMISSION = True.
 """
 from cms.utils.permissions import set_current_user
 
