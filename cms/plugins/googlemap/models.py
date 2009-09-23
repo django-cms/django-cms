b1b44ddfd4cbffa84273c434175a9c4b19e3768b
pcicman <pcicman@gmail.com>
2009-09-23 21:09:22 +0800

    Fine tune latitude & longitude for googlemap plugin
    Signed-off-by: Patrick Lauber <patrick.lauber@divio.ch>

--- cms/plugins/googlemap/models.py
+++ cms/plugins/googlemap/models.py
@@ -15,6 +15,10 @@ class GoogleMap(CMSPlugin):
     content = models.CharField(_("additional content"), max_length=255, blank=True, null=True)
     zoom = models.IntegerField(_("zoom level"), blank=True, null=True)
     
+    lat = models.DecimalField(_('latitude'), max_digits=10, decimal_places=6, null=True, blank=True, 
+        help_text=_('Use latitude & longitude to fine tune the map possiton.'))
+    lng = models.DecimalField(_('longitude'), max_digits=10, decimal_places=6, null=True, blank=True)
+    
     def __unicode__(self):
         return u"%s (%s, %s %s)" % (self.get_title(), self.address, self.zipcode, self.city,)
     
@@ -33,3 +37,6 @@ class GoogleMap(CMSPlugin):
             return 13
         return self.zoom
     
+    def get_lat_lng(self):
+        if self.lat and self.lng:
+            return [self.lat, self.lng]
