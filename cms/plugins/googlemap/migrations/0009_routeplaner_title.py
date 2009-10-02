6d147ebee53f627d242c93e796dd3f6c6353d5cc
Patrick Lauber <patrick.lauber@divio.ch>
2009-10-02 17:47:22 +0200

    google maps plugin has now a route_planer too.

--- cms/plugins/googlemap/migrations/0009_routeplaner_title.py
+++ cms/plugins/googlemap/migrations/0009_routeplaner_title.py
@@ -7,23 +7,18 @@ class Migration:
     
     def forwards(self, orm):
         
-        # Adding field 'GoogleMap.lng'
-        db.add_column('cmsplugin_googlemap', 'lng', orm['googlemap.googlemap:lng'])
+        # Adding field 'GoogleMap.route_planer_title'
+        db.add_column('cmsplugin_googlemap', 'route_planer_title', orm['googlemap.googlemap:route_planer_title'])
         
-        # Adding field 'GoogleMap.lat'
-        db.add_column('cmsplugin_googlemap', 'lat', orm['googlemap.googlemap:lat'])
-        
-        
+    
     
     def backwards(self, orm):
         
-        # Deleting field 'GoogleMap.lng'
-        db.delete_column('cmsplugin_googlemap', 'lng')
+        # Deleting field 'GoogleMap.route_planer_title'
+        db.delete_column('cmsplugin_googlemap', 'route_planer_title')
         
-        # Deleting field 'GoogleMap.lat'
-        db.delete_column('cmsplugin_googlemap', 'lat')
-        
-        
+    
+    
     models = {
         'cms.cmsplugin': {
             'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
@@ -41,21 +36,6 @@ class Migration:
             'publisher_state': ('django.db.models.fields.SmallIntegerField', [], {'default': '0', 'db_index': 'True'}),
             'rght': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'}),
             'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
-        },
-        'cms.cmspluginpublic': {
-            'creation_date': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 7, 14, 7, 37, 26, 118649)'}),
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'language': ('models.CharField', [], {'max_length': '5', 'db_index': 'True'}),
-            'level': ('models.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('models.PositiveIntegerField', [], {'db_index': 'True'}),
-            'mark_delete': ('models.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'page': ('models.ForeignKey', [], {'to': "orm['cms.PagePublic']"}),
-            'parent': ('models.ForeignKey', [], {'to': "orm['cms.CMSPluginPublic']", 'null': 'True', 'blank': 'True'}),
-            'placeholder': ('models.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'plugin_type': ('models.CharField', [], {'max_length': '50', 'db_index': 'True'}),
-            'position': ('models.PositiveSmallIntegerField', [], {'null': 'True', 'blank': 'True'}),
-            'rght': ('models.PositiveIntegerField', [], {'db_index': 'True'}),
-            'tree_id': ('models.PositiveIntegerField', [], {'db_index': 'True'})
         },
         'cms.page': {
             'changed_by': ('django.db.models.fields.CharField', [], {'max_length': '70'}),
@@ -83,29 +63,6 @@ class Migration:
             'template': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'tree_id': ('django.db.models.fields.PositiveIntegerField', [], {'db_index': 'True'})
         },
-        'cms.pagepublic': {
-            'changed_by': ('models.CharField', [], {'max_length': '70'}),
-            'created_by': ('models.CharField', [], {'max_length': '70'}),
-            'creation_date': ('models.DateTimeField', [], {'default': 'datetime.datetime(2009, 7, 14, 7, 37, 26, 379713)'}),
-            'id': ('models.AutoField', [], {'primary_key': 'True'}),
-            'in_navigation': ('models.BooleanField', [], {'default': 'True', 'db_index': 'True', 'blank': 'True'}),
-            'level': ('models.PositiveIntegerField', [], {'db_index': 'True'}),
-            'lft': ('models.PositiveIntegerField', [], {'db_index': 'True'}),
-            'login_required': ('models.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'mark_delete': ('models.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'moderator_state': ('models.SmallIntegerField', [], {'default': '1', 'blank': 'True'}),
-            'navigation_extenders': ('models.CharField', [], {'db_index': 'True', 'max_length': '80', 'null': 'True', 'blank': 'True'}),
-            'parent': ('models.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': "orm['cms.PagePublic']"}),
-            'publication_date': ('models.DateTimeField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
-            'publication_end_date': ('models.DateTimeField', [], {'db_index': 'True', 'null': 'True', 'blank': 'True'}),
-            'published': ('models.BooleanField', [], {'default': 'False', 'blank': 'True'}),
-            'reverse_id': ('models.CharField', [], {'db_index': 'True', 'max_length': '40', 'null': 'True', 'blank': 'True'}),
-            'rght': ('models.PositiveIntegerField', [], {'db_index': 'True'}),
-            'site': ('models.ForeignKey', [], {'to': "orm['sites.Site']"}),
-            'soft_root': ('models.BooleanField', [], {'default': 'False', 'db_index': 'True', 'blank': 'True'}),
-            'template': ('models.CharField', [], {'max_length': '100'}),
-            'tree_id': ('models.PositiveIntegerField', [], {'db_index': 'True'})
-        },
         'googlemap.googlemap': {
             'Meta': {'db_table': "'cmsplugin_googlemap'"},
             'address': ('django.db.models.fields.CharField', [], {'max_length': '150'}),
@@ -114,6 +71,8 @@ class Migration:
             'content': ('django.db.models.fields.CharField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
             'lat': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '6', 'blank': 'True'}),
             'lng': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '10', 'decimal_places': '6', 'blank': 'True'}),
+            'route_planer': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
+            'route_planer_title': ('django.db.models.fields.CharField', [], {'default': "u'Calculate your fastest way to here'", 'max_length': '150', 'null': 'True', 'blank': 'True'}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'zipcode': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
             'zoom': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'})
