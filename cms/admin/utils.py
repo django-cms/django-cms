b03b2384cdb85e57e32f2a08aac3639664ca7165
Patrick Lauber <patrick.lauber@divio.ch>
2009-10-05 15:49:02 +0200

    fixes #180

--- cms/admin/utils.py
+++ cms/admin/utils.py
@@ -1,10 +1,8 @@
-from django.template import loader, Context, TemplateDoesNotExist
-from django.template.loader_tags import ExtendsNode
+from django.template import loader, TemplateDoesNotExist
 from cms.utils import get_template_from_request
+from django.template.context import RequestContext
+from django.contrib.auth.models import AnonymousUser
 import re
-
-# must be imported like this for isinstance
-from django.templatetags.cms_tags import PlaceholderNode #do not remove
 
 def get_placeholders(request, template_name):
     """
@@ -14,16 +12,16 @@ def get_placeholders(request, template_name):
         temp = loader.get_template(template_name)
     except TemplateDoesNotExist:
         return []
-    context = Context()#RequestContext(request)#details(request, no404=True, only_context=True)
+    user = request.user
+    request.user = AnonymousUser()
+    context = RequestContext(request)#details(request, no404=True, only_context=True)
     template = get_template_from_request(request)
     request.current_page = "dummy"
+    
     context.update({'template':template,
                     'request':request,
                     'display_placeholder_names_only': True,
                     })
-    # lacks - it requests context in admin and eats user messages,
-    # standard context will be hopefully enough here
-    
-    # temp.render(RequestContext(request, context))
     output = temp.render(context)
+    request.user = user
     return re.findall("<!-- PlaceholderNode: (.+?) -->", output)
