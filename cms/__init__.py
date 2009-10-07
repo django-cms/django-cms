407a6709516da5b41f4aeef43c8ddc6306eeb21e
Patrick Lauber <patrick.lauber@divio.ch>
2009-10-08 00:03:18 +0200

    merge

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,28 +1,5 @@
-from django.core.exceptions import ImproperlyConfigured
+
 VERSION = (2, 0, 0, 'RC1')
 __version__ = '.'.join(map(str, VERSION))
 
-import signals
-import plugin_pool
 
-from django.conf import settings as d_settings
-plugin_pool.plugin_pool.discover_plugins()
-
-def validate_settings():
-    if not "django.core.context_processors.request" in d_settings.TEMPLATE_CONTEXT_PROCESSORS:
-        raise ImproperlyConfigured('django-cms needs django.core.context_processors.request in settings.TEMPLATE_CONTEXT_PROCESSORS to work correctly.')
-    if not 'mptt' in d_settings.INSTALLED_APPS:
-        raise ImproperlyConfigured('django-cms needs django-mptt installed.')
-    
-validate_settings()
-
-
-def validate_dependencies():
-    # check for right version of reversions
-    if 'reversion' in d_settings.INSTALLED_APPS:
-        from reversion.admin import VersionAdmin
-        if not hasattr(VersionAdmin, 'get_urls'):
-            raise ImproperlyConfigured('django-cms requires never version of reversion (VersionAdmin must contain get_urls method)')
-
-validate_dependencies()
-    
