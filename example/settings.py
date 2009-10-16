6fbf143afbf4d7880d8d9a0dbb960438302446eb
Patrick Lauber <patrick.lauber@divio.ch>
2009-10-16 17:47:30 +0200

    coverage.py support added

--- example/settings.py
+++ example/settings.py
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     'south',
     # sample application
     'example.sampleapp',
+    'test_utils',
     #'store',
 )
 
@@ -188,3 +189,76 @@ try:
     from local_settings import *
 except ImportError:
     pass
+
+try:
+    import coverage
+    TEST_RUNNER='cms.tests.test_runner_with_coverage'
+    COVERAGE_MODULES = [
+        'cms',
+        'cms.admin', 
+        'cms.admin.change_list',
+        'cms.admin.forms',
+        'cms.admin.models',
+        'cms.admin.permissionadmin',
+        'cms.admin.useradmin',
+        'cms.admin.utils',
+        'cms.admin.views',
+        'cms.admin.widgets',
+        'cms.admin.dialog',
+        'cms.admin.dialog.forms',
+        'cms.admin.dialog.utils',
+        'cms.admin.dialog.views',
+        'cms.cache',
+        'cms.cache.permissions',
+        'cms.cache.signals',
+        'cms.conf.global_settings',
+        'cms.conf.patch',
+        'cms.management.commands.publisher_publish',
+        'cms.middleware.multilingual',
+        'cms.middleware.page',
+        'cms.middleware.user',
+        'cms.migrations',
+        'cms.models', 
+        'cms.models.managers',
+        'cms.models.moderatormodels',
+        'cms.models.pagemodel',
+        'cms.models.permissionmodels',
+        'cms.models.pluginmodel',
+        'cms.models.query',
+        'cms.models.signals',
+        'cms.models.titlemodels',
+        'cms.sitemaps.cms_sitemap',
+        'cms.templatetags.cms_admin',
+        'cms.templatetags.cms_tags',
+        'cms.templatetags.js',
+        'cms.templatetags.mlurl',
+        'cms.utils',
+        'cms.utils.admin',
+        'cms.utils.helpers',
+        'cms.utils.i18n',
+        'cms.utils.mail',
+        'cms.utils.moderator',
+        'cms.utils.navigation',
+        'cms.utils.page',
+        'cms.utils.permissions',
+        'cms.utils.urlutils',
+        'cms.appresolver',
+        'cms.context_processors',
+        'cms.plugin_base',
+        'cms.plugin_pool',
+        'cms.signals',
+        'cms.urls',
+        'cms.views',
+        'publisher',
+        'publisher.base',
+        'publisher.errors',
+        'publisher.manager',
+        'publisher.models',
+        'publisher.mptt_support',
+        'publisher.options',
+        'publisher.query',
+        ]
+except:
+    pass
+
+
