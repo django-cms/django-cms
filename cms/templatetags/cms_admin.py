f0b5afdf36c94aea41d0b359ede3aba348766936
Oyvind Saltvik <oyvind.saltvik@gmail.com>
2009-10-21 17:58:34 +0200

    added templatetag/templates/css for language deletion link

--- cms/templatetags/cms_admin.py
+++ cms/templatetags/cms_admin.py
@@ -85,3 +85,26 @@ def render_plugin(context, plugin):
     return {'content': plugin.render_plugin(context, admin=True)}
 
 render_plugin = register.inclusion_tag('cms/content.html', takes_context=True)(render_plugin)
+
+def page_submit_row(context):
+    opts = context['opts']
+    change = context['change']
+    is_popup = context['is_popup']
+    save_as = context['save_as']
+    show_language_tabs = context['show_language_tabs']
+    language = context['language']
+    return {
+        'onclick_attrib': (opts.get_ordered_objects() and change
+                            and 'onclick="submitOrderForm();"' or ''),
+        'show_delete_link': (not is_popup and context['has_delete_permission']
+                              and (change or context['show_delete'])),
+        'show_save_as_new': not is_popup and change and save_as,
+        'show_save_and_add_another': context['has_add_permission'] and 
+                            not is_popup and (not save_as or context['add']),
+        'show_save_and_continue': not is_popup and context['has_change_permission'],
+        'is_popup': is_popup,
+        'show_save': True,
+        'language': language,
+        'show_language_tabs': show_language_tabs
+    }
+page_submit_row = register.inclusion_tag('admin/page_submit_line.html', takes_context=True)(page_submit_row)
