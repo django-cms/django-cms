4938e7ebe217fd6be6fc4d645b8cff6a93c40a82
Oyvind Saltvik <oyvind.saltvik@gmail.com>
2009-11-02 22:48:44 +0100

    take language into account for media

--- cms/plugins/utils.py
+++ cms/plugins/utils.py
@@ -1,24 +1,27 @@
 import operator
 from django.forms.widgets import Media
+from cms.utils import get_language_from_request
+from cms.utils.moderator import get_cmsplugin_queryset
 
-def get_plugins(request, obj):
-    if not hasattr(obj, '_plugins_cache'):
-        from cms.utils.moderator import get_cmsplugin_queryset
-        from cms.utils import get_language_from_request
-        l = get_language_from_request(request)
-        obj._plugins_cache = get_cmsplugin_queryset(request).filter(page=obj, language=l, parent__isnull=True).order_by('placeholder', 'position').select_related()
-    return obj._plugins_cache
+def get_plugins(request, obj, lang=None):
+    lang = lang or get_language_from_request(request)
+    if not hasattr(obj, '_%s_plugins_cache' % lang):
+        setattr(obj, '_%s_plugins_cache' % lang,  get_cmsplugin_queryset(request).filter(
+            page=obj, language=lang, parent__isnull=True
+        ).order_by('placeholder', 'position').select_related() )
+    return getattr(obj, '_%s_plugins_cache' % lang)
 
 def get_plugin_media(request, plugin):
     instance, plugin = plugin.get_plugin_instance()
     return plugin.get_plugin_media(request, instance)
 
 def get_plugins_media(request, obj):
-    if not hasattr(obj, '_plugins_media_cache'):
-        plugins = get_plugins(request, obj)
+    lang = get_language_from_request(request)
+    if not hasattr(obj, '_%s_plugins_media_cache' % lang):
+        plugins = get_plugins(request, obj, lang=lang)
         media_classes = [get_plugin_media(request, plugin) for plugin in plugins]
         if media_classes:
-        	obj._plugins_media_cache = reduce(operator.add, media_classes)
+            setattr(obj, '_%s_plugins_media_cache' % lang, reduce(operator.add, media_classes))
         else:
-        	obj._plugins_media_cache = Media()
-    return obj._plugins_media_cache
+             setattr(obj, '_%s_plugins_media_cache' % lang,  Media())
+    return getattr(obj, '_%s_plugins_media_cache' % lang)
