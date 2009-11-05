3247263c3bc5b1457e7e67e348c1c7a7d01df5d8
Mark Rogers <f4nt@f4ntasmic.com>
2009-11-05 15:26:47 -0600

    fix for Issue #233 (http://github.com/digi604/django-cms-2.0/issues/#issue/233)

--- cms/__init__.py
+++ cms/__init__.py
@@ -3,5 +3,14 @@ __version__ = '.'.join(map(str, VERSION))
 
 # patch settings 
 from conf import patch_settings
-patch_settings()
+try:
+    from django.conf import settings
+    patch_settings()
+except ImportError:
+    """
+    This exception means that either the application is being built, or is
+    otherwise installed improperly. Both make running patch_settings
+    irrelevant.
+    """
+    pass
 
