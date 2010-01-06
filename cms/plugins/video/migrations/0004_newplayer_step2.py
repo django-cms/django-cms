dadbc0230c9ff2b5d574e5982fcdac44c2fb5e55
Benjamin <bw@piquadrat.ch>
2010-01-06 19:08:51 +0800

    make newplayer migration work with other databases than mysql

--- cms/plugins/video/migrations/0004_newplayer_step2.py
+++ cms/plugins/video/migrations/0004_newplayer_step2.py
@@ -1,164 +1,29 @@
-
 from south.db import db
 from django.db import models
 from cms.plugins.video.models import *
 
 class Migration:
-    
+    no_dry_run = True
+
     def forwards(self, orm):
-        
-        # Adding field 'Video.movie_url'
-        db.add_column('cmsplugin_video', 'movie_url', orm['video.video:movie_url'])
-        
-        # Adding field 'Video.buttonhighlightcolor'
-        db.add_column('cmsplugin_video', 'buttonhighlightcolor', orm['video.video:buttonhighlightcolor'])
-        
-        # Adding field 'Video.auto_hide'
-        db.add_column('cmsplugin_video', 'auto_hide', orm['video.video:auto_hide'])
-        
-        # Adding field 'Video.seekbarcolor'
-        db.add_column('cmsplugin_video', 'seekbarcolor', orm['video.video:seekbarcolor'])
-        
-        # Adding field 'Video.buttonoutcolor'
-        db.add_column('cmsplugin_video', 'buttonoutcolor', orm['video.video:buttonoutcolor'])
-        
-        # Adding field 'Video.loadingbarcolor'
-        db.add_column('cmsplugin_video', 'loadingbarcolor', orm['video.video:loadingbarcolor'])
-        
-        # Adding field 'Video.seekbarbgcolor'
-        db.add_column('cmsplugin_video', 'seekbarbgcolor', orm['video.video:seekbarbgcolor'])
-        
-        # Adding field 'Video.textcolor'
-        db.add_column('cmsplugin_video', 'textcolor', orm['video.video:textcolor'])
-        
-        # Adding field 'Video.buttonovercolor'
-        db.add_column('cmsplugin_video', 'buttonovercolor', orm['video.video:buttonovercolor'])
-        
-        # Deleting field 'Video.volume'
-        db.delete_column('cmsplugin_video', 'volume')
-        
-        # Deleting field 'Video.mute'
-        db.delete_column('cmsplugin_video', 'mute')
-        
-        # Deleting field 'Video.click_target'
-        db.delete_column('cmsplugin_video', 'click_target')
-        
-        # Deleting field 'Video.flash_menu'
-        db.delete_column('cmsplugin_video', 'flash_menu')
-        
-        # Deleting field 'Video.auto_load'
-        db.delete_column('cmsplugin_video', 'auto_load')
-        
-        # Deleting field 'Video.mute_only'
-        db.delete_column('cmsplugin_video', 'mute_only')
-        
-        # Deleting field 'Video.wmode'
-        db.delete_column('cmsplugin_video', 'wmode')
-        
-        # Deleting field 'Video.controller_style'
-        db.delete_column('cmsplugin_video', 'controller_style')
-        
-        # Deleting field 'Video.fgcolor'
-        db.delete_column('cmsplugin_video', 'fgcolor')
-        
-        # Deleting field 'Video.click_url'
-        db.delete_column('cmsplugin_video', 'click_url')
-        
-        # Changing field 'Video.fullscreen'
-        # (to signature: django.db.models.fields.BooleanField(default=True, blank=True))
-        db.alter_column('cmsplugin_video', 'fullscreen', orm['video.video:fullscreen'])
-        
-        # Changing field 'Video.movie'
-        # (to signature: django.db.models.fields.files.FileField(max_length=100, null=True, blank=True))
-        db.alter_column('cmsplugin_video', 'movie', orm['video.video:movie'])
-        
-        # Changing field 'Video.height'
-        # (to signature: django.db.models.fields.PositiveSmallIntegerField())
-        db.alter_column('cmsplugin_video', 'height', orm['video.video:height'])
-        
-        # Changing field 'Video.width'
-        # (to signature: django.db.models.fields.PositiveSmallIntegerField())
-        db.alter_column('cmsplugin_video', 'width', orm['video.video:width'])
-        
-    
-    
+        for video in orm.Video.objects.all():
+            try:
+                video._i_height = int(video.height)
+            except ValueError:
+                video._i_height = 0
+            try:
+                video._i_width = int(video.width)
+            except ValueError:
+                video._i_width = 0
+            video.save()
+
+
     def backwards(self, orm):
-        
-        # Deleting field 'Video.movie_url'
-        db.delete_column('cmsplugin_video', 'movie_url')
-        
-        # Deleting field 'Video.buttonhighlightcolor'
-        db.delete_column('cmsplugin_video', 'buttonhighlightcolor')
-        
-        # Deleting field 'Video.auto_hide'
-        db.delete_column('cmsplugin_video', 'auto_hide')
-        
-        # Deleting field 'Video.seekbarcolor'
-        db.delete_column('cmsplugin_video', 'seekbarcolor')
-        
-        # Deleting field 'Video.buttonoutcolor'
-        db.delete_column('cmsplugin_video', 'buttonoutcolor')
-        
-        # Deleting field 'Video.loadingbarcolor'
-        db.delete_column('cmsplugin_video', 'loadingbarcolor')
-        
-        # Deleting field 'Video.seekbarbgcolor'
-        db.delete_column('cmsplugin_video', 'seekbarbgcolor')
-        
-        # Deleting field 'Video.textcolor'
-        db.delete_column('cmsplugin_video', 'textcolor')
-        
-        # Deleting field 'Video.buttonovercolor'
-        db.delete_column('cmsplugin_video', 'buttonovercolor')
-        
-        # Adding field 'Video.volume'
-        db.add_column('cmsplugin_video', 'volume', orm['video.video:volume'])
-        
-        # Adding field 'Video.mute'
-        db.add_column('cmsplugin_video', 'mute', orm['video.video:mute'])
-        
-        # Adding field 'Video.click_target'
-        db.add_column('cmsplugin_video', 'click_target', orm['video.video:click_target'])
-        
-        # Adding field 'Video.flash_menu'
-        db.add_column('cmsplugin_video', 'flash_menu', orm['video.video:flash_menu'])
-        
-        # Adding field 'Video.auto_load'
-        db.add_column('cmsplugin_video', 'auto_load', orm['video.video:auto_load'])
-        
-        # Adding field 'Video.mute_only'
-        db.add_column('cmsplugin_video', 'mute_only', orm['video.video:mute_only'])
-        
-        # Adding field 'Video.wmode'
-        db.add_column('cmsplugin_video', 'wmode', orm['video.video:wmode'])
-        
-        # Adding field 'Video.controller_style'
-        db.add_column('cmsplugin_video', 'controller_style', orm['video.video:controller_style'])
-        
-        # Adding field 'Video.fgcolor'
-        db.add_column('cmsplugin_video', 'fgcolor', orm['video.video:fgcolor'])
-        
-        # Adding field 'Video.click_url'
-        db.add_column('cmsplugin_video', 'click_url', orm['video.video:click_url'])
-        
-        # Changing field 'Video.fullscreen'
-        # (to signature: django.db.models.fields.BooleanField(default=False, blank=True))
-        db.alter_column('cmsplugin_video', 'fullscreen', orm['video.video:fullscreen'])
-        
-        # Changing field 'Video.movie'
-        # (to signature: django.db.models.fields.files.FileField(max_length=100))
-        db.alter_column('cmsplugin_video', 'movie', orm['video.video:movie'])
-        
-        # Changing field 'Video.height'
-        # (to signature: django.db.models.fields.CharField(max_length=6))
-        db.alter_column('cmsplugin_video', 'height', orm['video.video:height'])
-        
-        # Changing field 'Video.width'
-        # (to signature: django.db.models.fields.CharField(max_length=6))
-        db.alter_column('cmsplugin_video', 'width', orm['video.video:width'])
-        
-    
-    
+        for video in orm.Video.objects.all():
+            video.height = str(video._i_height)
+            video.width = str(video._i_width)
+            video.save()
+
     models = {
         'cms.cmsplugin': {
             'creation_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
@@ -211,6 +76,8 @@ class Migration:
         },
         'video.video': {
             'Meta': {'db_table': "'cmsplugin_video'"},
+            '_i_height': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            '_i_width': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
             'auto_hide': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
             'auto_play': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
             'bgcolor': ('django.db.models.fields.CharField', [], {'default': "'000000'", 'max_length': '6'}),
@@ -219,7 +86,7 @@ class Migration:
             'buttonovercolor': ('django.db.models.fields.CharField', [], {'default': "'000000'", 'max_length': '6'}),
             'cmsplugin_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['cms.CMSPlugin']", 'unique': 'True', 'primary_key': 'True'}),
             'fullscreen': ('django.db.models.fields.BooleanField', [], {'default': 'True', 'blank': 'True'}),
-            'height': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'height': ('django.db.models.fields.CharField', [], {'max_length': '6'}),
             'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'}),
             'loadingbarcolor': ('django.db.models.fields.CharField', [], {'default': "'828282'", 'max_length': '6'}),
             'loop': ('django.db.models.fields.BooleanField', [], {'default': 'False', 'blank': 'True'}),
@@ -228,8 +95,8 @@ class Migration:
             'seekbarbgcolor': ('django.db.models.fields.CharField', [], {'default': "'333333'", 'max_length': '6'}),
             'seekbarcolor': ('django.db.models.fields.CharField', [], {'default': "'13ABEC'", 'max_length': '6'}),
             'textcolor': ('django.db.models.fields.CharField', [], {'default': "'FFFFFF'", 'max_length': '6'}),
-            'width': ('django.db.models.fields.PositiveSmallIntegerField', [], {})
+            'width': ('django.db.models.fields.CharField', [], {'max_length': '6'})
         }
     }
-    
+
     complete_apps = ['video']
