2fd5e0056fca8663052ee2f2841da62969a2f52e
Manolis Stamatogiannakis <mstamat@gmail.com>
2010-01-29 17:22:49 +0200

    Fixed preview_link() in cms/templatetags/cms_admin.py and page_language_url() in cms/templatetags/cms_tags.py.
    page_language_url() should probably be a filter and not
    a template tag. Currently the template has no way to do
    something meaningful when no translation exists for a
    page in some language.

--- cms/templatetags/cms_admin.py
+++ cms/templatetags/cms_admin.py
@@ -78,7 +78,17 @@ def moderator_choices(page, user):
 @register.filter
 def preview_link(page, language):
     if 'cms.middleware.multilingual.MultilingualURLMiddleware' in settings.MIDDLEWARE_CLASSES:
-        return "/%s%s" % (language, page.get_absolute_url(language, fallback=True))
+        from django.core.urlresolvers import reverse
+
+        # Which one of page.get_slug() and page.get_path() is the right
+        # one to use in this block? They both seem to return the same thing.
+        try:
+            # attempt to retrieve the localized path/slug and return
+            return reverse('pages-root')+language+"/"+page.get_slug(language, fallback=False)
+        except:
+            # no localized path/slug. therefore nothing to preview. stay on the same page.
+            # perhaps the user should be somehow notified for this.
+            return ''
     return page.get_absolute_url(language)
 
 def render_plugin(context, plugin):
