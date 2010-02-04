b2d4270449864231ab46a16a49626c79ee6e5ce3
Patrick Lauber <patrick.lauber@divio.ch>
2010-02-04 19:29:52 +0100

    tests passing again

--- example/sampleapp/menu.py
+++ example/sampleapp/menu.py
@@ -1,6 +1,6 @@
 from menus.base import Menu, NavigationNode
 from example.sampleapp.models import Category
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, NoReverseMatch
 from menus.menu_pool import menu_pool
 from cms.app_base import CMSApp
 from django.utils.translation import ugettext_lazy as _
@@ -17,17 +17,19 @@ class CategoryMenu(Menu):
 menu_pool.register_menu(CategoryMenu)
 
 class StaticMenu(Menu):
-    
     def get_nodes(self, request):
         nodes = []
-        n = NavigationNode(_('sample root page'), reverse('sample-root'), 1)
-        n2 = NavigationNode(_('sample settings page'), reverse('sample-settings'), 2)
-        n3 = NavigationNode(_('sample account page'), reverse('sample-account'), 3)
-        n4 = NavigationNode(_('sample my profile page'), reverse('sample-profile'), 4, 3)
-        nodes.append(n)
-        nodes.append(n2)
-        nodes.append(n3)
-        nodes.append(n4)
+        try:
+            n = NavigationNode(_('sample root page'), reverse('sample-root'), 1)
+            n2 = NavigationNode(_('sample settings page'), reverse('sample-settings'), 2)
+            n3 = NavigationNode(_('sample account page'), reverse('sample-account'), 3)
+            n4 = NavigationNode(_('sample my profile page'), reverse('sample-profile'), 4, 3)
+            nodes.append(n)
+            nodes.append(n2)
+            nodes.append(n3)
+            nodes.append(n4)
+        except NoReverseMatch:
+            pass
         return nodes
 
 menu_pool.register_menu(StaticMenu)
