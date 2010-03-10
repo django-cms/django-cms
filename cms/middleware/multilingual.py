089998f792902c0431670dbcb29ebfebbb4f10cd
Manolis Stamatogiannakis <mstamat@gmail.com>
2010-03-10 15:07:10 +0200

    Added documentation comments for the regex used in multilingual middleware. Without documentation they are not easy to grasp quickly.
    Also reverted a bad fix for a link in the toolbar.

--- cms/middleware/multilingual.py
+++ cms/middleware/multilingual.py
@@ -75,9 +75,22 @@ class MultilingualURLMiddleware:
                 decoded_response = response.content
 
             # Customarily user pages are served from http://the.server.com/~username/
-            # When a user uses django-cms for his pages, the '~' of the url appears
-            # quoted in href links. We have to quote pages_root for the regular expression
-            # to match.
+            # When a user uses django-cms for his pages, the '~' of the url appears quoted in href links.
+            # We have to quote pages_root for the regular expression to match.
+            #
+            # The used regex is quite complex. The exact pattern depends on the used settings.
+            # The regex extracts the path of the url without the leading page root, but only matches urls
+            # that don't already contain a language string or aren't considered multilingual.
+            #
+            # Here is an annotated example pattern (_r_ is a shorthand for the value of pages_root):
+            #   pattern:        <a([^>]+)href="(?=_r_)(?!(/fr/|/de/|/en/|/pt-br/|/media/|/media/admin/))(_r_([^"]*))"([^>]*)>
+            #                     |-\1--|                |---------------------\2---------------------| |   |-\4--|| |-\5--|
+            #                                                                                           |----\3----|
+            #   input (_r_=/):  <a href="/admin/password_change/" class="foo">
+            #   matched groups: (u' ', None, u'/admin/password_change/', u'admin/password_change/', u' class="foo"')
+            #
+            # Notice that (?=...) and (?!=...) do not consume input or produce a group in the match object.
+            # If the regex matches, the extracted path we want is stored in the fourth group (\4).
             HREF_URL_FIX_RE = re.compile(ur'<a([^>]+)href="(?=%s)(?!(%s|%s|%s))(%s([^"]*))"([^>]*)>' % (
                 urllib.quote(pages_root),
                 "|".join(map(lambda l: urllib.quote(pages_root) + l[0] + "/" , settings.CMS_LANGUAGES)),
@@ -86,8 +99,9 @@ class MultilingualURLMiddleware:
                 urllib.quote(pages_root)
             ))
 
-            # Unlike in href links, the '~' (see above) the '~' in form actions appears
-            # unquoted.
+            # Unlike in href links, the '~' (see above) the '~' in form actions appears unquoted.
+            #
+            # For understanding this regex, please read the documentation for HREF_URL_FIX_RE above.
             FORM_URL_FIX_RE = re.compile(ur'<form([^>]+)action="(?=%s)(?!(%s|%s|%s))(%s([^"]*))"([^>]*)>' % (
                 pages_root,
                 "|".join(map(lambda l: pages_root + l[0] + "/" , settings.CMS_LANGUAGES)),
@@ -96,6 +110,7 @@ class MultilingualURLMiddleware:
                 pages_root
             ))
 
+            # Documentation comments for HREF_URL_FIX_RE above explain each match group (\1, \4, \5) represents.
             decoded_response = HREF_URL_FIX_RE.sub(ur'<a\1href="%s%s/\4"\5>' % (pages_root, request.LANGUAGE_CODE), decoded_response)
             response.content = FORM_URL_FIX_RE.sub(ur'<form\1action="%s%s/\4"\5>' % (pages_root, request.LANGUAGE_CODE), decoded_response).encode("utf8")
 
