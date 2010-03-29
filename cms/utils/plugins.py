532552afce01c266aae02a70a7811bd7312739fb
ojii <jonas.obrist@divio.ch>
2010-03-29 10:01:57 +0200

    removed old code

--- cms/utils/plugins.py
+++ cms/utils/plugins.py
@@ -18,35 +18,6 @@ def get_placeholders(template):
             placeholders.append(bits[1].strip('"\''))
     return placeholders
 
-'''
-def get_placeholders(request, template=None):
-    """
-    Return a list of PlaceholderNode found in the given template
-    """
-    if not template:
-        template = get_template_from_request(request)
-    try:
-        temp = loader.get_template(template)
-    except TemplateDoesNotExist:
-        return []
-    user = request.user
-    request.user = AnonymousUser()
-    context = RequestContext(request)#details(request, no404=True, only_context=True)
-    template = get_template_from_request(request)
-    old_page = request.current_page
-    request.current_page = "dummy"
-    
-    context.update({'template':template,
-                    'request':request,
-                    'display_placeholder_names_only': True,
-                    'current_page': "dummy",
-                    })
-    output = temp.render(context)
-    request.user = user
-    placeholders = re.findall("<!-- PlaceholderNode: (.+?) -->", output)
-    request.current_page = old_page
-    return placeholders
-'''
 
 SITE_VAR = "site__exact"
 
