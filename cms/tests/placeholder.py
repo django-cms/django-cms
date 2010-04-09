805ae1461fb4c750258d5f21cd912562a3f553a0
ojii <jonas.obrist@divio.ch>
2010-04-09 15:41:22 +0200

    fixed nested blocks breaking get_placeholders

--- cms/tests/placeholder.py
+++ cms/tests/placeholder.py
@@ -18,10 +18,14 @@ class PlaceholderTestCase(CMSTestCase):
         placeholders = get_placeholders('placeholder_tests/test_three.html')
         self.assertEqual(sorted(placeholders), sorted([u'new_one', u'two', u'new_three']))
         
-    def test_03_placeholder_scanning_complex(self):
+    def test_04_placeholder_scanning_complex(self):
         placeholders = get_placeholders('placeholder_tests/test_four.html')
         self.assertEqual(sorted(placeholders), sorted([u'new_one', u'child', u'four']))
         
-    def test_03_placeholder_scanning_super(self):
+    def test_05_placeholder_scanning_super(self):
         placeholders = get_placeholders('placeholder_tests/test_five.html')
         self.assertEqual(sorted(placeholders), sorted([u'one', u'extra_one', u'two', u'three']))
+        
+    def test_06_placeholder_scanning_nested(self):
+        placeholders = get_placeholders('placeholder_tests/test_six.html')
+        self.assertEqual(sorted(placeholders), sorted([u'new_one', u'new_two', u'new_three']))
--- cms/utils/plugins.py
+++ cms/utils/plugins.py
@@ -38,10 +38,10 @@ def _extend_nodelist(extend_node):
     _extend_blocks(extend_node, blocks)
     placeholders = []
     for block in blocks.values():
-        placeholders += _scan_placeholders(block.nodelist, block)
+        placeholders += _scan_placeholders(block.nodelist, block, blocks.keys())
     return placeholders
 
-def _scan_placeholders(nodelist, current_block=None):
+def _scan_placeholders(nodelist, current_block=None, ignore_blocks=[]):
     placeholders = []
 
     for node in nodelist:
@@ -59,6 +59,9 @@ def _scan_placeholders(nodelist, current_block=None):
         elif isinstance(node, VariableNode) and current_block:
             if node.filter_expression.token == 'block.super':
                 placeholders += _scan_placeholders(current_block.super.nodelist, current_block.super)
+        # ignore nested blocks which are already handled
+        elif isinstance(node, BlockNode) and node.name in ignore_blocks:
+            continue
         # if the node has the newly introduced 'child_nodelists' attribute, scan
         # those attributes for nodelists and recurse them
         elif hasattr(node, 'child_nodelists'):
