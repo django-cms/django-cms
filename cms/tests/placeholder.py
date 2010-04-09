041267198649ad1e7478984b37f307e539984420
ojii <jonas.obrist@divio.ch>
2010-04-09 11:49:03 +0200

    refactored get_placeholders and added tests for it

---
+++ cms/tests/placeholder.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+from django.conf import settings
+from cms.tests.base import CMSTestCase
+from cms.utils.plugins import get_placeholders
+
+
+class PlaceholderTestCase(CMSTestCase):
+        
+    def test_01_placeholder_scanning_extend(self):
+        placeholders = get_placeholders('placeholder_tests/test_one.html')
+        self.assertEqual(sorted(placeholders), sorted([u'new_one', u'two', u'three']))
+        
+    def test_02_placeholder_scanning_include(self):
+        placeholders = get_placeholders('placeholder_tests/test_two.html')
+        self.assertEqual(sorted(placeholders), sorted([u'child', u'three']))
+        
+    def test_03_placeholder_scanning_double_extend(self):
+        placeholders = get_placeholders('placeholder_tests/test_three.html')
+        self.assertEqual(sorted(placeholders), sorted([u'new_one', u'two', u'new_three']))
+        
+    def test_03_placeholder_scanning_complex(self):
+        placeholders = get_placeholders('placeholder_tests/test_four.html')
+        self.assertEqual(sorted(placeholders), sorted([u'new_one', u'child', u'four']))
+        
+    def test_03_placeholder_scanning_super(self):
+        placeholders = get_placeholders('placeholder_tests/test_five.html')
+        self.assertEqual(sorted(placeholders), sorted([u'one', u'extra_one', u'two', u'three']))
