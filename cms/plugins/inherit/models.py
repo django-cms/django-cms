d4da72ce57298311d6e6a7455e1e709cfc1e1e17
Jannis Leidel <jannis@leidel.info>
2010-04-12 10:00:55 +0200

    Added a page property to the plugin base model for easy access in the cms plugin forms.

--- cms/plugins/inherit/models.py
+++ cms/plugins/inherit/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin, Page
-from cms.utils.placeholder import get_page_from_placeholder_if_exists
 from cms import settings
 
 class InheritPagePlaceholder(CMSPlugin):
@@ -10,7 +9,3 @@ class InheritPagePlaceholder(CMSPlugin):
     """
     from_page = models.ForeignKey(Page, null=True, blank=True, help_text=_("Choose a page to include its plugins into this placeholder, empty will choose current page"))
     from_language = models.CharField(_("language"), max_length=5, choices=settings.CMS_LANGUAGES, blank=True, null=True, help_text=_("Optional: the language of the plugins you want"))
-
-    @property
-    def page(self):
-        return get_page_from_placeholder_if_exists(self.placeholder)
