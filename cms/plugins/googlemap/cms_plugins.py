d6ddc84ff2873275b9a745a42f852b3b6f1d40fe
ojii <jonas.obrist@divio.ch>
2010-04-13 14:03:37 +0200

    improved google map js loading

--- cms/plugins/googlemap/cms_plugins.py
+++ cms/plugins/googlemap/cms_plugins.py
@@ -12,18 +12,17 @@ class GoogleMapPlugin(CMSPluginBase):
     render_template = "cms/plugins/googlemap.html"
     
     def render(self, context, instance, placeholder):
-        if 'GOOGLE_MAPS_API_KEY' in context:
-            key = context['GOOGLE_MAPS_API_KEY']
-        else:
-            key = GOOGLE_MAPS_API_KEY
         context.update({
             'object':instance, 
             'placeholder':placeholder, 
-            'GOOGLE_MAPS_API_KEY':key
         })
         return context
     
     def get_plugin_media(self, request, context, plugin):
-        return Media(js = ('http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s&amp;hl=%s' % (settings.GOOGLE_MAPS_API_KEY, request.LANGUAGE_CODE),))
+        if 'GOOGLE_MAPS_API_KEY' in context:
+            key = context['GOOGLE_MAPS_API_KEY']
+        else:
+            key = GOOGLE_MAPS_API_KEY
+        return Media(js = ('http://maps.google.com/maps?file=api&amp;v=2&amp;key=%s&amp;hl=%s' % (key, request.LANGUAGE_CODE),))
  
 plugin_pool.register_plugin(GoogleMapPlugin)
