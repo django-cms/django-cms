6fba0e25d3936ea4b040a5c1dc907e863bcb9a90
ojii <jonas.obrist@divio.ch>
2010-05-14 16:53:02 +0200

    changed setup.py to use the proper classifiers bumped version to beta3

--- setup.py
+++ setup.py
@@ -14,6 +14,25 @@ for dirpath, dirnames, filenames in os.walk(os.path.join('cms', 'media')):
         if failed:
             continue
         media_files.append(os.path.join(*filepath.split(os.sep)[1:]))
+        
+if cms.VERSION[-1] == 'final':
+    CLASSIFIERS = ['Development Status :: 5 - Stable']
+elif 'beta' in cms.VERSION[-1]:
+    CLASSIFIERS = ['Development Status :: 4 - Beta']
+else:
+    CLASSIFIERS = ['Development Status :: 3 - Alpha']
+
+CLASSIFIERS += [
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
 
 setup(
     author="Patrick Lauber",
@@ -25,18 +44,7 @@ setup(
     url='http://www.django-cms.org/',
     license='BSD License',
     platforms=['OS Independent'],
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries :: Application Frameworks',
-    ],
+    classifiers=CLASSIFIERS,
     requires=[
         'django (>1.1.0)',
     ],
