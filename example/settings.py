b852791058261447b1d775cef2427ef87971e898
Jonas Obrist <jonas.obrist@divio.ch>
2010-05-26 22:54:00 +0200

    changed example database name to something more sane

--- example/settings.py
+++ example/settings.py
@@ -14,7 +14,7 @@ CACHE_BACKEND = 'locmem:///'
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'#'postgresql_psycopg2'       # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'cms'           # Or path to database file if using sqlite3.
+DATABASE_NAME = 'cms.sqlite'           # Or path to database file if using sqlite3.
 DATABASE_USER = 'cms'           # Not used with sqlite3.
 DATABASE_PASSWORD = 'cms'       # Not used with sqlite3.
 DATABASE_HOST = ''     # Set to empty string for localhost. Not used with sqlite3.
