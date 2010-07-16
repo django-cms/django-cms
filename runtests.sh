3da6e83cb500ae3c7b81c9137f2ec905ec296841
Jonas Obrist <jonas.obrist@divio.ch>
2010-07-16 16:30:11 +0200

    improved runtests.sh and gitignore

--- runtests.sh
+++ runtests.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
 cd tests
-if [ "`python -c 'import django; print django.get_version()' 2>/dev/null`" == "1.2.1" ]
-  then
-    cd testapp
-    if which coverage &> /dev/null; then
-        coverage run manage.py test cms
-        retcode=$?
-        coverage xml --omit=parts,/usr/,eggs
-        mv *.xml ..
-    else
-        python manage.py test cms
-        retcode=$?
-    fi
-    cd ..
-  else
-    echo "setting up test environment (this might take a while)..."
-    python bootstrap.py >/dev/null 2>&1
-    ./bin/buildout >/dev/null 2>&1
-    ./bin/coverage run ./testapp/manage.py test cms
-    retcode=$?
-    ./bin/coverage xml --omit=parts,/usr/,eggs
+echo "setting up test environment (this might take a while)..."
+python bootstrap.py >/dev/null 2>&1
+./bin/buildout >/dev/null 2>&1
+if [ $1 ]; then
+    suite="cms.$1"
+else
+    suite='cms'
 fi
+./bin/coverage run ./testapp/manage.py test $suite
+retcode=$?
+./bin/coverage xml --omit=parts,/usr/,eggs
+./bin/coverage html --omit=parts,/usr/,eggs
 cd ..
 exit $retcode
