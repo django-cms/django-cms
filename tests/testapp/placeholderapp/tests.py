bcfaada53e4d844905aab1b33932c44c18819312
yann.malet@gmail.com <yml@yml-laptop.(none)>
2010-08-04 18:23:03 +0200

    Test the get_fieldsets method directly rather with the django 'Client.get'

--- tests/testapp/placeholderapp/tests.py
+++ tests/testapp/placeholderapp/tests.py
@@ -1,6 +1,16 @@
 from django.test.testcases import TestCase
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.contrib import admin
+
+from testapp.placeholderapp.admin import (Example1Admin, Example2Admin,
+                                          Example3Admin, Example4Admin,
+                                          Example5Admin)
+from testapp.placeholderapp.models import (Example1, Example2, Example3,
+                                           Example4, Example5)
+
+class MockRequest(object):
+    GET = {}
 
 class Example1Test(TestCase):
     def setUp(self):
@@ -9,8 +19,14 @@ class Example1Test(TestCase):
     def tearDown(self):
         self.client.logout()
     
-    def test_example1_(self):
+    def test_admin_add_get(self):
         response = self.client.get(reverse('admin:placeholderapp_example1_add'))
+        
+    def test_admin_get_fieldsets(self):
+        request = MockRequest()
+        example1_admin = Example1Admin(Example1, admin.site)
+        fieldsets = example1_admin.get_fieldsets(request)
+        self.assertEqual(len(fieldsets),3)
         
 class Example2Test(TestCase):
     def setUp(self):
@@ -19,8 +35,13 @@ class Example2Test(TestCase):
     def tearDown(self):
         self.client.logout()
     
-    def test_example1_(self):
+    def test_admin_add_get(self):
         response = self.client.get(reverse('admin:placeholderapp_example2_add'))
+    def test_admin_get_fieldsets(self):
+        request = MockRequest()
+        example2_admin = Example1Admin(Example2, admin.site)
+        fieldsets = example2_admin.get_fieldsets(request)
+        self.assertEqual(len(fieldsets),3)
         
 class Example3Test(TestCase):
     def setUp(self):
@@ -29,8 +50,14 @@ class Example3Test(TestCase):
     def tearDown(self):
         self.client.logout()
     
-    def test_example1_(self):
+    def test_admin_add_get(self):
         response = self.client.get(reverse('admin:placeholderapp_example3_add'))
+    def test_admin_get_fieldsets(self):
+        request = MockRequest()
+        example3_admin = Example1Admin(Example3, admin.site)
+        fieldsets = example3_admin.get_fieldsets(request)
+        self.assertEqual(len(fieldsets),3)
+    
         
 class Example4Test(TestCase):
     def setUp(self):
@@ -39,8 +66,13 @@ class Example4Test(TestCase):
     def tearDown(self):
         self.client.logout()
     
-    def test_example1_(self):
+    def test_admin_add_get(self):
         response = self.client.get(reverse('admin:placeholderapp_example4_add'))
+    def test_admin_get_fieldsets(self):
+        request = MockRequest()
+        example4_admin = Example1Admin(Example4, admin.site)
+        fieldsets = example4_admin.get_fieldsets(request)
+        self.assertEqual(len(fieldsets),3)
         
 class Example5Test(TestCase):
     def setUp(self):
@@ -49,5 +81,10 @@ class Example5Test(TestCase):
     def tearDown(self):
         self.client.logout()
     
-    def test_example1_(self):
+    def test_admin_add_get(self):
         response = self.client.get(reverse('admin:placeholderapp_example5_add'))
+    def test_admin_get_fieldsets(self):
+        request = MockRequest()
+        example5_admin = Example1Admin(Example5, admin.site)
+        fieldsets = example5_admin.get_fieldsets(request)
+        self.assertEqual(len(fieldsets), 4)
