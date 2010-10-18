1cc8ac1c0e2a7e6f1e8995adcd5ad3e4f0f50c94
Jonas Obrist <jonas.obrist@divio.ch>
2010-10-18 10:17:40 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -37,8 +37,8 @@ CLASSIFIERS += [
 setup(
     author="Patrick Lauber",
     author_email="digi@treepy.com",
-    name='django-cms-dev-placeholder',
-    version='2.1.0a-dev53',
+    name='django-cms',
+    version=cms.__version__,
     description='An Advanced Django CMS',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     url='http://www.django-cms.org/',
