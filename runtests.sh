ee813b569a993854a268638213fafa112ea18b77
fivethreeo <oyvind.saltvik@gmail.com>
2010-12-04 22:55:47 +0100

    added --reuse-env and --help to runtests.sh

--- runtests.sh
+++ runtests.sh
@@ -1,31 +1,56 @@
 #!/bin/bash
 cd tests
-echo "setting up test environment (this might take a while)..."
-python bootstrap.py
-if [ $? != 0 ]; then
-    echo "bootstrap.py failed"
-    exit 1
-fi
-./bin/buildout
-if [ $? != 0 ]; then
-    echo "bin/buildout failed"
-    exit 1
-fi
 
 args=("$@")
 num_args=${#args[@]}
 index=0
 
+reuse_env=false
+
 while [ "$index" -lt "$num_args" ]
 do
-    if [ "${args[$index]}" = "--failfast" ]; then
-        failfast="--failfast"
-        
-    else
-        suite="cms.${args[$index]}"
-    fi
+    case "${args[$index]}" in
+        "--failfast")
+            failfast="--failfast"
+            ;;
+
+        "--reuse-env")
+            reuse_env=true
+            ;;
+
+        "--help")
+            echo ""
+            echo "usage:"
+            echo "    runtests.sh"
+            echo "    or runtests.sh [flags] [testcase]"
+            echo ""
+            echo "flags:"
+            echo "    --failfast - abort at first failing test"
+            echo "    --reuse-env - don't run builout, use last built environment" 
+            exit 1
+            ;;
+
+        *)
+            suite="cms.${args[$index]}"
+    esac
     let "index = $index + 1"
 done
+
+if [ $reuse_env == false ]; then
+    echo "setting up test environment (this might take a while)..."
+    python bootstrap.py
+    if [ $? != 0 ]; then
+        echo "bootstrap.py failed"
+        exit 1
+    fi
+    ./bin/buildout
+    if [ $? != 0 ]; then
+        echo "bin/buildout failed"
+        exit 1
+    fi
+else
+    echo "reusing current buildout environment"
+fi
 
 if [ "$failfast" ]; then
     echo "--failfast supplied, not using xmlrunner."
