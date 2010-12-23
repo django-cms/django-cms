87daaf590bee249b48d93a9cb5e414d49a580fda
Chris Glass <christopher.glass@divio.ch>
2010-12-23 15:52:08 +0100

    First run of unit testing the publisher command line tool.

--- cms/tests/publisher.py
+++ cms/tests/publisher.py
@@ -7,13 +7,15 @@ from cms.management.commands import publisher_publish
 from cms.models.pagemodel import Page
 from cms.tests.base import CMSTestCase
 from cms.tests.util.settings_contextmanager import SettingsOverride
+from cms.tests.util.standard_out_contextmanager import StdoutOverride
+from django.contrib.auth.models import User
 from django.core.management.base import CommandError
 
 class PublisherTestCase(CMSTestCase):
     '''
     A test case to exercise publisher
     '''
-
+    
     def test_01_simple_publisher(self):
         '''
         Creates the stuff needed for theses tests.
@@ -23,8 +25,6 @@ class PublisherTestCase(CMSTestCase):
                / \
               B  C
         '''
-        # Nuke all pages.
-        #Page.objects.all().delete()
         # Create a simple tree of 3 pages
         pageA = self.new_create_page(title="Page A", published= True, 
                                      in_navigation= True)
@@ -65,3 +65,53 @@ class PublisherTestCase(CMSTestCase):
             except CommandError:
                 raised = True
         self.assertTrue(raised)
+        
+        
+    def test_04_command_line_publishes_zero_pages_on_empty_db(self):
+        # we need to create a superuser (the db is empty)
+        User.objects.create_superuser('djangocms', 'cms@example.com', '123456')
+        
+        pages_from_output = 0
+        published_from_output = 0
+        
+        with StdoutOverride() as buffer:
+            # Now we don't expect it to raise, but we need to redirect IO
+            com = publisher_publish.Command()
+            com.handle_noargs()
+            lines = buffer.getvalue().split('\n') #NB: readlines() doesn't work
+            
+        for line in lines:
+            if 'Total' in line:
+                pages_from_output = int(line.split(':')[1])
+            elif 'Published' in line:
+                published_from_output = int(line.split(':')[1])
+                
+        self.assertEqual(pages_from_output,0)
+        self.assertEqual(published_from_output,0)
+        
+#    def test_05_command_line_publishes_one_page(self):
+#        # we need to create a superuser (the db is empty)
+#        User.objects.create_superuser('djangocms', 'cms@example.com', '123456')
+#        
+#        # Now, let's create an unpublished page.
+#        page = self.new_create_page(title="I'm unpublished", published= False)
+#        pages_from_output = 0
+#        published_from_output = 0
+#        
+#        with StdoutOverride() as buffer:
+#            # Now we don't expect it to raise, but we need to redirect IO
+#            com = publisher_publish.Command()
+#            com.handle_noargs()
+#            lines = buffer.getvalue().split('\n') #NB: readlines() doesn't work
+#            
+#        for line in lines:
+#            import ipdb
+#            if 'Total' in line:
+#                ipdb.set_trace()
+#                pages_from_output = int(line.split(':')[1])
+#            elif 'Published' in line:
+#                ipdb.set_trace()
+#                published_from_output = int(line.split(':')[1])
+#                
+#        self.assertEqual(pages_from_output,1)
+#        self.assertEqual(published_from_output,1)
--- cms/tests/util/standard_out_contextmanager.py
+++ cms/tests/util/standard_out_contextmanager.py
@@ -7,15 +7,23 @@ import StringIO
 import sys
 
 
-class StandardIOOverride(object):
+class StdoutOverride(object):
     """
+    This overrides Python's the standard output and redirrects it to a StringIO
+    object, so that on can test the output of the program.
     
+    example:
+    lines = None
+    with StdoutOverride() as buffer:
+        # print stuff
+        lines = buffer.getvalue()
     """
     def __enter__(self):
-        buffer = StringIO.StringIO()
-        sys.stdout = buffer
-        return buffer
+        self.buffer = StringIO.StringIO()
+        sys.stdout = self.buffer
+        return self.buffer
         
     def __exit__(self, type, value, traceback):
+        self.buffer.close()
         # Revert the stdout to the real one
         sys.stdout = sys.__stdout__
