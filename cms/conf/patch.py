a23850d81c551a5dd1dc24c0a55dc936d7b29cc5
Jonas Obrist <jonas.obrist@divio.ch>
2011-01-03 17:59:25 +0100

    added deprecation warning for CMS_DBGETTEXT_SLUGS being dropped in 2.2

--- cms/conf/patch.py
+++ cms/conf/patch.py
@@ -33,6 +33,9 @@ def post_patch():
     else:
         # dummy translation
         settings.dbgettext = lambda x: x
+    if settings.CMS_DBGETTEXT_SLUGS:
+        warn("CMS_DBGETTEXT_SLUGS (and CMS_DBGETTEXT) will be deprecated in "
+             "django CMS 2.2.", DeprecationWarning)
 
 
 def post_patch_check():
