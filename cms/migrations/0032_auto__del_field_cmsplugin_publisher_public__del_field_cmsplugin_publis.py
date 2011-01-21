f6c6d961a01602f31291d29d0ea7ee4ec333be6e
Chris Glass <christopher.glass@divio.ch>
2011-01-21 11:59:36 +0100

    Changed source encoding for South migrations.

--- cms/migrations/0032_auto__del_field_cmsplugin_publisher_public__del_field_cmsplugin_publis.py
+++ cms/migrations/0032_auto__del_field_cmsplugin_publisher_public__del_field_cmsplugin_publis.py
@@ -1,8 +1,6 @@
-# encoding: utf-8
-import datetime
+# -*- coding: utf-8 -*-
 from south.db import db
 from south.v2 import SchemaMigration
-from django.db import models
 
 class Migration(SchemaMigration):
 
