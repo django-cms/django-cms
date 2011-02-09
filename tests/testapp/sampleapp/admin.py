ce3b22496bca225ed59f1f982f8da7d9e46d2460
Jonas Obrist <jonas.obrist@divio.ch>
2011-02-09 17:37:24 +0100

    changed tests to use django 1.2.5

--- tests/testapp/sampleapp/admin.py
+++ tests/testapp/sampleapp/admin.py
@@ -1,10 +1,11 @@
+from cms.admin.placeholderadmin import PlaceholderAdmin
 from django.contrib import admin
 from testapp.sampleapp.models import Picture, Category
 
 class PictureInline(admin.StackedInline):
     model = Picture
 
-class CategoryAdmin(admin.ModelAdmin):
+class CategoryAdmin(PlaceholderAdmin):
     inlines = [PictureInline]
 
 admin.site.register(Category, CategoryAdmin)
