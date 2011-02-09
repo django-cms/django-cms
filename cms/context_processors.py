40821b6232ae0dae2ef34d635ac2bdec1e6178ae
Adrián Ribao Martínez <adrian@adrima.es>
2011-02-09 02:01:12 +0100

    Use django-staticfiles from django 1.3

--- cms/context_processors.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-
-def media(request):
-    """
-    Adds media-related context variables to the context.
-    """
-    return {'CMS_MEDIA_URL': settings.CMS_MEDIA_URL}
