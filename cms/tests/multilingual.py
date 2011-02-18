1a49a95d3d04c9239d6c79c81c70bf46f102355b
Benjamin Wohlwend <piquadrat@gmail.com>
2011-02-18 12:29:22 +0100

    made the test fail directly, instead of just ruining `reverse` for the following tests

--- cms/tests/multilingual.py
+++ cms/tests/multilingual.py
@@ -64,7 +64,14 @@ class MultilingualTestCase(CMSTestCase):
         self.assertEqual(placeholder.cmsplugin_set.filter(language='en').count(), 1)
 
     def test_03_multiple_reverse_monkeypatch(self):
+        """
+        This test is not very well behaved, every following
+        test that uses reverse will fail with a RuntimeException.
+        """
         from cms.models import monkeypatch_reverse
         monkeypatch_reverse()
         monkeypatch_reverse()
-
+        try:
+            reverse('foobar')
+        except RuntimeError:
+            self.fail('maximum recursion depth exceeded')
