c7b01db9be2d2ddcd74e2d5b4926a5d13ba1ffee
Jannis Leidel <jannis@leidel.info>
2011-02-21 17:20:05 +0100

    Only execute post_save_user_group signal if user isn't anonymous.

--- cms/signals.py
+++ cms/signals.py
@@ -154,7 +154,7 @@ def post_save_user_group(instance, raw, created, **kwargs):
     from cms.utils.permissions import get_current_user
     # read current user from thread locals
     creator = get_current_user()
-    if not creator or not created:
+    if not creator or not created or creator.is_anonymous():
         return
     from django.db import connection
     
