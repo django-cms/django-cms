c12012a1333efb6cb1144c3a047c72c3c4ca7d3a
Iacopo Spalletti <i.spalletti@nephila.it>
2011-02-21 19:01:50 +0100

    Solves show_sub_menu by checking visible property in menus.templatetags.cut_after()

--- menus/templatetags/menu_tags.py
+++ menus/templatetags/menu_tags.py
@@ -22,8 +22,15 @@ def cut_after(node, levels, removed):
         removed.extend(node.children)
         node.children = []
     else:
+        removed_local = []
         for n in node.children:
-            cut_after(n, levels - 1, removed)
+            if n.visible:
+                cut_after(n, levels - 1, removed)
+            else:
+                removed_local.append(n)
+        for n in removed_local:
+            node.children.remove(n)
+        removed.extend(removed_local)
 
 def remove(node, removed):
     removed.append(node)
