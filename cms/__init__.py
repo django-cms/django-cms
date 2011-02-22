f1296cee6eb6f5563fbc3b8321082a85dacdc6eb
Jonas Obrist <jonas.obrist@divio.ch>
2011-02-22 13:44:00 +0100

    Bumped version number

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-VERSION = (2, 1, 2, 'final')
+VERSION = (2, 1, 3, 'final')
 if VERSION[-1] != "final": # pragma: no cover
     __version__ = '.'.join(map(str, VERSION))
 else: # pragma: no cover
