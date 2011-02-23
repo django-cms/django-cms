2ff45e7c5557340f3714989d4b490f77b9222428
Jonas Obrist <jonas.obrist@divio.ch>
2011-02-23 17:51:11 +0100

    - Removed googlemap settings since we don't need them anymore - Removed media getter utils - Don't get coverage of tests

--- cms/plugins/utils.py
+++ cms/plugins/utils.py
@@ -1,5 +1,3 @@
-import operator
-from django.forms.widgets import Media
 from cms.utils import get_language_from_request
 from cms.utils.moderator import get_cmsplugin_queryset
 
@@ -13,10 +11,6 @@ def get_plugins(request, placeholder, lang=None):
         ).order_by('placeholder', 'position').select_related())
     return getattr(placeholder, '_%s_plugins_cache' % lang)
 
-def get_plugin_media(request, context, plugin):
-    instance, plugin = plugin.get_plugin_instance()
-    return plugin.get_plugin_media(request, context, instance)
-
 def get_plugins_for_page(request, page, lang=None):
     if not page:
         return []
@@ -26,17 +20,3 @@ def get_plugins_for_page(request, page, lang=None):
             placeholder__page=page, language=lang, parent__isnull=True
         ).order_by('placeholder', 'position').select_related())
     return getattr(page, '_%s_plugins_cache' % lang)
-
-def get_plugins_media(request, context, page):
-    lang = get_language_from_request(request)
-    if not page:
-        # current page is unknown
-        return []
-    if not hasattr(page, '_%s_plugins_media_cache' % lang):
-        plugins = get_plugins_for_page(request, page, lang=lang)
-        media_classes = [get_plugin_media(request, context, plugin) for plugin in plugins]
-        if media_classes:
-            setattr(page, '_%s_plugins_media_cache' % lang, reduce(operator.add, media_classes))
-        else:
-            setattr(page, '_%s_plugins_media_cache' % lang,  Media())
-    return getattr(page, '_%s_plugins_media_cache' % lang)
