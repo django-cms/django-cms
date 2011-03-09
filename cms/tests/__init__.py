3b9f608c8212ad750152923fb87f12c29da42c4d
Jonas Obrist <jonas.obrist@divio.ch>
2011-03-09 23:35:08 +0100

    Added some more tests, somehow dropped below 80% in this branch...

--- cms/tests/__init__.py
+++ cms/tests/__init__.py
@@ -1,33 +1,35 @@
 # -*- coding: utf-8 -*-
-from cms.tests.admin import AdminTestCase, AdminFieldsetTests, AdminListFilterTests
-from cms.tests.apphooks import ApphooksTestCase
+from cms.tests.admin import (AdminTestCase, AdminFieldsetTests, 
+    AdminListFilterTests, AdminTests)
 from cms.tests.api import PythonAPITests
+from cms.tests.apphooks import ApphooksTestCase
 from cms.tests.docs import DocsTestCase
 from cms.tests.forms import FormsTestCase
 from cms.tests.mail import MailTestCase
+from cms.tests.menu import (FixturesMenuTests, MenuTests, AdvancedSoftrootTests, 
+    ShowSubMenuCheck)
+from cms.tests.menu_utils import MenuUtilsTests
 from cms.tests.middleware import MiddlewareTestCase
 from cms.tests.multilingual import MultilingualTestCase
-from cms.tests.menu import FixturesMenuTests, MenuTests, AdvancedSoftrootTests, ShowSubMenuCheck
 from cms.tests.navextender import NavExtenderTestCase
 from cms.tests.nonroot import NonRootCase
 from cms.tests.page import PagesTestCase, NoAdminPageTests
 from cms.tests.permmod import PermissionModeratorTestCase, PatricksMoveTest
-from cms.tests.plugins import (PluginManyToManyTestCase, PluginsTestCase, 
-    SekizaiTests, LinkPluginTestCase)
 from cms.tests.placeholder import (PlaceholderModelTests, PlaceholderAdminTest, 
     PlaceholderTestCase, PlaceholderActionTests)
+from cms.tests.plugins import (PluginManyToManyTestCase, PluginsTestCase, 
+    SekizaiTests, LinkPluginTestCase)
+from cms.tests.po import PoTest
 from cms.tests.publisher import PublisherTestCase
 from cms.tests.rendering import RenderingTestCase
 from cms.tests.reversion_tests import ReversionTestCase
+from cms.tests.security import SecurityTests
 from cms.tests.settings import SettingsTests
 from cms.tests.site import SiteTestCase
 from cms.tests.templatetags import TemplatetagTests, TemplatetagDatabaseTests
 from cms.tests.toolbar import ToolbarTests
 from cms.tests.urlutils import UrlutilsTestCase
 from cms.tests.views import ViewTests
-from cms.tests.security import SecurityTests
-from cms.tests.po import PoTest
-from cms.tests.menu_utils import MenuUtilsTests
 
 try:
     from cms.tests.javascript import JavascriptTestCase
