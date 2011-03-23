01e21913d1513cca9ca4ee877020ef728e12757b
Darren Pearce <darren@darrenpearce.com>
2011-03-22 22:51:51 -0600

    removed caching from ShowUncachedPlaceholderById

--- cms/templatetags/cms_tags.py
+++ cms/templatetags/cms_tags.py
@@ -380,7 +380,7 @@ class ShowUncachedPlaceholderById(ShowPlaceholderById):
     name = 'show_uncached_placeholder_by_id'
     def get_kwargs(self, *args, **kwargs):
         kwargs = super(ShowUncachedPlaceholderById, self).get_kwargs(*args, **kwargs)
-        kwargs['cache_result'] = True
+        kwargs['cache_result'] = False
         return kwargs
 register.tag(ShowUncachedPlaceholderById)
 register.tag('show_uncached_placeholder', ShowUncachedPlaceholderById)
