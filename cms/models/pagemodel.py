3ff72d077b66b9eadda412dbc0bb45fa69b56c78
Frank Bieniek <frank.bieniek@produktlaunch.de>
2011-03-31 14:36:37 +0100

    inheritance of the can_view permission works now for anonymous users

--- cms/models/pagemodel.py
+++ cms/models/pagemodel.py
@@ -704,9 +704,11 @@ class Page(MPTTModel):
         if request.user.is_staff and settings.CMS_PUBLIC_FOR in ('staff', 'all'):
             return True
         # does any restriction exist?
-        # just checks the direct can_view Permission, not the inherited ones
-        is_restricted = PagePermission.objects.filter(page=self, can_view=True).exists()
-        # user is logged in
+        # direct
+        #is_restricted = PagePermission.objects.filter(page=self, can_view=True).exists()
+        # inherited and direct
+        is_restricted = PagePermission.objects.for_page(self).filter(can_view=True).exists()
+        
         if request.user.is_authenticated():
             site = current_site(request)
             global_view_perms = GlobalPagePermission.objects.with_user(
@@ -722,8 +724,6 @@ class Page(MPTTModel):
             if (not is_restricted and not global_view_perms and 
                 settings.CMS_PUBLIC_FOR == 'all'):
                 return True
-            
-            
         else:
             #anonymous user
             if is_restricted or not settings.CMS_PUBLIC_FOR == 'all':
