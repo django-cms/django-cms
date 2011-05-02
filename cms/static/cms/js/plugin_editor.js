c4ffac96f33bff92fab685803a2f1bb98e94a5a3
Adrián Ribao Martínez <adrian@adrima.es>
2011-05-02 19:29:50 +0200

    Added some missing files

--- cms/static/cms/js/plugin_editor.js
+++ cms/static/cms/js/plugin_editor.js
@@ -1,30 +1,6 @@
 (function($) {
     $(document).ready(function() {
-        // Manage Ajax CSRF connectios
-        $('html').ajaxSend(function(event, xhr, settings) {
-            function getCookie(name) {
-                var cookieValue = null;
-                if (document.cookie && document.cookie != '') {
-                    var cookies = document.cookie.split(';');
-                    for (var i = 0; i < cookies.length; i++) {
-                        var cookie = jQuery.trim(cookies[i]);
-                        // Does this cookie string begin with the name we want?
-                        if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                            cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                            break;
-                        }
-                    }
-                }
-                return cookieValue;
-            }
-            if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-                // Only send the token to relative URLs i.e. locally.
-                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-            }
-        });
-
         // Add Plugin Handler
-    	$.fn.cmsPatchCSRF();
         $('span.add-plugin').click(function(){
          var select = $(this).parent().children("select[name=plugins]");
             var pluginvalue = select.attr('value');
--- cms/static/cms/js/toolbar.js
+++ cms/static/cms/js/toolbar.js
@@ -12,30 +12,6 @@ function hide_iframe(){
 
 jQuery(document).ready(function($) {
 
-    // Manage Ajax CSRF connectios
-    $('html').ajaxSend(function(event, xhr, settings) {
-        function getCookie(name) {
-            var cookieValue = null;
-            if (document.cookie && document.cookie != '') {
-                var cookies = document.cookie.split(';');
-                for (var i = 0; i < cookies.length; i++) {
-                    var cookie = jQuery.trim(cookies[i]);
-                    // Does this cookie string begin with the name we want?
-                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                        break;
-                    }
-                }
-            }
-            return cookieValue;
-        }
-        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
-            // Only send the token to relative URLs i.e. locally.
-            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-        }
-    });
- 
-
     jQuery.fn.swapWith = function(to) {
         return this.each(function() {
             var copy_to = $(to).clone(true);
