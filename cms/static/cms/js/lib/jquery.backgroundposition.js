9272f232138ff956e73a4994ab53cff1b8627e96
Adrián Ribao Martínez <adrian@adrima.es>
2011-05-02 19:39:52 +0200

    More extra files that were ignored by .gitignore

---
+++ cms/static/cms/js/lib/jquery.backgroundposition.js
@@ -0,0 +1,52 @@
+(function($){
+	
+	if(!document.defaultView || !document.defaultView.getComputedStyle){
+		var oldCurCSS = jQuery.curCSS;
+		jQuery.curCSS = function(elem, name, force){
+			if(name !== 'backgroundPosition' || !elem.currentStyle || elem.currentStyle[ name ]){
+				return oldCurCSS.apply(this, arguments);
+			}
+			var style = elem.style;
+			if ( !force && style && style[ name ] ){
+				return style[ name ];
+			}
+			return oldCurCSS(elem, 'backgroundPositionX', force) +' '+ oldCurCSS(elem, 'backgroundPositionY', force);
+		};
+	}
+})(jQuery);
+
+(function($) {
+	
+	function toArray(strg){
+		strg = strg.replace(/left|top/g,'0px');
+		strg = strg.replace(/right|bottom/g,'100%');
+		strg = strg.replace(/([0-9\.]+)(\s|\)|$)/g,"$1px$2");
+		var res = strg.match(/(-?[0-9\.]+)(px|\%|em|pt)\s(-?[0-9\.]+)(px|\%|em|pt)/);
+		return [parseFloat(res[1],10),res[2],parseFloat(res[3],10),res[4]];
+	}
+	
+	$.fx.step. backgroundPosition = function(fx) {
+		if (!fx.bgPosReady) {
+			
+			var start = $.curCSS(fx.elem,'backgroundPosition');
+			if(!start){//FF2 no inline-style fallback
+				start = '0px 0px';
+			}
+			
+			start = toArray(start);
+			fx.start = [start[0],start[2]];
+			
+			var end = toArray(fx.options.curAnim.backgroundPosition);
+			fx.end = [end[0],end[2]];
+			
+			fx.unit = [end[1],end[3]];
+			fx.bgPosReady = true;
+		}
+		
+		var nowPosX = [];
+		nowPosX[0] = ((fx.end[0] - fx.start[0]) * fx.pos) + fx.start[0] + fx.unit[0];
+		nowPosX[1] = ((fx.end[1] - fx.start[1]) * fx.pos) + fx.start[1] + fx.unit[1];           
+		fx.elem.style.backgroundPosition = nowPosX[0]+' '+nowPosX[1];
+
+	};
+})(jQuery);
