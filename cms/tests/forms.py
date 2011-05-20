d0f42189d44cf6cf2bef395a2e4ed43b8dd87ca1
Jonas Obrist <jonas.obrist@divio.ch>
2011-05-20 19:25:37 +0200

    Made update_site_and_pages_choices 3 times faster and added a test for it

--- cms/tests/forms.py
+++ cms/tests/forms.py
@@ -4,10 +4,12 @@ from cms.admin import forms
 from cms.admin.forms import PageUserForm
 from cms.api import create_page, create_page_user
 from cms.forms.fields import PageSelectFormField, SuperLazyIterator
-from cms.forms.utils import get_site_choices, get_page_choices
+from cms.forms.utils import (get_site_choices, get_page_choices, 
+    update_site_and_page_choices)
 from cms.test_utils.testcases import CMSTestCase
 from cms.test_utils.util.context_managers import SettingsOverride
 from django.contrib.auth.models import User
+from django.contrib.sites.models import Site
 from django.core.cache import cache
 
 class Mock_PageSelectFormField(PageSelectFormField): 
@@ -35,7 +37,7 @@ class FormsTestCase(CMSTestCase):
             result = get_site_choices()
             self.assertEquals(result, [])
             
-    def test_get_site_choices_without_moderator(self):
+    def test_get_site_choices_without_moderator_with_superuser(self):
         with SettingsOverride(CMS_MODERATOR=False):
             # boilerplate (creating a page)
             user_super = User(username="super", is_staff=True, is_active=True, 
@@ -84,6 +86,24 @@ class FormsTestCase(CMSTestCase):
             data_list = (0, home_page.pk) #(site_id, page_id) dsite-id is not used
             result = fake_field.compress(data_list)
             self.assertEquals(home_page,result)
+            
+    def test_update_site_and_page_choices(self):
+        with SettingsOverride(CMS_MODERATOR=False):
+            Site.objects.all().delete()
+            site = Site.objects.create(domain='http://www.django-cms.org', name='Django CMS')
+            page1 = create_page('Page 1', 'nav_playground.html', 'en', site=site)
+            page2 = create_page('Page 2', 'nav_playground.html', 'de', site=site)
+            # enfore the choices to be casted to a list
+            site_choices, page_choices = [list(bit) for bit in update_site_and_page_choices('en')]
+            self.assertEqual(page_choices, [
+                ('', '----'),
+                (site.name, [
+                    (page1.pk, 'Page 1'),
+                    (page2.pk, 'Page 2'),
+                ])
+            ])
+            self.assertEqual(site_choices, [(site.pk, site.name)])
+
         
     def test_superlazy_iterator_behaves_properly_for_sites(self):
         normal_result = get_site_choices()
