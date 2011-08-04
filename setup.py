a04064216cded28a27a3cd00550a9794d7a90bab
Jonas Obrist <jonas.obrist@divio.ch>
2011-08-04 13:55:52 +0200

    Updated MANIFEST.in and setup.py to reflect the staticfiles changes

--- setup.py
+++ setup.py
@@ -1,19 +1,7 @@
 from setuptools import setup, find_packages
-import os, fnmatch
+import os
 import cms
 
-media_files = []
-
-for dirpath, dirnames, filenames in os.walk(os.path.join('cms', 'media')):
-    for filename in filenames:
-        filepath = os.path.join(dirpath, filename)
-        failed = False
-        for pattern in ('*.py', '*.pyc', '*~', '.*', '*.bak', '*.swp*'):
-            if fnmatch.fnmatchcase(filename, pattern):
-                failed = True
-        if failed:
-            continue
-        media_files.append(os.path.join(*filepath.split(os.sep)[1:]))
         
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
@@ -46,25 +34,7 @@ setup(
         'django-mptt>=0.4.2',
         'django-sekizai>=0.4.2',
     ],
-    packages=find_packages(exclude=["testdata","testdata.*"]),
+    packages=find_packages(exclude=["project","project.*"]),
     include_package_data=True,
-#    package_data={
-#        'cms': [
-#            'templates/admin/*.html',
-#            'templates/admin/cms/mail/*.html',
-#            'templates/admin/cms/mail/*.txt',
-#            'templates/admin/cms/page/*.html',
-#            'templates/admin/cms/page/*/*.html',
-#            'templates/cms/*.html',
-#            'templates/cms/*/*.html',
-#            'plugins/*/templates/cms/plugins/*.html',
-#            'plugins/*/templates/cms/plugins/*/*.html',
-#            'plugins/*/templates/cms/plugins/*/*.js',
-#            'locale/*/LC_MESSAGES/*',
-#        ] + media_files,
-#        'menus': [
-#            'templates/menu/*.html',
-#        ],
-#    },
     zip_safe = False
 )
