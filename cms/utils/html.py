7610771fa254f902599e53f1bd1d22714d387b4a
Jonas Obrist <jonas.obrist@divio.ch>
2011-08-24 16:09:20 +0200

    Moved text plugin cleaning to cms.plugins.text.models.AbstractText.clean Moved html cleaning to cms.utils.html.clean_html

---
+++ cms/utils/html.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+import html5lib
+from html5lib import sanitizer
+
+
+DEFAULT_PARSER = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer)
+
+
+def _get_inner_body(doc):
+    # find 'body'
+    def _rec(node):
+        if node.type == 5: # Element Type
+            if node.name == 'body': # the body element
+                return node
+        for child in node.childNodes:
+            childfound = _rec(child)
+            if childfound:
+                return childfound
+        return None
+    body = _rec(doc)
+    # if the first element after <body> is a html tag, this returns an Element
+    # instance, otherwise a (unicode) string, this is why we need to check
+    # the output of this and potentially call .toxml() again.
+    out = reduce(lambda x,y:x.toxml()+y.toxml(), body.childNodes)
+    if isinstance(out, basestring):
+        return out
+    return out.toxml()
+
+
+def clean_html(data, full=True, parser=DEFAULT_PARSER):
+    """
+    Cleans HTML from XSS vulnerabilities using html5lib
+    
+    If full is False, only the contents inside <body> will be returned (without
+    the <body> tags).
+    """
+    doc = parser.parse(data)
+    if full:
+        return doc.toxml()
+    else:
+        return _get_inner_body(doc)
