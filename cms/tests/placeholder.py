d300ffb4a20c4b6d22bac1869ad11e4ef69e0a42
Jonas Obrist <jonas.obrist@divio.ch>
2011-10-14 13:39:36 +0200

    fixed typo in cms.tests.placeholder

--- cms/tests/placeholder.py
+++ cms/tests/placeholder.py
@@ -73,7 +73,7 @@ class PlaceholderTestCase(CMSTestCase):
         placeholders = get_placeholders('placeholder_tests/outside.html')
         self.assertEqual(sorted(placeholders), sorted([u'new_one', u'two', u'base_outside']))
 
-    def test_placeholder_scanning_extend_outside_block_nested(self)
+    def test_placeholder_scanning_extend_outside_block_nested(self):
         placeholders = get_placeholders('placeholder_tests/outside_nested.html')
         self.assertEqual(sorted(placeholders), sorted([u'new_one', u'two', u'base_outside']))
     
