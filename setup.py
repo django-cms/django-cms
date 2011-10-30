7664c33ac5131218d35148097dcc39e1d841c921
Jonas Obrist <jonas.obrist@divio.ch>
2011-10-30 16:30:40 +0100

    fix mptt to 0.4.2, since 0.5 breaks all the things

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         'django-classy-tags>=0.3.4.1',
         'south>=0.7.2',
         'html5lib',
-        'django-mptt>=0.4.2',
+        'django-mptt==0.4.2',
         'django-sekizai>=0.4.2',
     ],
     packages=find_packages(exclude=["project","project.*"]),
