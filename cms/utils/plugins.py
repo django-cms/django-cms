5c228a5b4eb5c715fbe0f9b44a949c03b7272028
Tom Shortall <scytale@gmail.com>
2012-03-05 14:49:57 +0000

    use 'hasattr' instead of try/except for parent_name_expr check

--- cms/utils/plugins.py
+++ cms/utils/plugins.py
@@ -20,11 +20,8 @@ def _extend_blocks(extend_node, blocks):
     Extends the dictionary `blocks` with *new* blocks in the parent node (recursive)
     """
     # we don't support variable extensions
-    try:
-        if extend_node.parent_name_expr:
-            return
-    except AttributeError:
-        pass
+    if hasattr(extend_node, 'parent_name_expr') and extend_node.parent_name_expr:
+        return
     parent = extend_node.get_parent(None)
     # Search for new blocks
     for node in parent.nodelist.get_nodes_by_type(BlockNode):
@@ -57,11 +54,8 @@ def _extend_nodelist(extend_node):
     ExtendsNode
     """
     # we don't support variable extensions
-    try:
-        if extend_node.parent_name_expr:
-            return []
-    except AttributeError:
-        pass
+    if hasattr(extend_node, 'parent_name_expr') and extend_node.parent_name_expr:
+        return []
     blocks = extend_node.blocks
     _extend_blocks(extend_node, blocks)
     placeholders = []
