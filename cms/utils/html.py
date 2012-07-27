370018c5ba0326f1a0a0ffbd4cd05c7a893ba950
Iacopo Spalletti <i.spalletti@nephila.it>
2012-07-27 19:02:15 +0200

    Import cleanup

--- cms/utils/html.py
+++ cms/utils/html.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from html5lib import sanitizer, serializer, treebuilders, treewalkers, sanitizer
+from html5lib import sanitizer, serializer, treebuilders, treewalkers
 import html5lib
 
 DEFAULT_PARSER = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer,
