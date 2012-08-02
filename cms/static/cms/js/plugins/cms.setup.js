f4f2b4b16b7a930dad312ad75241abb6770556b2
Benjamin Wohlwend <piquadrat@gmail.com>
2012-08-02 13:12:29 +0200

    move the namespaced jQuery instantiation into the CMS array

--- cms/static/cms/js/plugins/cms.setup.js
+++ cms/static/cms/js/plugins/cms.setup.js
@@ -4,12 +4,9 @@
 // insuring django namespace is available when using on admin
 var django = django || undefined;
 
-// assigning correct jquery instance to our jQuery variable
-var cmsQuery = (django) ? django.jQuery : window.jQuery || undefined;
-
 // assign global namespaces
 var CMS = {
-	'$': cmsQuery.noConflict(),
+	'$': (django ? django.jQuery : window.jQuery || undefined).noConflict(),
 	'Class': Class.$noConflict(),
 	'API': {}
 };
