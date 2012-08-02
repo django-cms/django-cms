41632195a232e8d2d619c67c0929fcaf1a07467e
Benjamin Wohlwend <piquadrat@gmail.com>
2012-08-02 11:58:41 +0200

    don't overwrite the global jQuery object, but assign it to one of our own.
    Disclaimer: I have no idea what I'm doing.
    
    Fixes #1349

--- cms/static/cms/js/plugins/cms.setup.js
+++ cms/static/cms/js/plugins/cms.setup.js
@@ -4,12 +4,12 @@
 // insuring django namespace is available when using on admin
 var django = django || undefined;
 
-// assigning correct jquery instance to jQuery variable
-var jQuery = (django) ? django.jQuery : window.jQuery || undefined;
+// assigning correct jquery instance to our jQuery variable
+var cmsQuery = (django) ? django.jQuery : window.jQuery || undefined;
 
 // assign global namespaces
 var CMS = {
-	'$': jQuery.noConflict(),
+	'$': cmsQuery.noConflict(),
 	'Class': Class.$noConflict(),
 	'API': {}
 };
