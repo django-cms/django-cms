807f0b61a646adea8da6c81bbf7b75dfc6b25b1b
Jonathan Stoppani <jonathan@stoppani.name>
2012-08-10 18:51:23 +0200

    Setting the current_app on the context based on the page reverse_id and the app_name attribute of the CMSApp instance

--- cms/views.py
+++ cms/views.py
@@ -70,6 +70,7 @@ def details(request, slug):
                 pattern_list += urlpatterns
             urlpatterns = patterns('', *pattern_list)
             try:
+                context.current_app = page.reverse_id if page.reverse_id else app.app_name
                 view, args, kwargs = resolve('/', tuple(urlpatterns))
                 return view(request, *args, **kwargs)
             except Resolver404:
