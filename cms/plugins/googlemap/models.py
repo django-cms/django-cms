07bcbf8d419af235c8492827a728658afbe77ea1
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-08-13 18:22:15 +0200

    [Google Maps] Reverts one code styling in models.py.

--- cms/plugins/googlemap/models.py
+++ cms/plugins/googlemap/models.py
@@ -1,5 +1,4 @@
-from django.db.models import CharField, IntegerField, DecimalField, \
-                             BooleanField
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from cms.models import CMSPlugin
 
@@ -8,26 +7,28 @@ class GoogleMap(CMSPlugin):
     """
     A google maps integration
     """
-    title = CharField(_("map title"), max_length=100, blank=True, null=True)
+    title = models.CharField(_("map title"), max_length=100, blank=True,
+                             null=True)
 
-    address = CharField(_("address"), max_length=150)
-    zipcode = CharField(_("zip code"), max_length=30)
-    city = CharField(_("city"), max_length=100)
+    address = models.CharField(_("address"), max_length=150)
+    zipcode = models.CharField(_("zip code"), max_length=30)
+    city = models.CharField(_("city"), max_length=100)
 
-    content = CharField(_("additional content"), max_length=255, blank=True)
-    zoom = IntegerField(_("zoom level"), blank=True, null=True)
+    content = models.CharField(_("additional content"), max_length=255,
+                               blank=True)
+    zoom = models.IntegerField(_("zoom level"), blank=True, null=True)
 
-    lat = DecimalField(_('latitude'), max_digits=10, decimal_places=6,
-                       null=True, blank=True,
-                       help_text=_('Use latitude & longitude to fine '
-                                   'tune the map position.'))
-    lng = DecimalField(_('longitude'), max_digits=10, decimal_places=6,
+    lat = models.DecimalField(_('latitude'), max_digits=10, decimal_places=6,
+                              null=True, blank=True,
+                              help_text=_('Use latitude & longitude to fine '
+                                          'tune the map position.'))
+    lng = models.DecimalField(_('longitude'), max_digits=10, decimal_places=6,
                        null=True, blank=True)
 
-    route_planer_title = CharField(_("route planer title"), max_length=150,
-                               blank=True, null=True,
+    route_planer_title = models.CharField(_("route planer title"),
+                               max_length=150, blank=True, null=True,
                                default=_('Calculate your fastest way to here'))
-    route_planer = BooleanField(_("route planer"), default=False)
+    route_planer = models.BooleanField(_("route planer"), default=False)
 
     def __unicode__(self):
         return u"%s (%s, %s %s)" % (self.get_title(), self.address,
