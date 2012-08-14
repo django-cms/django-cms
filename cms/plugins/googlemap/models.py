316e237cefd5dcd7cba783ec9ff7551455609ad2
Bertrand Bordage <bordage.bertrand@gmail.com>
2012-08-14 12:34:07 +0200

    [Google Map] Use choices for zoom level.

--- cms/plugins/googlemap/models.py
+++ cms/plugins/googlemap/models.py
@@ -15,15 +15,18 @@ class GoogleMap(CMSPlugin):
     city = models.CharField(_("city"), max_length=100)
 
     content = models.CharField(_("additional content"), max_length=255,
-                               blank=True)
-    zoom = models.IntegerField(_("zoom level"), blank=True, null=True)
+                         blank=True,
+                         help_text=_('Displayed under address in the bubble.'))
+    ZOOM_LEVELS = map(lambda c: (c, str(c)), range(22))
+    zoom = models.PositiveSmallIntegerField(_("zoom level"),
+                                            choices=ZOOM_LEVELS, default=13)
 
     lat = models.DecimalField(_('latitude'), max_digits=10, decimal_places=6,
                               null=True, blank=True,
                               help_text=_('Use latitude & longitude to fine '
                                           'tune the map position.'))
     lng = models.DecimalField(_('longitude'), max_digits=10, decimal_places=6,
-                       null=True, blank=True)
+                              null=True, blank=True)
 
     route_planer_title = models.CharField(_("route planer title"),
                                max_length=150, blank=True, null=True,
@@ -43,11 +46,6 @@ class GoogleMap(CMSPlugin):
         if self.title is None:
             return _("Map")
         return self.title
-
-    def get_zoom_level(self):
-        if self.zoom is None:
-            return 13
-        return self.zoom
 
     def get_lat_lng(self):
         if self.lat and self.lng:
