6e89cbb3c521963f68e79dda3bcc5f4cf1bf72e6
Gustavo Díaz Jaimes <togusav@linux-gds4.site>
2012-11-25 01:29:03 -0500

    optional clean_html from settings
    settings.CMS_CLEAN_HTML let you change the default html sanitizer

--- cms/utils/html.py
+++ cms/utils/html.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
+from django.conf import settings
 from html5lib import sanitizer, serializer, treebuilders, treewalkers
 import html5lib
 
 DEFAULT_PARSER = html5lib.HTMLParser(tokenizer=sanitizer.HTMLSanitizer,
                                      tree=treebuilders.getTreeBuilder("dom"))
 
-def clean_html(data, full=True, parser=DEFAULT_PARSER):
+def clean_html(data, **kwargs):
     """
     Cleans HTML from XSS vulnerabilities using html5lib
     
     If full is False, only the contents inside <body> will be returned (without
     the <body> tags).
     """
+    full = getattr(settings, 'full', True)
+    parser = getattr(kwargs, 'parser', DEFAULT_PARSER)
+    
     if full:
         dom_tree = parser.parse(data)
     else:
@@ -21,3 +25,5 @@ def clean_html(data, full=True, parser=DEFAULT_PARSER):
     s = serializer.htmlserializer.HTMLSerializer(omit_optional_tags=False,
                                                  quote_attr_values=True)
     return u''.join(s.serialize(stream))
+
+clean_html = getattr(settings, "CMS_CLEAN_HTML", clean_html)
