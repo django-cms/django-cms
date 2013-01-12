7e2a4b7c0c032f8c5f7fd2f1ac24538e3121d1a8
Nathaniel Tucker <me@ntucker.me>
2013-01-11 16:47:23 -0800

    Django 1.5 json compatibility

--- cms/templatetags/cms_js_tags.py
+++ cms/templatetags/cms_js_tags.py
@@ -1,15 +1,18 @@
 # -*- coding: utf-8 -*-
 from classytags.core import Tag, Options
+import django
 from django import template
-from django.core.serializers.json import DjangoJSONEncoder
+from django.core.serializers import json
 from django.utils import simplejson
 from django.utils.text import javascript_quote
+
+jsonparser = json.json if django.get_version() >= '1.5' else simplejson
 
 register = template.Library()
 
 @register.filter
 def js(value):
-    return simplejson.dumps(value, cls=DjangoJSONEncoder)
+    jsonparser.dumps(value, cls=json.DjangoJSONEncoder)
 
 @register.filter
 def bool(value):
