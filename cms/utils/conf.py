e5705258903b2dc637a9cbf2ad14c5a1924989a3
Jonas Obrist <ojiidotch@gmail.com>
2013-01-16 00:20:17 +0900

    Fixed cms.utils.i18n.get_languages failing due to missing default
    Also ensure that get_settings('LANGUAGES') has a useful 'default' key
    Added tests for CMS_LANGUAGES without a 'default' key

--- cms/utils/conf.py
+++ cms/utils/conf.py
@@ -1,10 +1,20 @@
 # -*- coding: utf-8 -*-
 from functools import update_wrapper
+import pprint
 import urlparse
 from cms import constants
+from cms.exceptions import CMSDeprecationWarning
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 from django.utils.translation import ugettext_lazy as _
 import os
+import warnings
+
+
+__all__ = ['get_setting']
+
+
+class VERIFIED: pass # need a unique identifier for CMS_LANGUAGES
 
 
 def default(name):
@@ -67,14 +77,117 @@ def get_templates():
         templates.append((constants.TEMPLATE_INHERITANCE_MAGIC, _('Inherit the template of the nearest ancestor')))
     return templates
 
-def get_languages():
-    languages = getattr(settings, 'CMS_LANGUAGES', None)
-    if languages:
-        return languages
+
+def _ensure_languages_settings_new(languages):
+    valid_language_keys = ['code', 'name', 'fallbacks', 'hide_untranslated', 'redirect_on_fallback', 'public']
+    required_language_keys = ['code', 'name']
+    simple_defaults = ['public', 'redirect_on_fallback', 'hide_untranslated']
+
+    defaults = languages.pop('default', {})
+    default_fallbacks = defaults.get('fallbacks')
+    needs_fallbacks = []
+
+    for key in defaults:
+        if key not in valid_language_keys:
+            raise ImproperlyConfigured("CMS_LANGUAGES has an invalid property in the default properties: s" % key)
+
+    for key in simple_defaults:
+        if key not in defaults:
+            defaults[key] = True
+
+    for site, language_list in languages.items():
+        if not isinstance(site, int):
+            raise ImproperlyConfigured(
+                "CMS_LANGUAGES can only be filled with integers (site IDs) and 'default'"
+                " for default values. %s is not a valid key." % site)
+
+        for language_object in language_list:
+            for required_key in required_language_keys:
+                if required_key not in language_object:
+                    raise ImproperlyConfigured("CMS_LANGUAGES has a language which is missing the required key %r "
+                        "in site %r" % (key, site))
+            language_code = language_object['code']
+            for key in language_object:
+                if key not in valid_language_keys:
+                    raise ImproperlyConfigured(
+                        "CMS_LANGUAGES has invalid key %r in language %r in site %r" % (key, language_code, site)
+                    )
+
+            if 'fallbacks' not in language_object:
+                if default_fallbacks:
+                    language_object['fallbacks'] = default_fallbacks
+                else:
+                    needs_fallbacks.append((site, language_object))
+            for key in simple_defaults:
+                if key not in language_object:
+                    language_object[key] = defaults[key]
+
+    site_fallbacks = {}
+    for site, language_object in needs_fallbacks:
+        if site not in site_fallbacks:
+            site_fallbacks[site] = [lang['code'] for lang in languages[site] if lang['public']]
+        language_object['fallbacks'] = [lang_code for lang_code in site_fallbacks[site] if lang_code != language_object['code']]
+
+    languages['default'] = defaults
+
+    return languages
+
+def _get_old_language_conf(code, name, template):
+    language = template.copy()
+    language['code'] = code
+    language['name'] = name
+    default_fallbacks = dict(settings.CMS_LANGUAGES).keys()
+    if hasattr(settings, 'CMS_LANGUAGE_FALLBACK'):
+        if settings.CMS_LANGUAGE_FALLBACK:
+            if hasattr(settings, 'CMS_LANGUAGE_CONF'):
+                language['fallbacks'] = settings.CMS_LANGUAGE_CONF.get(code, default_fallbacks)
+            else:
+                language['fallbacks'] = default_fallbacks
+        else:
+            language['fallbacks'] = []
     else:
-        return {
-            settings.SITE_ID: [{'code': code, 'name': _(name)} for code, name in settings.LANGUAGES]
-        }
+        if hasattr(settings, 'CMS_LANGUAGE_CONF'):
+            language['fallbacks'] = settings.CMS_LANGUAGE_CONF.get(code, default_fallbacks)
+        else:
+            language['fallbacks'] = default_fallbacks
+    if hasattr(settings, 'CMS_FRONTEND_LANGUAGES'):
+        language['public'] = code in settings.CMS_FRONTEND_LANGUAGES
+    return language
+
+def _translate_legacy_languages_settings(languages):
+    new_languages = {}
+    lang_template = {'fallbacks': [], 'public': True, 'redirect_on_fallback': True,
+                     'hide_untranslated': getattr(settings, 'CMS_HIDE_UNTRANSLATED', False)}
+    codes = dict(languages)
+    for site, site_languages in getattr(settings, 'CMS_SITE_LANGUAGES', {1: languages}).items():
+        new_languages[site] = []
+        for site_language in site_languages:
+            if site_language in codes:
+                new_languages[site].append(_get_old_language_conf(site_language, codes[site_language], lang_template))
+
+    pp = pprint.PrettyPrinter(indent=4)
+    warnings.warn("CMS_LANGUAGES has changed in django-cms 2.4\n"
+        "You may replace CMS_LANGUAGES with the following:\n%s" % pp.pformat(settings.CMS_LANGUAGES),
+        CMSDeprecationWarning)
+    new_languages['default'] = lang_template.copy()
+    return new_languages
+
+def _ensure_languages_settings(languages):
+    if isinstance(languages, dict):
+        verified_languages = _ensure_languages_settings_new(languages)
+    else:
+        verified_languages = _translate_legacy_languages_settings(languages)
+    verified_languages[VERIFIED] = True # this will be busted by SettingsOverride and cause a re-check
+    return verified_languages
+
+def get_languages():
+    languages = getattr(settings, 'CMS_LANGUAGES', {
+        settings.SITE_ID: [{'code': code, 'name': _(name)} for code, name in settings.LANGUAGES]
+    })
+    if VERIFIED in languages:
+        return languages
+    _ensure_languages_settings(languages)
+    return languages
 
 COMPLEX = {
     'CACHE_DURATIONS': get_cache_durations,
