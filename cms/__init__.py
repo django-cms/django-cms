b402b1ed96d8544b9e67c15981678e13a36a9702
Nathaniel Tucker <me@ntucker.me>
2013-01-15 23:54:29 -0800

    Setup shouldn't require DJANGO_SETTINGS_MODULE to be configured

--- cms/__init__.py
+++ cms/__init__.py
@@ -1,3 +1,25 @@
 # -*- coding: utf-8 -*-
 __version__ = '2.4.0.beta'
 
+# patch settings
+try:
+    from django.core.exceptions import ImproperlyConfigured
+    try:
+        from django.conf import settings
+        if 'cms' in settings.INSTALLED_APPS:
+            from conf import patch_settings
+            patch_settings()
+    except ImproperlyConfigured:
+        """
+        This exception means that either the application is being built, or is
+        otherwise installed improperly. Both make running patch_settings
+        irrelevant.
+        """
+        pass
+except ImportError:  # pragma: no cover
+    """
+    This exception means that either the application is being built, or is
+    otherwise installed improperly. Both make running patch_settings
+    irrelevant.
+    """
+    pass
