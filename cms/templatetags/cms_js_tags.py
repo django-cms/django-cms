15bf8e190611e0f8d918224dcc2651874ac3029c
Nathaniel Tucker <me@ntucker.me>
2013-03-26 09:24:20 -0700

    Fix template tag, assure proper version comparison

--- cms/templatetags/cms_js_tags.py
+++ cms/templatetags/cms_js_tags.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from distutils.version import LooseVersion
+
 from classytags.core import Tag, Options
 import django
 from django import template
@@ -6,13 +8,13 @@ from django.core.serializers import json
 from django.utils import simplejson
 from django.utils.text import javascript_quote
 
-jsonparser = json.json if django.get_version() >= '1.5' else simplejson
+jsonparser = json.json if LooseVersion(django.get_version()) >= LooseVersion('1.5') else simplejson
 
 register = template.Library()
 
 @register.filter
 def js(value):
-    jsonparser.dumps(value, cls=json.DjangoJSONEncoder)
+    return jsonparser.dumps(value, cls=json.DjangoJSONEncoder)
 
 @register.filter
 def bool(value):
