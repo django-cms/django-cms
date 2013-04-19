4e17b800e376d51ea331a3738da4ca60ac511af1
Patrick Lauber <digi@treepy.com>
2013-04-19 10:28:12 +0200

    clears url caches

--- cms/tests/no_i18n.py
+++ cms/tests/no_i18n.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.core.urlresolvers import clear_url_caches
+from cms.test_utils.util.context_managers import SettingsOverride
 from django.template import Template
 from cms.api import create_page
 from cms.test_utils.testcases import SettingsOverrideTestCase
@@ -10,6 +12,7 @@ class TestNoI18N(SettingsOverrideTestCase):
         'LANGUAGES': None,
         'CMS_LANGUAGES': None,
         'USE_I18N': False,
+        'ROOT_URLCONF': 'cms.test_utils.project.urls_no18n',
         'MIDDLEWARE_CLASSES': [
             'django.contrib.sessions.middleware.SessionMiddleware',
             'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -39,14 +42,18 @@ class TestNoI18N(SettingsOverrideTestCase):
         ],
     }
 
+    def setUp(self):
+        clear_url_caches()
+        super(TestNoI18N, self).setUp()
+
     def test_language_chooser(self):
         # test simple language chooser with default args
-        page1 = create_page("home", template="col_two.html", language="en-us",published=True)
+        page1 = create_page("home", template="col_two.html", language="en-us", published=True)
         context = self.get_context(path="/")
         del context['request'].LANGUAGE_CODE
         tpl = Template("{% load menu_tags %}{% language_chooser %}")
         tpl.render(context)
-        self.assertEqual(len(context['languages']),1)
+        self.assertEqual(len(context['languages']), 1)
         # try a different template and some different args
         tpl = Template("{% load menu_tags %}{% language_chooser 'menu/test_language_chooser.html' %}")
         tpl.render(context)
@@ -58,10 +65,12 @@ class TestNoI18N(SettingsOverrideTestCase):
             self.assertEqual(*lang)
 
     def test_page_language_url(self):
-        page1 = create_page("home", template="col_two.html", language="en-us",published=True)
-        path = "/"
-        context = self.get_context(path=path)
-        del context['request'].LANGUAGE_CODE
-        tpl = Template("{%% load menu_tags %%}{%% page_language_url '%s' %%}" % "en-us")
-        url = tpl.render(context)
-        self.assertEqual(url, "%s" % path)
+        with SettingsOverride(ROOT_URLCONF='cms.test_utils.project.urls_no18n'):
+            page1 = create_page("home", template="col_two.html", language="en-us", published=True)
+            path = "/"
+            context = self.get_context(path=path)
+            del context['request'].LANGUAGE_CODE
+            context['request'].urlconf = "cms.test_utils.project.urls_no18n"
+            tpl = Template("{%% load menu_tags %%}{%% page_language_url '%s' %%}" % "en-us")
+            url = tpl.render(context)
+            self.assertEqual(url, "%s" % path)
