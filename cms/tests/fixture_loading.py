e91c3b4e7c04d494feacdd3cf80cfe2b37adce65
Iacopo Spalletti <i.spalletti@nephila.it>
2013-05-21 10:15:35 +0200

    Different testing strategies to overcome the problems of json fixtures in tests

--- cms/tests/fixture_loading.py
+++ cms/tests/fixture_loading.py
@@ -1,18 +1,39 @@
 # -*- coding: utf-8 -*-
-import os
-from django.test import TestCase
+import tempfile
+import codecs
+
+try:
+    from cStringIO import StringIO
+except:
+    from io import StringIO
+
 from django.core.management import call_command
 
-from cms.models.pagemodel import Page
+from cms.test_utils.fixtures.navextenders import NavextendersFixture
+from cms.test_utils.testcases import SettingsOverrideTestCase
+from cms.models import Page
 
 
-class FixtureTestCase(TestCase):
+class FixtureTestCase(NavextendersFixture, SettingsOverrideTestCase):
+    settings_overrides = {
+        'USE_TZ': False
+    }
 
-    #fixtures = ['cms.json',]
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def test_fixture_load(self):
+        output = StringIO()
+        dump = tempfile.mkstemp(".json")
+        call_command('dumpdata', 'cms', indent=3, stdout=output)
+        Page.objects.all().delete()
+        output.seek(0)
+        with codecs.open(dump[1], 'w', 'utf-8') as dumpfile:
+            dumpfile.write(output.read())
+
         self.assertEqual(0, Page.objects.count())
-        call_command('loaddata', 'cms.json')
-        self.assertEqual(3, Page.objects.count())
+        call_command('loaddata', dump[1], commit=False, stdout=output)
+        self.assertEqual(10, Page.objects.count())
