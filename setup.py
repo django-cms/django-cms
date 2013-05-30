b1c2c9057a968152eda0355c4a99ddadcfb71848
Patrick Lauber <digi@treepy.com>
2013-05-30 11:18:12 +0200

    fixed version extraction

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_version(package):
     Return package version as listed in `__version__` in `init.py`.
     """
     init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search("__version__\s*=\s*(.*)", init_py).group(1)
+    return re.search("__version__\s*=\s*(.*)", init_py).group(1)[1:-1]
 
 
 version = get_version('cms')
