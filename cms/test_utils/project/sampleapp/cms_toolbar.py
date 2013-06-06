d5c5a94ff13033f00542245b9ea3c687a5296bbb
Jonas Obrist <ojiidotch@gmail.com>
2013-06-07 03:21:46 +0900

    some tests still fail, but mostly works

--- cms/test_utils/project/sampleapp/cms_toolbar.py
+++ cms/test_utils/project/sampleapp/cms_toolbar.py
@@ -8,6 +8,6 @@ from django.utils.translation import ugettext_lazy as _
 def category_toolbar(toolbar, request, is_current_app, app_name):
     if not toolbar.is_staff:
         return
-    admin_menu = toolbar.get_menu('category', _('Category'))
-    admin_menu.add_sideframe_item(_('Categories'), url=reverse('admin:sampleapp_category_changelist'))
-    admin_menu.add_modal_item(_('Add Category'), url=reverse('admin:sampleapp_category_add'), close_on_url_change=True)
+    category_menu = toolbar.get_menu('category', _('Category'))
+    category_menu.add_sideframe_item(_('Categories'), url=reverse('admin:sampleapp_category_changelist'))
+    category_menu.add_modal_item(_('Add Category'), url=reverse('admin:sampleapp_category_add'), close_on_url_change=True)
