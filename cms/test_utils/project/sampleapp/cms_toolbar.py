cb21e64f608c316faf212c31ad997ab82ce80b61
Jonas Obrist <ojiidotch@gmail.com>
2013-06-07 03:10:17 +0900

    fixed sampleapp toolbar

--- cms/test_utils/project/sampleapp/cms_toolbar.py
+++ cms/test_utils/project/sampleapp/cms_toolbar.py
@@ -1,20 +1,13 @@
 # -*- coding: utf-8 -*-
-from cms.toolbar.items import List, Item
-from cms.toolbar_base import CMSToolbar
 from cms.toolbar_pool import toolbar_pool
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
-class CategoryToolbar(CMSToolbar):
-    def insert_items(self, items, toolbar, request, is_app):
-        if toolbar.is_staff:
-            items[0].items = [Item(reverse('admin:sampleapp_category_changelist'), _('Categories'),
-                                   load_side_frame=True)] + items[0].items
-            if is_app:
-                cat_list = List(reverse('admin:sampleapp_category_changelist'), _('Category'))
-                cat_list.items.append(Item(reverse('admin:sampleapp_category_add'), _("Add Category")))
-                items.append(cat_list)
-
-
-toolbar_pool.register(CategoryToolbar)
+@toolbar_pool.register
+def category_toolbar(toolbar, request, is_current_app, app_name):
+    if not toolbar.is_staff:
+        return
+    admin_menu = toolbar.get_menu('category', _('Category'))
+    admin_menu.add_sideframe_item(_('Categories'), url=reverse('admin:sampleapp_category_changelist'))
+    admin_menu.add_modal_item(_('Add Category'), url=reverse('admin:sampleapp_category_add'), close_on_url_change=True)
