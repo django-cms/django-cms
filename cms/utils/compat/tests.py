e6c55dac56884d7a420976e2763e37c15a4e4bb3
Jonas Obrist <ojiidotch@gmail.com>
2013-06-08 23:13:10 +0900

    another unittest compat fix

--- cms/utils/compat/tests.py
+++ cms/utils/compat/tests.py
@@ -2,7 +2,7 @@ from cms.utils.compat import PY2
 
 if PY2:
     class UnittestCompatMixin(object):
-        def assertSameElements(self, a, b, msg=None):
+        def assertListEqual(self, a, b, msg=None):
             return self.assertItemsEqual(a, b, msg)
 
         def assertNotRegex(self, text, regex, msg=None):
