bc9bbd3765d652735ecab13093e5e81e7e3fc8f3
Iacopo Spalletti <i.spalletti@nephila.it>
2013-07-19 20:09:43 +0200

    First implementation of frontend-editable models

--- cms/templatetags/placeholder_tags.py
+++ cms/templatetags/placeholder_tags.py
@@ -3,6 +3,8 @@ from classytags.arguments import Argument
 from classytags.core import Tag, Options
 from django import template
 from django.template.defaultfilters import safe
+from classytags.helpers import InclusionTag
+from django.core.urlresolvers import reverse
 
 register = template.Library()
 
@@ -24,3 +26,34 @@ class RenderPlaceholder(Tag):
             return ''
         return safe(placeholder.render(context, width, lang=language))
 register.tag(RenderPlaceholder)
+
+
+class CMSEditableObject(InclusionTag):
+    template = 'cms/toolbar/model_attribute_noedit.html'
+    edit_template = 'cms/toolbar/model_attribute_edit.html'
+    name = 'show_editable_model'
+    options = Options(
+        Argument('instance'),
+        Argument('attribute'),
+    )
+
+    def _is_editable(self, request):
+        return (request and hasattr(request, 'toolbar') and
+                request.toolbar.edit_mode)
+
+    def get_template(self, context, **kwargs):
+        if self._is_editable(context.get('request', None)):
+            return self.edit_template
+        return self.template
+
+    def get_context(self, context, instance, attribute):
+        context['item'] = getattr(instance, attribute, '')
+        context['instance'] = instance
+        context['opts'] = instance._meta
+        context['admin_url'] = reverse('admin:%s_%s_change' % (
+            instance._meta.app_label, instance._meta.module_name),
+                                       args=(instance.pk,))
+        return context
+
+
+register.tag(CMSEditableObject)
