a11ce0d82313792712b89cc7b1d9220c234e9364
Stefan Foulis <stefan@foulis.ch>
2013-07-31 13:24:52 +0200

    auto set extended_object on add

--- cms/extensions/admin.py
+++ cms/extensions/admin.py
@@ -1,13 +1,22 @@
+from cms.models import Page
 from django.contrib import admin
 
 
-class PageExtensionAdmin(admin.ModelAdmin):
+class ExtensionAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if not change:
+            extended_object_id = request.GET['extended_object']
+            obj.extended_object = Page.objects.get(pk=extended_object_id)
+        return super(ExtensionAdmin, self).save_model(request, obj, form, change)
+
+
+class PageExtensionAdmin(ExtensionAdmin):
 
     def queryset(self, request):
         return super(PageExtensionAdmin, self).queryset(request).filter(extended_object__publisher_is_draft=True)
 
 
-class TitleExtensionAdmin(admin.ModelAdmin):
+class TitleExtensionAdmin(ExtensionAdmin):
 
     def queryset(self, request):
         return super(TitleExtensionAdmin, self).queryset(request).filter(extended_object__page__publisher_is_draft=True)
