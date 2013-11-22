4abb9f11d15f6f39351c2803795f0a8347f64a7f
FinalAngel <angelo.dini@divio.ch>
2013-11-22 18:30:35 +0100

    cms.placeholder is now cms.plugins

--- cms/tests/toolbar.py
+++ cms/tests/toolbar.py
@@ -140,7 +140,7 @@ class ToolbarTests(ToolbarTestBase):
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'nav_playground.html')
         self.assertContains(response, '<div id="cms_toolbar"')
-        self.assertContains(response, 'cms.placeholders.js')
+        self.assertContains(response, 'cms.plugins.js')
         self.assertContains(response, 'cms.placeholders.css')
 
     def test_markup_generic_module(self):
