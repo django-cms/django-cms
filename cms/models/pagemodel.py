a48a5fffe3da4d916cbf08c94eb1cb60fe37dcb6
Iacopo Spalletti <i.spalletti@nephila.it>
2013-12-08 11:22:31 +0100

    Fix is_home default value

--- cms/models/pagemodel.py
+++ cms/models/pagemodel.py
@@ -74,7 +74,7 @@ class Page(with_metaclass(PageMetaClass, MPTTModel)):
     limit_visibility_in_menu = models.SmallIntegerField(_("menu visibility"), default=None, null=True, blank=True,
                                                         choices=LIMIT_VISIBILITY_IN_MENU_CHOICES, db_index=True,
                                                         help_text=_("limit when this page is visible in the menu"))
-    is_home = models.BooleanField(editable=False, db_index=True)
+    is_home = models.BooleanField(editable=False, db_index=True, default=False)
     application_urls = models.CharField(_('application'), max_length=200, blank=True, null=True, db_index=True)
     application_namespace = models.CharField(_('application namespace'), max_length=200, blank=True, null=True)
     level = models.PositiveIntegerField(db_index=True, editable=False)
