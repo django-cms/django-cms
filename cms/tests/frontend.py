49726599d0e9eb8cdf2fb7e325cbb24ec42c62bd
Patrick Lauber <digi@treepy.com>
2013-12-12 17:04:07 +0100

    fixed on other server string issue

--- cms/tests/frontend.py
+++ cms/tests/frontend.py
@@ -26,7 +26,7 @@ class CMSLiveTests(LiveServerTestCase):
             access_key = os.environ.get("SAUCE_ACCESS_KEY")
             capabilities["tunnel-identifier"] = [os.environ.get("TRAVIS_JOB_NUMBER")]
             hub_url = "%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key)
-            cls.driver = webdriver.Remote(desired_capabilities=capabilities, command_executor="http://%s/wd/hub" % hub_url)
+            cls.driver = webdriver.Remote(desired_capabilities=capabilities, command_executor=hub_url)
             cls.driver.implicitly_wait(30)
         else:
             cls.driver = webdriver.Firefox()
