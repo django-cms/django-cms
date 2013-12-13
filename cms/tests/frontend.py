aa89a56c1858d2ea5029d15e4c6f7d1789924d91
Patrick Lauber <digi@treepy.com>
2013-12-13 13:16:54 +0100

    not in an array

--- cms/tests/frontend.py
+++ cms/tests/frontend.py
@@ -26,7 +26,7 @@ class CMSLiveTests(LiveServerTestCase):
             capabilities['tags'] = [os.environ.get("TRAVIS_PYTHON_VERSION"), "CI"]
             username = os.environ.get("SAUCE_USERNAME")
             access_key = os.environ.get("SAUCE_ACCESS_KEY")
-            capabilities["tunnel-identifier"] = [os.environ.get("TRAVIS_JOB_NUMBER")]
+            capabilities["tunnel-identifier"] = os.environ.get("TRAVIS_JOB_NUMBER")
             hub_url = "http://%s:%s@ondemand.saucelabs.com/wd/hub" % (username, access_key)
             cls.driver = webdriver.Remote(desired_capabilities=capabilities, command_executor=hub_url)
             cls.driver.implicitly_wait(30)
