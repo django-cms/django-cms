e10b8382e3f14a6efee1a3157ea13bbeec1deee1
Patrick Lauber <digi@treepy.com>
2013-12-18 12:00:33 +0100

    fixed copy lang for static placeholder

--- cms/management/commands/subcommands/copy_lang.py
+++ cms/management/commands/subcommands/copy_lang.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
-from optparse import make_option
-
 from django.conf import settings
 
 from django.core.management.base import BaseCommand, CommandError
 
 from cms.api import copy_plugins_to_language
-from cms.models import Title, Page
-from cms.stacks.models import Stack
+from cms.models import Title, Page, StaticPlaceholder
 from cms.utils.copy_plugins import copy_plugins_to
 from cms.utils.i18n import get_language_list
 
@@ -62,16 +59,16 @@ class CopyLangCommand(BaseCommand):
                 if verbose:
                     self.stdout.write('Skipping page %s, language %s not defined\n' % (page, from_lang))
 
-        for stack in Stack.objects.all():
+        for static_placeholder in StaticPlaceholder.objects.all():
             plugin_list = []
-            for plugin in stack.draft.get_plugins():
+            for plugin in static_placeholder.draft.get_plugins():
                 if plugin.language == from_lang:
                     plugin_list.append(plugin)
 
             if plugin_list:
                 if verbose:
-                    self.stdout.write("copying plugins from stack '%s' in '%s' to '%s'\n" % (stack.name, from_lang,
+                    self.stdout.write("copying plugins from static_placeholder '%s' in '%s' to '%s'\n" % (static_placeholder.name, from_lang,
                                                                                              to_lang))
-                copy_plugins_to(plugin_list, stack.draft, to_lang)
+                copy_plugins_to(plugin_list, static_placeholder.draft, to_lang)
 
         self.stdout.write(u"all done")
