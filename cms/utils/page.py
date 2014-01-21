64b55b6c2606992fdf5aa6931afadf09f3fe0dde
Iacopo Spalletti <i.spalletti@nephila.it>
2014-01-21 14:35:48 +0100

    Remove filter on PUBLISHER_STATE_DELETE

--- cms/utils/page.py
+++ cms/utils/page.py
@@ -11,20 +11,14 @@ def is_valid_page_slug(page, parent, lang, slug, site, path=None):
     """Validates given slug depending on settings.
     """
     from cms.models import Title
-    # Exclude the page with the publisher_state == page.PUBLISHER_STATE_DELETE
     # Since 3.0 this must take into account unpublished pages as it's necessary
     # to be able to open every page to edit content.
     # If page is newly created (i.e. page.pk is None) we skip filtering out
     # titles attached to the same page
     if page.pk:
-        qs = Title.objects.filter(page__site=site).exclude(
-            Q(page=page) |
-            Q(page__publisher_state=page.PUBLISHER_STATE_DELETE)
-        )
+        qs = Title.objects.filter(page__site=site).exclude(page=page)
     else:
-        qs = Title.objects.filter(page__site=site).exclude(
-            Q(page__publisher_state=page.PUBLISHER_STATE_DELETE)
-        )
+        qs = Title.objects.filter(page__site=site)
 
     if settings.USE_I18N:
         qs = qs.filter(language=lang)
