49ea69ea7825315c1036477b7e5452a2fb2b8ab0
Martin Koistinen <mkoistinen@gmail.com>
2014-01-31 18:05:55 +0200

    Fix active sub-menus which didn't disappear when hover changes
    Signed-off-by: Martin Koistinen <mkoistinen@gmail.com>

--- cms/static/cms/js/plugins/cms.toolbar.js
+++ cms/static/cms/js/plugins/cms.toolbar.js
@@ -168,6 +168,9 @@ $(document).ready(function () {
 					} else {
 						lists.find('ul ul').hide();
 					}
+
+					// Remove stale submenus
+					el.siblings().find('> ul').hide();
 				});
 
 				// fix leave event
