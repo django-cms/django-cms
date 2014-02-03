44e8c367872b9f9a0cf3189c064fff6128db9d28
Patrick Lauber <digi@treepy.com>
2014-02-03 11:50:37 +0100

    deprecation warning changed

--- cms/plugin_pool.py
+++ cms/plugin_pool.py
@@ -99,7 +99,7 @@ class PluginPool(object):
                         splitted = table_name.split(splitter, 1)
                         table_name = 'cmsplugin_%s' % splitted[1]
                         model._meta.db_table = table_name
-                        warnings.warn('please rename the table "%s" to "%s" in %s\nThe compatibility code will be removed in 3.2' % (table_name, old_db_name, model._meta.app_label), DeprecationWarning)
+                        warnings.warn('please rename the table "%s" to "%s" in %s\nThe compatibility code will be removed in 3.1' % (table_name, old_db_name, model._meta.app_label), DeprecationWarning)
                 for att_name in model.__dict__.keys():
                     att = model.__dict__[att_name]
                     if isinstance(att, ManyToManyField):
@@ -110,7 +110,7 @@ class PluginPool(object):
                             table_name.split(splitter, 1)
                             table_name = 'cmsplugin_%s' % splitted[1]
                             att.rel.through._meta.db_table = table_name
-                            warnings.warn('please rename the table "%s" to "%s" in %s\nThe compatibility code will be removed in 3.2' % (table_name, old_db_name, model._meta.app_label), DeprecationWarning)
+                            warnings.warn('please rename the table "%s" to "%s" in %s\nThe compatibility code will be removed in 3.1' % (table_name, old_db_name, model._meta.app_label), DeprecationWarning)
         self.patched = True
 
     def get_all_plugins(self, placeholder=None, page=None, setting_key="plugins", include_page_only=True):
