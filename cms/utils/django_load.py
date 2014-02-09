02912acb178657d2755034ab775f28cb946a60ff
Hamish Downer <hamish@foobacca.co.uk>
2014-02-09 12:59:09 +0000

    don't swallow ImportError due to importing app that exists
    Previously get_module() would try swallow the ImportError, but there are
    two causes for the ImportError - the module file doesn't exists, or
    importing the module file causes an ImportError.  We want to swallow the
    first but not the second.
    
    This commit uses the imp module - imp.find_module() looks for the module
    and will cause an ImportError if the module does not exist.  But it will
    not actually run the file code.  So the try/except block is put around
    imp.find_module().  If there was no ImportError at that point, we then
    to load_module() outside the try/except block, and if that fails we die
    here immediately.

--- cms/utils/django_load.py
+++ cms/utils/django_load.py
@@ -9,7 +9,9 @@ documentet clearly within this file with comments.
 For documentation on how to use the functions described in this file, please
 refer to http://django-load.readthedocs.org/en/latest/index.html.
 """
+import imp
 import traceback # changed
+
 from django.conf import settings
 from django.utils.importlib import import_module
 
@@ -18,39 +20,48 @@ def get_module(app, modname, verbose, failfast):
     Internal function to load a module from a single app.
     """
     module_name = '%s.%s' % (app, modname)
+    # the module *should* exist - raise an error if it doesn't
+    module_info = imp.find_module(modname)
+    module = imp.load_module(modname, module_info)
     try:
-        module = import_module(module_name)
+        imp.find_module(app, module.__path__)
     except ImportError:
+        # this ImportError will be due to the module not existing
+        # so here we can silently ignore it.  But an ImportError
+        # when we import_module() should not be ignored
         if failfast:
             raise
         elif verbose:
-            print(u"Could not load %r from %r" % (modname, app)) # changed
+            print(u"Could not find %r from %r" % (modname, app)) # changed
             traceback.print_exc() # changed
         return None
+
+    module = import_module(module_name)
+
     if verbose:
         print(u"Loaded %r from %r" % (modname, app))
     return module
-        
+
 
 def load(modname, verbose=False, failfast=False):
     """
     Loads all modules with name 'modname' from all installed apps.
-    
+
     If verbose is True, debug information will be printed to stdout.
-    
+
     If failfast is True, import errors will not be surpressed.
     """
     for app in settings.INSTALLED_APPS:
         get_module(app, modname, verbose, failfast)
-        
+
 
 def iterload(modname, verbose=False, failfast=False):
     """
     Loads all modules with name 'modname' from all installed apps and returns
     and iterator of those modules.
-    
+
     If verbose is True, debug information will be printed to stdout.
-    
+
     If failfast is True, import errors will not be surpressed.
     """
     for app in settings.INSTALLED_APPS:
@@ -62,15 +73,15 @@ def load_object(import_path):
     """
     Loads an object from an 'import_path', like in MIDDLEWARE_CLASSES and the
     likes.
-    
+
     Import paths should be: "mypackage.mymodule.MyObject". It then imports the
     module up until the last dot and tries to get the attribute after that dot
     from the imported module.
-    
+
     If the import path does not contain any dots, a TypeError is raised.
-    
+
     If the module cannot be imported, an ImportError is raised.
-    
+
     If the attribute does not exist in the module, a AttributeError is raised.
     """
     if '.' not in import_path:
