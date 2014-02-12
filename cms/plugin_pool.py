46b2ff3c414667cb196f5585e367fcefdef40b0f
Patrick Lauber <digi@treepy.com>
2014-02-12 18:16:47 +0100

    check property

--- cms/plugin_pool.py
+++ cms/plugin_pool.py
@@ -44,8 +44,8 @@ class PluginPool(object):
                 "CMS Plugins must be subclasses of CMSPluginBase, %r is not."
                 % plugin
             )
-        if plugin.render_plugin:
-            if not plugin.render_template and not hasattr(plugin.model, 'render_template'):
+        if plugin.render_plugin and not type(plugin.render_plugin) == property or hasattr(plugin.model, 'render_template'):
+            if plugin.render_template is None and not hasattr(plugin.model, 'render_template'):
                 raise ImproperlyConfigured(
                     "CMS Plugins must define a render template or set render_plugin=False: %s"
                     % plugin
