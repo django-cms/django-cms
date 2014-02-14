0b2a89ef6da2336d90964126ed5a75f15a8e3b9d
Patrick Lauber <digi@treepy.com>
2014-02-14 10:32:57 +0100

    fixes #1738

--- cms/admin/placeholderadmin.py
+++ cms/admin/placeholderadmin.py
@@ -212,7 +212,7 @@ class PlaceholderAdmin(ModelAdmin):
         placeholder_id = request.POST.get('placeholder_id', None)
         parent_id = request.POST.get('parent_id', None)
         if parent_id:
-            warnings.warn("parent_id is deprecated and will be removed in 3.0.1, use plugin_parent instead",
+            warnings.warn("parent_id is deprecated and will be removed in 3.1, use plugin_parent instead",
                           DeprecationWarning)
         if not parent_id:
             parent_id = request.POST.get('plugin_parent', None)
