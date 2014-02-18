196724f98aefc6da90988bd264d7dc566db1239c
Iacopo Spalletti <i.spalletti@nephila.it>
2014-02-18 17:21:55 +0100

    Add PAGE_TEMPLATE variable to context

--- cms/context_processors.py
+++ cms/context_processors.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
 from cms.utils.conf import get_cms_setting
+import warnings
 
-def media(request):
+
+def cms_settings(request):
     """
     Adds media-related context variables to the context.
     """
-    return {'CMS_MEDIA_URL': get_cms_setting('MEDIA_URL')}
+    try:
+        template = request.current_page.get_template()
+    except AttributeError:
+        template = ''
+    return {
+        'CMS_MEDIA_URL': get_cms_setting('MEDIA_URL'),
+        'PAGE_TEMPLATE': template,
+    }
+
+
+def media(request):
+    warnings.warn('cms.context_processors.media has been deprecated in favor of'
+                  'cms.context_processors.cms_settings. Please update your'
+                  'configuration', DeprecationWarning)
+    return cms_settings(request)
--- cms/test_utils/cli.py
+++ cms/test_utils/cli.py
@@ -49,7 +49,7 @@ def configure(db_url, **extra):
             "django.core.context_processors.request",
             "django.core.context_processors.media",
             'django.core.context_processors.csrf',
-            "cms.context_processors.media",
+            "cms.context_processors.cms_settings",
             "sekizai.context_processors.sekizai",
             "django.core.context_processors.static",
         ],
