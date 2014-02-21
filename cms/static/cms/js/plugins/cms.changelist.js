1a4fda912f11009de3ad8ae0de2b0e78b231ea06
FinalAngel <angelo.dini@divio.ch>
2014-02-21 18:26:52 +0100

    tree tooltip change to be more responsive and awesome. Fixes #2678 and #2677

--- cms/static/cms/js/plugins/cms.changelist.js
+++ cms/static/cms/js/plugins/cms.changelist.js
@@ -103,7 +103,64 @@ $(document).ready(function () {
 	// TODO this will be refactored in 3.1
 
 	// ADD DIRECT PUBLISHING
-	$('.publish-trigger').bind('click', function (e) {
+	var tree = $('.tree');
+	var langTrigger = '.col-language .trigger-tooltip span';
+	var langTooltips = '.language-tooltip';
+	var langTimer = function () {};
+	var langDelay = 100;
+	var langFadeDuration = 200;
+
+	// show the tooltip
+	tree.delegate(langTrigger, 'mouseenter', function () {
+		var el = $(this).closest('.col-language').find('.language-tooltip');
+		var anchors = el.find('a');
+		var span = $(this);
+
+		// clear timer
+		clearTimeout(langTimer);
+
+		// cancel if tooltip already visible
+		if(el.is(':visible')) return false;
+
+		// set correct position
+		el.css('right', 20 + $(this).position().left);
+
+		// figure out what should be shown
+		anchors.hide();
+		if(span.hasClass('unpublished') || span.hasClass('unpublishedparent')) anchors.eq(1).show();
+		if(span.hasClass('published')) anchors.eq(0).show();
+		if(span.hasClass('dirty')) anchors.show().parent().addClass('language-tooltip-multiple');
+
+		// hide all elements
+		$(langTooltips).fadeOut(langDelay);
+
+		// use a timeout to display the tooltip
+		langTimer = setTimeout(function () {
+			el.stop(true, true).fadeIn(langFadeDuration);
+		}, langDelay);
+	});
+	// hide the tooltip when leaving the area
+	tree.delegate(langTrigger, 'mouseleave', function () {
+		// clear timer
+		clearTimeout(langTimer);
+		// hide all elements
+		langTimer = setTimeout(function () {
+			$(langTooltips).fadeOut(langFadeDuration);
+		}, langDelay * 2);
+	});
+	// reset hiding when entering the tooltip itself
+	tree.delegate(langTooltips, 'mouseover', function () {
+		// clear timer
+		clearTimeout(langTimer);
+	});
+	tree.delegate(langTooltips, 'mouseleave', function () {
+		// hide all elements
+		langTimer = setTimeout(function () {
+			$(langTooltips).fadeOut(langFadeDuration);
+		}, langDelay * 2);
+	});
+	// attach double check event if publish or unpublish should be triggered
+	tree.delegate.bind('.language-tooltip a', 'click', function (e) {
 		e.preventDefault();
 
 		// cancel if not confirmed
@@ -111,22 +168,8 @@ $(document).ready(function () {
 
 		// publish page and update
 		$.get($(this).attr('href'), function () {
-			var el = $(e.currentTarget);
-			el.closest('.col-language').find('a > span').attr('class', el.data('class'));
 			window.location.reload();
 		});
-	});
-	// ensure seamless transition when hovering an element down
-	$('.ld-switch').on('mouseenter', function () {
-		$(this).closest('.language-dropdown').hide();
-	});
-	// still make the dot clickable using a transparent layer
-	$('.ld-preview').on('click', function () {
-		window.top.location.href = $(this).parent().find('a').eq(0).attr('href');
-	});
-	// resets display:none style added using javascript
-	$('.col-language a').on('mouseenter', function () {
-		$('.language-dropdown').removeAttr('style');
 	});
 
 	// TAB CLICK
