4f8bb7bbe25031b26eb71e6b933896552d063b59
Patrick Lauber <digi@treepy.com>
2014-02-26 10:40:34 +0100

    fix no i18n

--- cms/toolbar/toolbar.py
+++ cms/toolbar/toolbar.py
@@ -62,7 +62,8 @@ class CMSToolbar(ToolbarAPIMixin):
                 placeholder.save()
                 user_settings.clipboard = placeholder
                 user_settings.save()
-            if user_settings.language in dict(settings.LANGUAGES):
+            if (settings.USE_I18N and user_settings.language in dict(settings.LANGUAGES)) or (
+                    not settings.USE_I18N and user_settings.language == settings.LANGUAGE_CODE):
                 self.toolbar_language = user_settings.language
             else:
                 user_settings.language = self.language
