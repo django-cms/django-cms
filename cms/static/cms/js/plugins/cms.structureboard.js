748e1f6fff706eaa2d0a2e5ee0f47040c2bf9b6f
FinalAngel <angelo.dini@divio.ch>
2014-02-28 18:37:21 +0100

    fixes issue #1308

--- cms/static/cms/js/plugins/cms.structureboard.js
+++ cms/static/cms/js/plugins/cms.structureboard.js
@@ -51,6 +51,9 @@ $(document).ready(function () {
 		_setup: function () {
 			var that = this;
 
+			// cancel if there are no dragareas
+			if(!this.dragareas.length) return false;
+
 			// setup toolbar mode
 			if(this.settings.mode === 'structure') setTimeout(function () { that.show(true); }, 100);
 
@@ -160,6 +163,8 @@ $(document).ready(function () {
 				id = cls.replace('cms_placeholder-', '');
 			} else if(el.hasClass('cms_dragbar')) {
 				id = cls.replace('cms_dragbar-', '');
+			} else if(el.hasClass('cms_dragarea')) {
+				id = cls.replace('cms_dragarea-', '');
 			}
 
 			return id;
@@ -416,7 +421,8 @@ $(document).ready(function () {
 					if(original.data('settings') === null) return false;
 					var type = original.data('settings').plugin_type;
 					// prepare variables for bound
-					var holder = placeholder.parent().prevAll('.cms_dragarea').first();
+					var holderId = that.getId(placeholder.closest('.cms_dragarea'));
+					var holder = $('.cms_placeholder-' + holderId);
 					var plugin = $('.cms_plugin-' + that.getId(placeholder.closest('.cms_draggable')));
 
 					// now set the correct bounds
