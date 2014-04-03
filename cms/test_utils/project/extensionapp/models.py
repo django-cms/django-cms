a0f030cd03d28d97924a3277722d7a51cf3a3e92
Martin Koistinen <mkoistinen@gmail.com>
2014-04-03 19:12:07 +0300

    Update extension app to include a M2M

--- cms/test_utils/project/extensionapp/models.py
+++ cms/test_utils/project/extensionapp/models.py
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
 from cms.extensions import PageExtension, TitleExtension
 from cms.extensions.extension_pool import  extension_pool
+from django.contrib.auth.models import User
 from django.db import models
 
 class MyPageExtension(PageExtension):
     extra = models.CharField(blank=True, default='', max_length=255)
+    favorite_users = models.ManyToManyField(User, blank=True, null=True)
 
+    def copy_relations(self, other, language):
+        for favorite_user in other.favorite_users.all():
+            favorite_user.pk = None
+            favorite_user.mypageextension = self
+            favorite_user.save()
 
 extension_pool.register(MyPageExtension)
 
 
 class MyTitleExtension(TitleExtension):
     extra_title = models.CharField(blank=True, default='', max_length=255)
-
 
 extension_pool.register(MyTitleExtension)
