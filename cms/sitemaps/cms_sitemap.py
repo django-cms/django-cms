e733b0d5192437a95c4eafd1babc02385fb4fcf7
Martin Koistinen <mkoistinen@gmail.com>
2014-04-12 20:37:34 +0300

    Remove redirected pages from the sitemap

--- cms/sitemaps/cms_sitemap.py
+++ cms/sitemaps/cms_sitemap.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
+
 from django.contrib.sitemaps import Sitemap
+from django.db.models import Q
 from django.utils import translation
+
 from cms.models import Title
 
 
@@ -18,7 +21,20 @@ class CMSSitemap(Sitemap):
     priority = 0.5
 
     def items(self):
-        all_titles = Title.objects.public().filter(page__login_required=False)
+        #
+        # It is counter-productive to provide entries for:
+        #   > Pages which redirect:
+        #       - If the page redirects to another page on this site, the
+        #         destination page will already be in the sitemap, and
+        #       - If the page redirects externally, then it shouldn't be
+        #         part of our sitemap anyway.
+        #   > Pages which cannot be accessed by anonymous users (like
+        #     search engines are).
+        #
+        all_titles = Title.objects.public().filter(
+            Q(redirect='') | Q(redirect__isnull=True),
+            page__login_required=False
+        )
         return all_titles
 
     def lastmod(self, title):
