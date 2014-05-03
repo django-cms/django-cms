fe6e254e35598bbe5fa25182e5d4b8260b0320a3
Iacopo Spalletti <i.spalletti@nephila.it>
2014-05-03 12:16:28 +0200

    Improve tests checking actual template loading and for badly placed templates directory

--- cms/tests/templates.py
+++ cms/tests/templates.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 import os.path
+try:
+    from configparser import ConfigParser
+except ImportError:
+    from ConfigParser import SafeConfigParser as ConfigParser
+
 from django.conf import settings
+from django.template import loader, TemplateDoesNotExist
 from django.test.utils import override_settings
 from django.utils.encoding import force_text
 from django.utils.translation import ugettext_lazy as _
@@ -9,6 +15,9 @@ from cms import constants
 from cms.test_utils.testcases import CMSTestCase
 from cms.utils import get_cms_setting
 
+PATH_PREFIX = os.path.join('inner_dir', 'custom_templates')
+GOOD_PATH = os.path.join(settings.PROJECT_PATH, 'project', 'templates', PATH_PREFIX)
+BAD_PATH = os.path.join(settings.PROJECT_PATH, 'project', 'custom_templates')
 
 class TemplatesConfig(CMSTestCase):
 
@@ -23,14 +32,17 @@ class TemplatesConfig(CMSTestCase):
         self.assertEqual(len(files), 6)
         self.assertEqual(set(files), set(original_files))
 
-    @override_settings(CMS_TEMPLATES_DIR="%s/custom_templates" % settings.PROJECT_PATH)
+    @override_settings(CMS_TEMPLATES_DIR=GOOD_PATH)
     def test_custom_templates(self):
         """
         Test that using CMS_TEMPLATES_DIR both template list and template labels are extracted from the new directory
         """
         template_dir = settings.CMS_TEMPLATES_DIR
-        original_labels = [force_text(_(label.split(':')[1].strip())) for label in open(os.path.join(template_dir, 'templates.conf')).readlines()]
-        original_files = [label.split(':')[0].strip() for label in open(os.path.join(template_dir, 'templates.conf')).readlines()]
+        config = ConfigParser()
+        config.read(os.path.join(template_dir, 'templates.ini'))
+        templates = [tpl for tpl in config.items('templates') if len(tpl) > 0]
+        original_labels = [force_text(_(template[1])) for template in templates]
+        original_files = [os.path.join(PATH_PREFIX, template[0]) for template in templates]
         templates = get_cms_setting('TEMPLATES')
         self.assertEqual(len(templates), 3)
         labels = [force_text(template[1]) for template in templates]
@@ -40,3 +52,26 @@ class TemplatesConfig(CMSTestCase):
             original_files.append(constants.TEMPLATE_INHERITANCE_MAGIC)
         self.assertEqual(set(labels), set(original_labels))
         self.assertEqual(set(files), set(original_files))
+
+    @override_settings(CMS_TEMPLATES_DIR=GOOD_PATH)
+    def test_custom_templates_loading(self):
+        """
+        Checking that templates can be loaded by the template loader
+        """
+        templates = get_cms_setting('TEMPLATES')
+        files = [template[0] for template in templates]
+        for template in templates:
+            if template[0] != constants.TEMPLATE_INHERITANCE_MAGIC:
+                tpl = loader.get_template(template[0])
+                self.assertTrue(tpl.name in files)
+
+    @override_settings(CMS_TEMPLATES_DIR=BAD_PATH)
+    def test_custom_templates_bad_dir(self):
+        """
+        Checking that templates can be loaded by the template loader
+        """
+        templates = get_cms_setting('TEMPLATES')
+        for template in templates:
+            if template[0] != constants.TEMPLATE_INHERITANCE_MAGIC:
+                with self.assertRaises(TemplateDoesNotExist):
+                    loader.get_template(template[0])
