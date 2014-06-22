5250e53e438a3312224b3039576df6787e2161ec
Iacopo Spalletti <i.spalletti@nephila.it>
2014-06-22 17:03:20 +0200

    Scan for submit buttons in iframe#content only

--- cms/static/cms/js/modules/cms.modal.js
+++ cms/static/cms/js/modules/cms.modal.js
@@ -432,7 +432,7 @@ $(document).ready(function () {
 			// if there are no given buttons within the submit-row area
 			// scan deeper within the form itself
 			if(!buttons.length) {
-				row = iframe.contents().find('form:eq(0)');
+				row = iframe.contents().find('#content form:eq(0)');
 				buttons = row.find('input[type="submit"], button[type="submit"]');
 				buttons.attr('name', '_save')
 					.addClass('deletelink')
