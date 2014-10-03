c7209e9a620ce477829a645dc67385e33e9caf6b
Patrick Lauber <digi@treepy.com>
2014-10-03 13:04:57 +0200

    fix imports

--- cms/test_utils/project/customuserapp/models.py
+++ cms/test_utils/project/customuserapp/models.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.core.mail import send_mail
 from django.db import models
+from django.utils.http import urlquote
+
 try:
     import re
     from django.contrib.auth.models import (AbstractBaseUser, PermissionsMixin,
