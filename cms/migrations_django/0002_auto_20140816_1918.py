3e76e454755f2d34e9da5802751edcce533719a8
John-Scott Atlakson <john.scott.atlakson@gmail.com>
2014-10-06 11:44:41 -0400

    Removed Django < 1.5 code path in Django 1.7+ migrations file
    Can be guaranteed that get_user_model will be available in a 1.7 migrations file
    Also renamed User._meta.module_name => User._meta.model_name since the former is deprecated in 1.7 and removed in Django master (1.8)

--- cms/migrations_django/0002_auto_20140816_1918.py
+++ cms/migrations_django/0002_auto_20140816_1918.py
@@ -6,14 +6,11 @@ import cms.models.static_placeholder
 import cms.models.fields
 from django.conf import settings
 import django.utils.timezone
-try:
-    from django.contrib.auth import get_user_model
-except ImportError: # django < 1.5
-    from django.contrib.auth.models import User
-else:
-    User = get_user_model()
-user_orm_label = '%s.%s' % (User._meta.app_label, User._meta.object_name)
-user_model_label = '%s.%s' % (User._meta.app_label, User._meta.module_name)
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+user_model_label = '%s.%s' % (User._meta.app_label, User._meta.model_name)
 user_ptr_name = '%s_ptr' % User._meta.object_name.lower()
 
 
