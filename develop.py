26ff6978d8dc6ba66715d06fd004cad168e5247e
Martin Koistinen <mkoistinen@gmail.com>
2014-11-26 05:35:26 +0200

    Add app to develop

--- develop.py
+++ develop.py
@@ -195,7 +195,7 @@ def makemigrations(migrate_plugins=True, merge=False, squash=False):
         'cms', 'menus',
         # testing applications
         'meta', 'manytomany_rel', 'fileapp', 'placeholderapp', 'sampleapp', 'fakemlng', 'one_thing', 'extensionapp',
-        'objectpermissionsapp', 'bunch_of_plugins',
+        'objectpermissionsapp', 'bunch_of_plugins', 'emailuserapp', 'mti_pluginapp',
     ]
     if os.environ.get("AUTH_USER_MODEL") == "emailuserapp.EmailUser":
         applications.append('emailuserapp')
