31760f4ae33616bd881972b56db3163a0e8bb9db
FinalAngel <angelo.dini@divio.ch>
2014-11-27 19:27:51 +0100

    fix spacing

--- cms/static/cms/js/modules/cms.modal.js
+++ cms/static/cms/js/modules/cms.modal.js
@@ -96,10 +96,10 @@ $(document).ready(function () {
 				CMS.API.locked = true;
 			}
 
-            // because a new instance is called, we have to ensure minimized state is removed #3620
+			// because a new instance is called, we have to ensure minimized state is removed #3620
 			if(this.modal.is(':visible'), this.modal.find('.cms_modal-collapsed').length) {
-			    this.minimized = true;
-			    this._minimize();
+				this.minimized = true;
+				this._minimize();
 			}
 
 			// show loader
