b1f5ab46d621e499f17b9edaff34564aceaf255c
Anatoly Ivanov <anatoly.ivanov@webrunners.de>
2014-12-18 17:31:03 +0400

    Fix for PageField-containing form validation working incorrectly in FF/Safari

--- cms/forms/widgets.py
+++ cms/forms/widgets.py
@@ -100,10 +100,10 @@ class PageSelectWidget(MultiWidget):
     };
     var handlePageChange = function(page_id) {
         if (page_id) {
-            $("#id_%(name)s_2 option").removeAttr('selected');
-            $("#id_%(name)s_2 option[value=" + page_id + "]").attr('selected','selected');
+            $("#id_%(name)s_2 option").prop('selected', false);
+            $("#id_%(name)s_2 option[value=" + page_id + "]").prop('selected', true);
         } else {
-            $("#id_%(name)s_2 option[value=]").attr('selected','selected');
+            $("#id_%(name)s_2 option[value=]").prop('selected', true);
         };
     };
     $("#id_%(name)s_0").change(function(){
