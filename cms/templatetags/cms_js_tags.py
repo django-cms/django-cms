f89e355a14d76f044d352022be54a11e2cfc2b80
Jacob Rief <jacob.rief@gmail.com>
2014-12-30 21:34:27 +0100

    Added template filter "json" which is not vulnerable to XSS attacks

--- cms/templatetags/cms_js_tags.py
+++ cms/templatetags/cms_js_tags.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
+import warnings
 from classytags.core import Tag, Options
 from cms.utils.compat import DJANGO_1_4
 from django import template
 from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.html import conditional_escape
 from django.utils.text import javascript_quote
+from django.utils.safestring import mark_safe
 if DJANGO_1_4:
     from django.utils import simplejson as json
 else:
@@ -13,7 +16,33 @@ register = template.Library()
 
 @register.filter
 def js(value):
+    warnings.warn("The template filter '...|js' is vulnerable to XSS attacks, please use '...|json' instead.",
+                  DeprecationWarning, stacklevel=2)
     return json.dumps(value, cls=DjangoJSONEncoder)
+
+
+class SafeJSONEncoder(DjangoJSONEncoder):
+    def _recursive_escape(self, o, esc=conditional_escape):
+        if isinstance(o, dict):
+            return type(o)((esc(k), self._recursive_escape(v)) for (k, v) in o.iteritems())
+        if isinstance(o, (list, tuple)):
+            return type(o)(self._recursive_escape(v) for v in o)
+        try:
+            return type(o)(esc(o))
+        except ValueError:
+            return esc(o)
+
+    def encode(self, o):
+        value = self._recursive_escape(o)
+        return super(SafeJSONEncoder, self).encode(value)
+
+
+@register.filter('json')
+def json_filter(value):
+    """
+    Returns the JSON representation of ``value`` in a safe manner.
+    """
+    return mark_safe(json.dumps(value, cls=SafeJSONEncoder))
 
 
 @register.filter
