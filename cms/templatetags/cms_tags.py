5c03c7673a78695ab1262a8a4f773b12d66da6b4
Stephan Hepper <sh@points.de>
2015-01-06 11:56:59 +0100

    Fixing #3642

--- cms/templatetags/cms_tags.py
+++ cms/templatetags/cms_tags.py
@@ -361,10 +361,7 @@ class RenderPlugin(InclusionTag):
         if not plugin:
             return {'content': ''}
 
-        try:
-            placeholder = context['cms_placeholder_instance']
-        except KeyError:
-            placeholder = plugin.placeholder
+        placeholder = plugin.placeholder
 
         processors = self.get_processors(context, plugin, placeholder)
 
@@ -1094,7 +1091,6 @@ class StaticPlaceholderNode(Tag):
         else:
             placeholder = static_placeholder.public
         placeholder.is_static = True
-        context.update({'cms_placeholder_instance': placeholder})
         content = render_placeholder(placeholder, context, name_fallback=code, default=nodelist)
         return content
 register.tag(StaticPlaceholderNode)
