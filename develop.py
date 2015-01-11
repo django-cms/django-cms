4042a34f94de200c286420c7a684a6cab7e87966
Martin Koistinen <mkoistinen@gmail.com>
2015-01-11 14:40:28 +0200

    Re-add 'emailuserapp' if AUTO_USER_MODEL is set to 'emailuserapp.EmailUser'

--- develop.py
+++ develop.py
@@ -197,6 +197,8 @@ def makemigrations(migrate_plugins=True, merge=False, squash=False):
         'meta', 'manytomany_rel', 'fileapp', 'placeholderapp', 'sampleapp', 'fakemlng', 'one_thing', 'extensionapp',
         'objectpermissionsapp', 'bunch_of_plugins', 'emailuserapp'
     ]
+    if os.environ.get("AUTH_USER_MODEL") == "emailuserapp.EmailUser":
+        applications.append('emailuserapp')
     if migrate_plugins:
         applications.extend([
             # official plugins
