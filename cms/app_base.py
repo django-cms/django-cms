cee9c222afe0624431451ef455cd9df9c0ed482a
Iacopo Spalletti <i.spalletti@nephila.it>
2015-01-24 06:32:04 +0100

    Enforced the 1:1 relation between Configurable AppHook and AppHookConfig

--- cms/app_base.py
+++ cms/app_base.py
@@ -10,13 +10,20 @@ class CMSApp(object):
 
     def __init__(self):
         if self.app_config:
+            if hasattr(self.app_config, 'cmsapp'):
+                raise RuntimeError(
+                    'Only one AppHook per AppHookConfiguration must exists.\n'
+                    'AppHook %s already defined for %s AppHookConfig' % (
+                        self.app_config.cmsapp, self.app_config
+                    )
+                )
             self.app_config.cmsapp = self
 
     def get_configs(self):
-        raise NotImplemented('Configured AppHooks must implement this method')
+        raise NotImplemented('Configurable AppHooks must implement this method')
 
     def get_config(self, namespace):
-        raise NotImplemented('Configured AppHooks must implement this method')
+        raise NotImplemented('Configurable AppHooks must implement this method')
 
     def get_config_add_url(self):
-        raise NotImplemented('Configured AppHooks must implement this method')
+        raise NotImplemented('Configurable AppHooks must implement this method')
