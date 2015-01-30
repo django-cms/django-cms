c85cdf714f1f58e4cb1d3481762a51924e094cd4
Jonas Obrist <ojiidotch@gmail.com>
2015-01-30 14:53:06 +0900

    let selenium handle domain setting for cookies

--- cms/tests/frontend.py
+++ cms/tests/frontend.py
@@ -121,8 +121,6 @@ class CMSLiveTests(LiveServerTestCase, CMSTestCase):
         self.driver.add_cookie({
             'name': settings.SESSION_COOKIE_NAME,
             'value': session.session_key,
-            'path': '/',
-            'domain': urlparse(self.driver.current_url).hostname
         })
         self.driver.get('{0}/?{1}'.format(
             self.live_server_url,
