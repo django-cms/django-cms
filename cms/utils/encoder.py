ccbbb1f7565737d17b49f9994b7f5625ab380c2b
Iacopo Spalletti <i.spalletti@nephila.it>
2015-03-01 15:16:18 +0100

    Fix python 3 code

--- cms/utils/encoder.py
+++ cms/utils/encoder.py
@@ -3,12 +3,13 @@ from django.utils.html import conditional_escape
 from django.utils.encoding import force_text
 from django.utils.functional import Promise
 from django.core.serializers.json import DjangoJSONEncoder
+from django.utils.six import iteritems
 
 
 class SafeJSONEncoder(DjangoJSONEncoder):
     def _recursive_escape(self, o, esc=conditional_escape):
         if isinstance(o, dict):
-            return type(o)((esc(k), self._recursive_escape(v)) for (k, v) in o.iteritems())
+            return type(o)((esc(k), self._recursive_escape(v)) for (k, v) in iteritems(o))
         if isinstance(o, (list, tuple)):
             return type(o)(self._recursive_escape(v) for v in o)
         if type(o) is bool:
