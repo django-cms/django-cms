fb2c08215b8ea8be1f6b0fc06e0537b6f96dc1bb
Jacob Rief <jacob.rief@gmail.com>
2015-03-31 21:13:10 +0200

    Attempt to fix #3973

--- cms/models/permissionmodels.py
+++ cms/models/permissionmodels.py
@@ -1,47 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.db import models
-from django.utils import importlib
 from django.utils.translation import ugettext_lazy as _
-from django.core.exceptions import ImproperlyConfigured
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 
 from cms.models import Page
-from cms.models.managers import (PagePermissionManager,
-                                 GlobalPagePermissionManager)
-from cms.utils.compat import DJANGO_1_6
-from cms.utils.compat.dj import (force_unicode, python_2_unicode_compatible,
-                                 is_user_swapped, user_model_label)
+from cms.models.managers import PagePermissionManager, GlobalPagePermissionManager
+from cms.utils.compat.dj import force_unicode, python_2_unicode_compatible, user_model_label
 from cms.utils.helpers import reversion_register
-
-# To avoid circular dependencies, don't use cms.compat.get_user_model, and
-# don't depend on the app registry, to get the custom user model if used
-if is_user_swapped:
-    user_app_name, user_model_name = user_model_label.rsplit('.', 1)
-    User = None
-    if DJANGO_1_6:
-        for app in settings.INSTALLED_APPS:
-            if app.endswith(user_app_name):
-                user_app_models = importlib.import_module(app + ".models")
-                User = getattr(user_app_models, user_model_name)
-                break
-    else:
-        # This is sort of a hack
-        # AppConfig is not ready yet, and we blindly check if the user model
-        # application has already been loaded
-        from django.apps import apps
-        try:
-            User = apps.all_models[user_app_name][user_model_name.lower()]
-        except KeyError:
-            pass
-    if User is None:
-        raise ImproperlyConfigured(
-            "You have defined a custom user model %s, but the app %s is not "
-            "in settings.INSTALLED_APPS" % (user_model_label, user_app_name)
-        )
-else:
-    from django.contrib.auth.models import User
 
 # NOTE: those are not just numbers!! we will do binary AND on them,
 # so pay attention when adding/changing them, or MASKs..
@@ -63,6 +30,7 @@ ACCESS_CHOICES = (
     (ACCESS_DESCENDANTS, _('Page descendants')),
     (ACCESS_PAGE_AND_DESCENDANTS, _('Page and descendants')),
 )
+
 
 class AbstractPagePermission(models.Model):
     """Abstract page permissions
@@ -136,9 +104,10 @@ class PagePermission(AbstractPagePermission):
         return "%s :: %s has: %s" % (page, self.audience, force_unicode(dict(ACCESS_CHOICES)[self.grant_on]))
 
 
-class PageUser(User):
+class PageUser(models.Model):
     """Cms specific user data, required for permission system
     """
+    user_ptr = models.OneToOneField(settings.AUTH_USER_MODEL, primary_key=True, parent_link=True)
     created_by = models.ForeignKey(user_model_label, related_name="created_users")
 
     class Meta:
