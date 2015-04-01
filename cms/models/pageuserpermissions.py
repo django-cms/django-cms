eaf47de98048509b3c9641ac50d95dbeaae2f710
Jacob Rief <jacob.rief@gmail.com>
2015-04-01 16:20:40 +0200

    removed duplicate line

--- cms/models/pageuserpermissions.py
+++ cms/models/pageuserpermissions.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# -*- coding: utf-8 -*-
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.core.exceptions import ImproperlyConfigured
