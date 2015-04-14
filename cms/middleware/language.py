38dc248fb73ac590f74e1bd1eff50f49585fbc66
Luis Diego Garcia <ldgarcia27@gmail.com>
2015-04-14 17:36:10 -0600

    Fix #4023: Update language middleware for Django 1.8.

--- cms/middleware/language.py
+++ cms/middleware/language.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
 
-from django.utils.translation import get_language
+from django.utils.translation import (get_language, LANGUAGE_SESSION_KEY)
 from django.conf import settings
 
 
@@ -9,9 +9,9 @@ class LanguageCookieMiddleware(object):
     def process_response(self, request, response):
         language = get_language()
         if hasattr(request, 'session'):
-            session_language = request.session.get('django_language', None)
+            session_language = request.session.get(LANGUAGE_SESSION_KEY, None)
             if session_language and not session_language == language:
-                request.session['django_language'] = language
+                request.session[LANGUAGE_SESSION_KEY] = language
                 request.session.save()
         if settings.LANGUAGE_COOKIE_NAME in request.COOKIES and \
                         request.COOKIES[settings.LANGUAGE_COOKIE_NAME] == language:
