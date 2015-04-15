c504a4f8d11348f84e61eed762ae47aec73084a4
Benjamin Wohlwend <piquadrat@gmail.com>
2015-04-15 21:44:01 +0200

    explicitly copy __module__, __doc__ and __name__/__class__.__name__ to decorating function

--- cms/utils/decorators.py
+++ cms/utils/decorators.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-from functools import wraps
-from cms.views import _handle_no_page
+from django.conf import settings
 from django.contrib.auth.views import redirect_to_login
 from django.utils.http import urlquote
-from django.conf import settings
+
+from cms.views import _handle_no_page
 
 
 def cms_perms(func):
-    @wraps(func)
     def inner(request, *args, **kwargs):
         page = request.current_page
         if page:
@@ -16,4 +15,10 @@ def cms_perms(func):
             if not page.has_view_permission(request):
                 return _handle_no_page(request, "$")
         return func(request, *args, **kwargs)
+    inner.__module__ = func.__module__
+    inner.__doc__ = func.__doc__
+    if hasattr(func, '__name__'):
+        inner.__name__ = func.__name__
+    elif hasattr(func, '__class__'):
+        inner.__name__ = func.__class__.__name__
     return inner
