a266be0c84d9134209e6799dfef00aa75c5919a5
Iacopo Spalletti <i.spalletti@nephila.it>
2015-04-25 23:34:35 +0200

    Fix obsolete test

--- cms/tests/page.py
+++ cms/tests/page.py
@@ -34,6 +34,7 @@ from cms.templatetags.cms_tags import get_placeholder_content
 from cms.test_utils.testcases import (CMSTestCase, URL_CMS_PAGE, URL_CMS_PAGE_ADD)
 from cms.test_utils.util.context_managers import LanguageOverride, UserLoginContext
 from cms.utils import get_cms_setting
+from cms.utils.compat import DJANGO_1_7
 from cms.utils.compat.dj import installed_apps
 from cms.utils.page_resolver import get_page_from_request, is_valid_url
 from cms.utils.page import is_valid_page_slug, get_available_slug
@@ -51,8 +52,10 @@ class PageMigrationTestCase(CMSTestCase):
         Test correct content type is set for Page object
         """
         from django.contrib.contenttypes.models import ContentType
-        self.assertFalse(ContentType.objects.filter(model='page', name='', app_label='cms').exists())
-        self.assertTrue(ContentType.objects.filter(model='page', name='page', app_label='cms').exists())
+        if DJANGO_1_7:
+            # obsolete test for an old bug, can be dropped at any time
+            self.assertFalse(ContentType.objects.filter(model='page', name='', app_label='cms').exists())
+        self.assertEqual(ContentType.objects.filter(model='page', app_label='cms').count(), 1)
 
 
 def has_no_custom_user():
