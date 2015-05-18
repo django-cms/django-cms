c46f09063ad292d5315f5fde5ac3390c13a04f63
Louis Haddrell <louishaddrell@gmail.com>
2015-05-18 10:57:54 +0100

    Fixes alias TypeError issue (#4104)

--- cms/utils/plugins.py
+++ cms/utils/plugins.py
@@ -135,8 +135,9 @@ def build_plugin_tree(plugins):
                 in groupby(nonroots, by_parent_id))
     for parent, children in families:
         parent.child_plugin_instances = children
+    by_position = attrgetter('position')
     return sorted(filterfalse(by_parent_id, cache.values()),
-                  key=attrgetter('position'))
+                  key=(lambda x: (by_position(x) is None, by_position(x))))
 
 
 def downcast_plugins(queryset, placeholders=None, select_placeholder=False):
