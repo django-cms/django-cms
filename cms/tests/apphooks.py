719274e283c0351cb3116a62ec85e6acbe1d06f8
Venelin Stoykov <venelin@magicsolutions.bg>
2015-05-20 15:38:56 +0300

    Fix order of which application urls are injected into urlpatterns
    This will fix problem when there are nested apphooks and urls of top level apphook are more greedy

--- cms/tests/apphooks.py
+++ cms/tests/apphooks.py
@@ -142,7 +142,7 @@ class ApphooksTestCase(CMSTestCase):
             apphook_pool.clear()
             hooks = apphook_pool.get_apphooks()
             app_names = [hook[0] for hook in hooks]
-            self.assertEqual(len(hooks), 4)
+            self.assertEqual(len(hooks), 6)
             self.assertIn(NS_APP_NAME, app_names)
             self.assertIn(APP_NAME, app_names)
             apphook_pool.clear()
@@ -663,6 +663,34 @@ class ApphooksTestCase(CMSTestCase):
                 # parameters - non page object
                 response = self.client.post(url, {'pk': ex1.pk, 'model': 'placeholderapp.example1'})
                 self.assertEqual(response.content.decode('utf-8'), ex1.get_absolute_url())
+
+    def test_nested_apphooks_urls(self):
+        # make sure that urlparams actually reach the apphook views
+        with SettingsOverride(ROOT_URLCONF='cms.test_utils.project.urls'):
+            apphook_pool.clear()
+
+            superuser = get_user_model().objects.create_superuser('admin', 'admin@admin.com', 'admin')
+            create_page("home", "nav_playground.html", "en", created_by=superuser, published=True, )
+            parent_page = create_page("parent-apphook-page", "nav_playground.html", "en",
+                                      created_by=superuser, published=True, apphook="ParentApp")
+            create_page("child-apphook-page", "nav_playground.html", "en", parent=parent_page,
+                        created_by=superuser, published=True, apphook="ChildApp")
+
+            parent_app_path = reverse('parentapp_view', kwargs={'path': 'parent/path/'})
+            child_app_path = reverse('childapp_view', kwargs={'path': 'child-path/'})
+
+            # Ensure the page structure is ok before getting responses
+            self.assertEqual(parent_app_path, '/en/parent-apphook-page/parent/path/')
+            self.assertEqual(child_app_path, '/en/parent-apphook-page/child-apphook-page/child-path/')
+
+            # Get responses for both paths and ensure that the right view will answer
+            response = self.client.get(parent_app_path)
+            self.assertContains(response, 'parent app content', status_code=200)
+
+            response = self.client.get(child_app_path)
+            self.assertContains(response, 'child app content', status_code=200)
+
+            apphook_pool.clear()
 
 
 class ApphooksPageLanguageUrlTestCase(SettingsOverrideTestCase):
