6bc16e9d0f12024b0f120da7e7104fb1c33501a0
Martin Koistinen <mkoistinen@gmail.com>
2015-05-26 22:53:19 +0300

    Remove debugging code

--- menus/menu_pool.py
+++ menus/menu_pool.py
@@ -294,7 +294,6 @@ class MenuPool(object):
                 if menu[1].__name__ not in classes:
                     classes.append(menu[1].__name__)
                     found.append((menu[0], menu[1].name))
-        print(classes, found)
         return found
 
     def get_nodes_by_attribute(self, nodes, name, value):
