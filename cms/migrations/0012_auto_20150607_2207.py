ad70c54f2cd5a66d62f61b26e12b18c3bbfba45d
Iacopo Spalletti <i.spalletti@nephila.it>
2015-06-07 23:40:31 +0200

    Format migration for Django 1.7

--- cms/migrations/0012_auto_20150607_2207.py
+++ cms/migrations/0012_auto_20150607_2207.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 from django.conf import settings
-import django.contrib.auth.models
 
 
 class Migration(migrations.Migration):
@@ -13,20 +12,16 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AlterModelManagers(
-            name='pageuser',
-            managers=[
-                ('objects', django.contrib.auth.models.UserManager()),
-            ],
-        ),
         migrations.AlterField(
             model_name='globalpagepermission',
             name='sites',
-            field=models.ManyToManyField(blank=True, to='sites.Site', verbose_name='sites', help_text='If none selected, user haves granted permissions to all sites.'),
+            field=models.ManyToManyField(help_text='If none selected, user haves granted permissions to all sites.', to='sites.Site', verbose_name='sites', blank=True),
+            preserve_default=True,
         ),
         migrations.AlterField(
             model_name='usersettings',
             name='user',
-            field=models.OneToOneField(to=settings.AUTH_USER_MODEL, editable=False, related_name='djangocms_usersettings'),
+            field=models.OneToOneField(related_name='djangocms_usersettings', editable=False, to=settings.AUTH_USER_MODEL),
+            preserve_default=True,
         ),
     ]
