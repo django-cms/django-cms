21c92ad27a3f6015f44f4b54e9c1d458eeaa32cf
Hans Andersen <hans@dueandersen.com>
2015-07-12 00:00:23 +0200

    Added parentheses for multi-line condition

--- cms/plugin_rendering.py
+++ cms/plugin_rendering.py
@@ -60,8 +60,8 @@ def render_plugin(context, instance, placeholder, template, processors=None, cur
         processors = []
     if isinstance(template, six.string_types):
         content = render_to_string(template, context)
-    elif isinstance(template, Template) or (hasattr(template, 'template') and\
-         hasattr(template, 'render') and isinstance(template.template, Template)):
+    elif (isinstance(template, Template) or (hasattr(template, 'template') and\
+         hasattr(template, 'render') and isinstance(template.template, Template))):
         content = template.render(context)
     else:
         content = ''
