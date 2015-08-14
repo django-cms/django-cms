fadaa923a1bdec745c29c5d8d467f35b7345b724
Jonas Obrist <ojiidotch@gmail.com>
2015-08-14 15:05:34 +0200

    Django test case is only needed in Python 2.6, 2.7+ unittest test case can be used

--- cms/tests/test_static_analysis.py
+++ cms/tests/test_static_analysis.py
@@ -1,9 +1,14 @@
-from django.test import testcases
+import sys
+
+if sys.version_info[0] == 2 and sys.version_info[1] == 6:
+    from django.test.testcases import TestCase
+else:
+    from unittest import TestCase
 
 from cms.test_utils.util.static_analysis import pyflakes
 
 
-class AboveStaticAnalysisCodeTest(testcases.TestCase):
+class AboveStaticAnalysisCodeTest(TestCase):
     """
     Name is pretty lame, but ensure it's executed before every other test
     """
