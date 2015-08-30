166d2de36cfd654654a52004b57c110983cad2f8
AlexProfi <avd-84@yandex.ru>
2015-08-31 01:14:48 +0300

    add page slug to navigation node

--- cms/cms_menus.py
+++ cms/cms_menus.py
@@ -160,6 +160,8 @@ def page_to_node(page, home, cut):
     # Do we have a redirectURL?
     attr['redirect_url'] = page.get_redirect()  # save redirect URL if any
 
+    attr['slug'] = page.get_slug() #save page slug
+
     # Now finally, build the NavigationNode object and return it.
     ret_node = NavigationNode(
         page.get_menu_title(),
