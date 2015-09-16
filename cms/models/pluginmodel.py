a07432c3d77c607d3751b375112f200f60928d08
Martin Koistinen <mkoistinen@gmail.com>
2015-09-16 16:44:24 -0400

    Fixes #4456

--- cms/models/pluginmodel.py
+++ cms/models/pluginmodel.py
@@ -345,13 +345,15 @@ class CMSPlugin(six.with_metaclass(PluginModelBase, MP_Node)):
                 order = CMSPlugin.objects.filter(
                         placeholder_id=placeholder.pk, language=language,
                         parent_id__isnull=True
-                    ).values_list('pk', flat=True)
+                    ).order_by('position').values_list('pk', flat=True)
                 reorder_plugins(placeholder, None, language, order)
+
                 for plugin in CMSPlugin.objects.filter(
                         placeholder_id=placeholder.pk,
                         language=language).order_by('depth', 'path'):
                     order = CMSPlugin.objects.filter(
-                        parent_id=plugin.pk).values_list('pk', flat=True)
+                            parent_id=plugin.pk
+                        ).order_by('position').values_list('pk', flat=True)
                     reorder_plugins(placeholder, plugin.pk, language, order)
 
     def post_copy(self, old_instance, new_old_ziplist):
