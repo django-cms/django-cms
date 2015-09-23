309f298d71ce8b13af84390f013e5ba65151946f
Vadim Sikora <vadim.sikora@divio.ch>
2015-09-23 11:37:58 +0200

    remove unused import

--- cms/tests/test_frontend.py
+++ cms/tests/test_frontend.py
@@ -22,7 +22,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
 
 from cms.api import create_page, create_title, add_plugin
