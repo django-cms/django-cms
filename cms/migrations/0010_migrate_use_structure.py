ecaf740fe05e20bdd68d05d8d5a610393a5f632c
Iacopo Spalletti <i.spalletti@nephila.it>
2015-09-23 08:26:15 +0200

    Catch any exception in 0010_migrate_use_structure

--- cms/migrations/0010_migrate_use_structure.py
+++ cms/migrations/0010_migrate_use_structure.py
@@ -23,7 +23,7 @@ def forwards(apps, schema_editor):
         for group in Group.objects.all():
             if page_permission in group.permissions.all():
                 group.permissions.add(permission.pk)
-    except ContentType.DoesNotExist:
+    except Exception:
         print(u'Users not migrated to use_structure permission, please add the permission manually')
 
 
@@ -31,13 +31,16 @@ def backwards(apps, schema_editor):
     user_model = apps.get_model(settings.AUTH_USER_MODEL)
     ph_model = apps.get_model('cms', 'Placeholder')
     ph_ctype = ContentType.objects.get(app_label=ph_model._meta.app_label, model=ph_model._meta.model_name)
-    permission, __ = Permission.objects.get_or_create(
-        codename='use_structure', content_type=ph_ctype, name=u"Can use Structure mode")
-    for user in user_model.objects.filter(is_superuser=False, is_staff=True):
-        user.user_permissions.remove(permission.pk)
-    for group in Group.objects.all():
-        if permission in group.permissions.all():
-            group.permissions.remove(permission.pk)
+    try:
+        permission, __ = Permission.objects.get_or_create(
+            codename='use_structure', content_type=ph_ctype, name=u"Can use Structure mode")
+        for user in user_model.objects.filter(is_superuser=False, is_staff=True):
+            user.user_permissions.remove(permission.pk)
+        for group in Group.objects.all():
+            if permission in group.permissions.all():
+                group.permissions.remove(permission.pk)
+    except Exception:
+        print(u'use_structure not removed from all the users, please check the permission manually')
 
 
 class Migration(migrations.Migration):
