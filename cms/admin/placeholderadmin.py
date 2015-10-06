e0214c6dd565e79a9e6738ecc743014160b75eb9
Martin Koistinen <mkoistinen@gmail.com>
2015-10-06 12:30:03 -0400

    Fix up placeholder references

--- cms/admin/placeholderadmin.py
+++ cms/admin/placeholderadmin.py
@@ -459,7 +459,8 @@ class PlaceholderAdminMixin(object):
                 return HttpResponseBadRequest(er)
         if move_a_copy:
             new_plugins = copy_plugins.copy_plugins_to(
-                [plugin] + list(plugin.get_descendants()), source_placeholder)
+                [plugin] + list(plugin.get_descendants()), placeholder,
+                    language, parent_id)
             plugin = new_plugins[0][0]
             # If an ordering was supplied, we should replace the item that has
             # been copied with the new copy
