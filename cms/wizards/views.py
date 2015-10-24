6bb1562125f51d2afe5c9b435167449ff76c59a9
Iacopo Spalletti <i.spalletti@nephila.it>
2015-10-24 18:25:48 +0200

    Fix wizard in python 3

--- cms/wizards/views.py
+++ cms/wizards/views.py
@@ -139,7 +139,7 @@ class WizardCreateView(WizardViewMixin, SessionWizardView):
         This step only runs if all forms are valid. Simply emits a simple
         template that uses JS to redirect to the newly created object.
         """
-        form_two = form_list[1]
+        form_two = list(form_list)[1]
         instance = form_two.save()
 
         context = {
