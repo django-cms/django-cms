593bec631793568533744585edc3d96506408f56
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-03 14:55:38 +0100

    use `addPluginHelpTitle` from options instead of dom node
    sometimes it might be that the plugin doesn't have a dom representation
    and that line would throw an error then

--- cms/static/cms/js/modules/cms.plugins.js
+++ cms/static/cms/js/modules/cms.plugins.js
@@ -883,7 +883,7 @@ var CMS = window.CMS || {};
                     );
 
                     modal.open({
-                        title: that.ui.container.data('settings').addPluginHelpTitle,
+                        title: that.options.addPluginHelpTitle,
                         html: pluginsCopy,
                         width: 530,
                         height: 400
