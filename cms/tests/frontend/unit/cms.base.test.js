fd90fc86d14de0bf0820b266ecc458950f22fb57
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-03 14:15:01 +0100

    add a small test [use saucelabs]

--- cms/tests/frontend/unit/cms.base.test.js
+++ cms/tests/frontend/unit/cms.base.test.js
@@ -5,5 +5,6 @@ describe('cms.base.js', function () {
         expect(CMS.API).toEqual(jasmine.any(Object));
         expect(CMS.KEYS).toEqual(jasmine.any(Object));
         expect(CMS.BREAKPOINTS).toEqual(jasmine.any(Object));
+        expect(CMS.$).toEqual(jQuery);
     });
 });
