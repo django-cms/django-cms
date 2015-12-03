a29b1b40f26923b0d65df33a2abd01eed5c8d52f
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-03 18:07:55 +0100

    tests for CMS.API.Helpers.makeURL

--- cms/tests/frontend/unit/cms.base.test.js
+++ cms/tests/frontend/unit/cms.base.test.js
@@ -37,7 +37,50 @@ describe('cms.base.js', function () {
         });
 
         describe('.makeURL()', function () {
+            it('exists', function () {
+                expect(CMS.API.Helpers.makeURL).toEqual(jasmine.any(Function));
+            });
 
+            it('outputs the same url when no additional params passed', function () {
+                var url = CMS.API.Helpers.makeURL('test');
+                expect(url).toEqual('test');
+            });
+
+            it('outputs new url when additional params passed', function () {
+                var url;
+
+                url = CMS.API.Helpers.makeURL('test', ['param=1']);
+                expect(url).toEqual('test?param=1');
+            });
+
+            it('outputs new url when there are multiple additional params', function () {
+                var url;
+
+                url = CMS.API.Helpers.makeURL('test', ['param=1', 'another=2']);
+                expect(url).toEqual('test?param=1&amp;another=2');
+
+                url = CMS.API.Helpers.makeURL('test?param=1', ['another=2']);
+                expect(url).toEqual('test?param=1&amp;another=2');
+
+                url = CMS.API.Helpers.makeURL('test?param=1&another=2', ['different=3']);
+                expect(url).toEqual('test?param=1&amp;another=2&amp;different=3');
+
+                url = CMS.API.Helpers.makeURL('test?param=1&amp;another=2', ['different=3']);
+                expect(url).toEqual('test?param=1&amp;another=2&amp;different=3');
+
+                url = CMS.API.Helpers.makeURL('test?param=1&another=2&amp;again=3', ['different=3']);
+                expect(url).toEqual('test?param=1&amp;another=2&amp;again=3&amp;different=3');
+            });
+
+            it('replaces param values with new ones if they match', function () {
+                var url;
+
+                url = CMS.API.Helpers.makeURL('test?param=1&amp;another=2', ['another=3']);
+                expect(url).toEqual('test?param=1&amp;another=3');
+
+                url = CMS.API.Helpers.makeURL('test?param=1&amp;another=2', ['another=3', 'param=4']);
+                expect(url).toEqual('test?param=4&amp;another=3');
+            });
         });
 
         describe('.debounce()', function () {
