30ef80b4ee38fe4b47342252ba1e0806df410617
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-03 14:13:05 +0100

    don't use coveralls on local machine

--- cms/tests/frontend/karma.conf.js
+++ cms/tests/frontend/karma.conf.js
@@ -104,7 +104,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage', 'coveralls', 'saucelabs'],
+        reporters: ['progress', 'coverage', 'saucelabs'].concat(process.env.CI ? ['coveralls'] : []),
 
         // web server port
         port: 9876,
