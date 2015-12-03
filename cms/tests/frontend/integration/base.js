5a396d7d353bbfdd3a5ffed1a9455f63759de553
Dmitry Gorelik <freelancer2go@gmail.com>
2015-12-03 12:19:12 +0200

    sample integration test

---
+++ cms/tests/frontend/integration/base.js
@@ -0,0 +1,9 @@
+casper.test.begin('Sample test', function(test) {
+    casper
+        .start('http://www.google.com/', function() {
+            test.assertTitle('Google', 'google.com has the correct title');
+        })
+        .run(function() {
+            test.done();
+        });
+});
