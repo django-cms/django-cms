10ea4730113fcbfdce44d781d5b701d355f772f1
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-08 13:49:02 +0100

    simple modal tests

--- cms/tests/frontend/unit/cms.modal.test.js
+++ cms/tests/frontend/unit/cms.modal.test.js
@@ -15,7 +15,60 @@ describe('CMS.Modal', function () {
     });
 
     describe('instance', function () {
+        it('has ui', function (done) {
+            $(function () {
+                var modal = new CMS.Modal();
+                expect(modal.ui).toEqual(jasmine.any(Object));
+                expect(Object.keys(modal.ui)).toContain('modal');
+                expect(Object.keys(modal.ui)).toContain('body');
+                expect(Object.keys(modal.ui)).toContain('window');
+                expect(Object.keys(modal.ui)).toContain('toolbarLeftPart');
+                expect(Object.keys(modal.ui)).toContain('minimizeButton');
+                expect(Object.keys(modal.ui)).toContain('maximizeButton');
+                expect(Object.keys(modal.ui)).toContain('title');
+                expect(Object.keys(modal.ui)).toContain('titlePrefix');
+                expect(Object.keys(modal.ui)).toContain('titleSuffix');
+                expect(Object.keys(modal.ui)).toContain('resize');
+                expect(Object.keys(modal.ui)).toContain('breadcrumb');
+                expect(Object.keys(modal.ui)).toContain('closeAndCancel');
+                expect(Object.keys(modal.ui)).toContain('modalButtons');
+                expect(Object.keys(modal.ui)).toContain('modalBody');
+                expect(Object.keys(modal.ui)).toContain('frame');
+                expect(Object.keys(modal.ui)).toContain('shim');
+                expect(Object.keys(modal.ui).length).toEqual(16);
+                done();
+            });
+        });
 
+        it('has options', function (done) {
+            $(function () {
+                var modal = new CMS.Modal();
+                expect(modal.options).toEqual({
+                    onClose: false,
+                    minHeight: 400,
+                    minWidth: 800,
+                    modalDuration: 200,
+                    newPlugin: false,
+                    resizable: true,
+                    maximizable: true,
+                    minimizable: true
+                });
+
+                var modal2 = new CMS.Modal({ minHeight: 300, minWidth: 100 });
+                expect(modal2.options).toEqual({
+                    onClose: false,
+                    minHeight: 300,
+                    minWidth: 100,
+                    modalDuration: 200,
+                    newPlugin: false,
+                    resizable: true,
+                    maximizable: true,
+                    minimizable: true
+                });
+
+                done();
+            });
+        });
     });
 
     describe('.open()', function () {
