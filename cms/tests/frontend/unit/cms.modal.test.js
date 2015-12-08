2d0668d1bf13bb26837fff85275a8eecca3613ab
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-08 13:49:19 +0100

    initial `open` tests

--- cms/tests/frontend/unit/cms.modal.test.js
+++ cms/tests/frontend/unit/cms.modal.test.js
@@ -3,6 +3,8 @@
 'use strict';
 
 describe('CMS.Modal', function () {
+    fixture.setBase('cms/tests/frontend/unit/fixtures');
+
     it('creates a Modal class when document is ready', function () {
         expect(CMS.Modal).toBeDefined();
     });
@@ -72,12 +74,84 @@ describe('CMS.Modal', function () {
     });
 
     describe('.open()', function () {
-        it('opens the modal', function (done) {
+        beforeEach(function (done) {
+            fixture.load('modal.html');
+            CMS.API.Tooltip = {
+                hide: jasmine.createSpy()
+            };
+            CMS.API.Toolbar = {
+                showLoader: jasmine.createSpy()
+            };
             $(function () {
-                var modal = new CMS.Modal();
-
                 done();
             });
+        });
+
+        afterEach(function () {
+            fixture.cleanup();
+        });
+
+        it('throws an error when no url or html options were passed', function () {
+            var modal = new CMS.Modal();
+
+            expect(modal.open.bind(modal, {})).toThrowError(
+                Error, 'The arguments passed to "open" were invalid.'
+            );
+            expect(modal.open.bind(modal, { html: '' })).toThrowError(
+                Error, 'The arguments passed to "open" were invalid.'
+            );
+            expect(modal.open.bind(modal, { url: '' })).toThrowError(
+                Error, 'The arguments passed to "open" were invalid.'
+            );
+            expect(modal.open.bind(modal, { html: '<div></div>' })).not.toThrow();
+            expect(modal.open.bind(modal, {
+                url: '/base/cms/tests/frontend/unit/html/modal_iframe.html'
+            })).not.toThrow();
+        });
+
+        it('does not open if there is a plugin creation in process', function () {
+            var modal = new CMS.Modal();
+            CMS._newPlugin = true;
+            spyOn(modal, '_deletePlugin').and.callFake(function () {
+                return false;
+            });
+
+            expect(modal.open({ html: '<div></div>' })).toEqual(false);
+        });
+
+        it('confirms if the user wants to remove freshly created plugin when opening new modal', function () {
+            var modal = new CMS.Modal();
+            spyOn(CMS.Navigation.prototype, 'initialize').and.callFake(function () {
+                return {};
+            });
+            CMS.API.Toolbar = new CMS.Toolbar();
+            CMS._newPlugin = {
+                delete: '/delete-url',
+                breadcrumb: [{ title: 'Fresh plugin' }]
+            };
+
+            CMS.config = $.extend(CMS.config, {
+                csrf: 'custom-token',
+                lang: {
+                    confirmEmpty: 'Question about {1}?'
+                }
+            });
+
+            spyOn(modal, '_deletePlugin').and.callThrough();
+            jasmine.Ajax.install();
+            spyOn(CMS.API.Toolbar, 'openAjax').and.callThrough();
+            spyOn(CMS.API.Helpers, 'secureConfirm').and.callFake(function () {
+                return false;
+            });
+
+            expect(modal.open({ html: '<div></div>' })).toEqual(false);
+            expect(CMS.API.Toolbar.openAjax).toHaveBeenCalledWith({
+                url: '/delete-url',
+                post: '{ "csrfmiddlewaretoken": "custom-token" }',
+                text: 'Question about Fresh plugin?',
+                callback: jasmine.any(Function)
+            });
+            jasmine.Ajax.uninstall();
         });
     });
 });
