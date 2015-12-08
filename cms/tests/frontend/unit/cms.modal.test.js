eeab00cd0ba99f74c5e4b1b15b27053f20025fc5
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-08 09:54:14 +0100

    wip

--- cms/tests/frontend/unit/cms.modal.test.js
+++ cms/tests/frontend/unit/cms.modal.test.js
@@ -3,17 +3,28 @@
 'use strict';
 
 describe('CMS.Modal', function () {
-    it('creates a Modal class when document is ready', function (done) {
-        $(function () {
-            expect(CMS.Modal).toBeDefined();
-            done();
-        });
+    it('creates a Modal class when document is ready', function () {
+        expect(CMS.Modal).toBeDefined();
     });
 
-    it('has public methods', function (done) {
-        $(function () {
-            expect(CMS.Modal.prototype.open).toEqual(jasmine.any(Function));
-            done();
+    it('has public API', function () {
+        expect(CMS.Modal.prototype.open).toEqual(jasmine.any(Function));
+        expect(CMS.Modal.prototype.close).toEqual(jasmine.any(Function));
+        expect(CMS.Modal.prototype.minimize).toEqual(jasmine.any(Function));
+        expect(CMS.Modal.prototype.maximize).toEqual(jasmine.any(Function));
+    });
+
+    describe('instance', function () {
+
+    });
+
+    describe('.open()', function () {
+        it('opens the modal', function (done) {
+            $(function () {
+                var modal = new CMS.Modal();
+
+                done();
+            });
         });
     });
 });
