5e833dab17b203fbb7d410289c8bbc3222dd2de9
Mykhailo Kolesnyk <mykhailo.kolesnyk@divio.ch>
2015-12-09 17:13:03 +0200

    Integration tests: adjust first window title to match plain CMS installation

--- cms/tests/frontend/integration/settings/globals.js
+++ cms/tests/frontend/integration/settings/globals.js
@@ -5,11 +5,16 @@
 // TODO update with actual data
 
 module.exports = {
-    websiteName: 'example.com',
+    // plain Django admin login window title looks like this:
+    websiteName: 'Log in | Django site admin',
+    // TODO:
+    // - configure languages with djangocms-helper
+    // - remove hardcoded host, port, language values
     baseUrl: 'http://localhost:8000/en/',
     editUrl: 'http://localhost:8000/en/?edit',
     editOffUrl: 'http://localhost:8000/en/?edit_off',
     credentials: {
+        // djangocms helper creates such a user by default
         username: 'admin',
         password: 'admin'
     }
