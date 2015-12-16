7dc62aff6120ce0761faef5e0cddee7647c8fef1
Ruslan Malogulko <ruslan.malogulko@divio.ch>
2015-12-16 17:57:09 +0200

    Implement switch language integration test

---
+++ cms/tests/frontend/integration/switchLanguage.js
@@ -0,0 +1,66 @@
+'use strict';
+
+// #############################################################################
+// Switch language via the admin panel
+
+var globals = require('./settings/globals');
+var messages = require('./settings/messages').page.addContent;
+var randomString = require('./helpers/randomString').randomString;
+
+// random text string for filtering and content purposes
+var randomText = randomString(10);
+
+casper.test.begin('Switch language', function (test) {
+    casper
+        .start(globals.editUrl)
+        // click on language bar
+        .waitUntilVisible('.cms-toolbar-expanded', function () {
+            this.click('.cms-toolbar-item-navigation > li:nth-child(4) > a');
+        })
+        // select german language
+        .waitUntilVisible('.cms-toolbar-item-navigation > li:nth-child(4) > ul', function () {
+            this.click('.cms-toolbar-item-navigation-hover a[href="/de/"]');
+        })
+        // no page should be here (warning message instead)
+        .waitUntilVisible('.cms-toolbar-expanded', function () {
+            test.assertSelectorHasText('.cms-screenblock-inner h1', 'This page has no preview');
+            this.click('.cms-toolbar-item-navigation > li:nth-child(4) > a');
+        })
+        // add german translation
+        .waitUntilVisible('.cms-toolbar-item-navigation > li:nth-child(4) > ul', function () {
+            this.click('.cms-toolbar-item-navigation-hover a[href*="?language=de"]');
+        })
+        // open Change pane modal
+        .waitUntilVisible('.cms-modal-open', function () {
+            this.page.switchToChildFrame(0);
+            this.fill('#page_form', {
+                'title': randomText,
+                'slug': randomText
+            });
+            this.page.switchToParentFrame();
+            this.click('.cms-modal-open .cms-modal-item-buttons .cms-btn-action')
+        })
+        // check if german version appears
+        .waitWhileVisible('.cms-modal-open', function () {
+            test.assertSelectorHasText('ul.nav > .child > a[href="/de/"]', randomText);
+        })
+        // click on language bar
+        .waitUntilVisible('.cms-toolbar-expanded', function () {
+            this.click('.cms-toolbar-item-navigation > li:nth-child(4) > a');
+        })
+        // delete german translation
+        .waitUntilVisible('.cms-toolbar-item-navigation > li:nth-child(4) > ul', function () {
+            this.click('.cms-toolbar-item-navigation-hover a[href*="delete-translation/?language=de"]');
+        })
+        // submit translation deletion
+        .waitUntilVisible('.cms-modal-open', function () {
+            this.click('.cms-modal-open .cms-modal-item-buttons .cms-btn.deletelink')
+        })
+        // make sure translation has been deleted
+        .waitWhileVisible('.cms-modal-open', function () {
+            test.assertSelectorHasText('.cms-screenblock-inner h1', 'This page has no preview');
+        })
+        .run(function () {
+            test.done();
+        });
+});
