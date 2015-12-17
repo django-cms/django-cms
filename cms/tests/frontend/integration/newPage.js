859f428c3e920e808c8fc1510fcc807c1c09fea5
Dmitry Gorelik <freelancer2go@gmail.com>
2015-12-17 11:58:38 +0200

    modal open

--- cms/tests/frontend/integration/newPage.js
+++ cms/tests/frontend/integration/newPage.js
@@ -10,6 +10,25 @@ casper.test.begin('New Page Creation', function (test) {
     casper
         .start(globals.editUrl)
         .waitUntilVisible('.cms-toolbar-expanded', function () {
+            this.click('.cms-toolbar-item-navigation > li:nth-child(2) > a');
+        })
+        .waitForSelector('.cms-toolbar-item-navigation-hover', function () {
+            this.click('.cms-toolbar-item-navigation-hover li:first-child a');
+        })
+        .then(function () {
+            this.click('.cms-toolbar-item-navigation-hover .cms-toolbar-item-navigation-hover li:first-child a');
+        })
+        .waitUntilVisible('.cms-modal', function () {
+            test.assertVisible('.cms-modal', 'modal visible');
+
+            this.click('.cms-modal .cms-btn-action');
+
+        })
+        .withFrame(0, function () {
+            casper
+                .waitForSelector('#page_form', function () {
+                    test.assertExists('.errornote', 'failed to submit empty');
+                });
         })
         .run(function () {
             test.done();
