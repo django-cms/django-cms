fa33704ff0ae176793c51f665ddb54b8ba6ecd23
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-30 13:36:36 +0100

    initial tests for instance

--- cms/tests/frontend/unit/cms.structureboard.test.js
+++ cms/tests/frontend/unit/cms.structureboard.test.js
@@ -7,11 +7,55 @@ describe('CMS.StructureBoard', function () {
         expect(CMS.StructureBoard).toBeDefined();
     });
 
-    it('has public API');
+    it('has public API', function () {
+        expect(CMS.StructureBoard.prototype.show).toEqual(jasmine.any(Function));
+        expect(CMS.StructureBoard.prototype.hide).toEqual(jasmine.any(Function));
+        expect(CMS.StructureBoard.prototype.getId).toEqual(jasmine.any(Function));
+        expect(CMS.StructureBoard.prototype.getIds).toEqual(jasmine.any(Function));
+    });
 
     describe('instance', function () {
-        it('has ui');
-        it('has options');
+        var board;
+        beforeEach(function (done) {
+            fixture.load('plugins.html');
+            CMS.settings = {
+                mode: 'edit'
+            };
+            CMS.config = {
+                mode: 'edit'
+            };
+            $(function () {
+                board = new CMS.StructureBoard();
+                done();
+            });
+        });
+
+        afterEach(function () {
+            fixture.cleanup();
+        });
+
+        it('has ui', function () {
+            expect(board.ui).toEqual(jasmine.any(Object));
+            expect(Object.keys(board.ui)).toContain('container');
+            expect(Object.keys(board.ui)).toContain('content');
+            expect(Object.keys(board.ui)).toContain('doc');
+            expect(Object.keys(board.ui)).toContain('window');
+            expect(Object.keys(board.ui)).toContain('html');
+            expect(Object.keys(board.ui)).toContain('toolbar');
+            expect(Object.keys(board.ui)).toContain('sortables');
+            expect(Object.keys(board.ui)).toContain('plugins');
+            expect(Object.keys(board.ui)).toContain('render_model');
+            expect(Object.keys(board.ui)).toContain('placeholders');
+            expect(Object.keys(board.ui)).toContain('dragitems');
+            expect(Object.keys(board.ui)).toContain('dragareas');
+            expect(Object.keys(board.ui)).toContain('toolbarModeSwitcher');
+            expect(Object.keys(board.ui)).toContain('toolbarModeLinks');
+            expect(Object.keys(board.ui)).toContain('toolbarTrigger');
+        });
+
+        it('has no options', function () {
+            expect(board.options).toEqual(undefined);
+        });
     });
 
     describe('.show()', function () {
