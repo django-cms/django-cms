3ade7a2cac11c4a18ce0add3d7db4886ed575c88
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-30 13:01:41 +0100

    initial setup

---
+++ cms/tests/frontend/unit/cms.structureboard.test.js
@@ -0,0 +1,32 @@
+'use strict';
+
+describe('CMS.StructureBoard', function () {
+    fixture.setBase('cms/tests/frontend/unit/fixtures');
+
+    it('creates a StructureBoard class', function () {
+        expect(CMS.StructureBoard).toBeDefined();
+    });
+
+    it('has public API');
+
+    describe('instance', function () {
+        it('has ui');
+        it('has options');
+    });
+
+    describe('.show()', function () {
+
+    });
+
+    describe('.hide()', function () {
+
+    });
+
+    describe('.getId()', function () {
+
+    });
+
+    describe('.getIds()', function () {
+
+    });
+});
