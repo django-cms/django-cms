015eab7974f9cca71abfd68992a120c547c92842
Vadim Sikora <vadim.sikora@divio.ch>
2015-12-31 18:24:32 +0100

    tests for the instance

--- cms/tests/frontend/unit/cms.clipboard.test.js
+++ cms/tests/frontend/unit/cms.clipboard.test.js
@@ -1,3 +1,4 @@
+/* global document */
 'use strict';
 
 describe('CMS.Clipboard', function () {
@@ -7,14 +8,110 @@ describe('CMS.Clipboard', function () {
         expect(CMS.Clipboard).toBeDefined();
     });
 
-    it('has public API');
+    it('has public API', function () {
+        expect(CMS.Clipboard.prototype.clear).toEqual(jasmine.any(Function));
+    });
 
     describe('instance', function () {
-        it('has options');
-        it('has ui');
-        it('has its own private modal instance');
-        it('sets up events to open the modal');
-        it('sets up events to clear the clipboard');
+        var clipboard;
+        beforeEach(function (done) {
+            fixture.load('clipboard.html');
+            $(function () {
+                clipboard = new CMS.Clipboard();
+                done();
+            });
+        });
+
+        afterEach(function () {
+            fixture.cleanup();
+        });
+
+        it('has no options', function () {
+            expect(clipboard.options).toEqual(undefined);
+        });
+
+        it('has ui', function () {
+            expect(clipboard.ui).toEqual(jasmine.any(Object));
+            expect(Object.keys(clipboard.ui)).toContain('clipboard');
+            expect(Object.keys(clipboard.ui)).toContain('triggers');
+            expect(Object.keys(clipboard.ui)).toContain('triggerRemove');
+            expect(Object.keys(clipboard.ui)).toContain('pluginsList');
+            expect(Object.keys(clipboard.ui)).toContain('document');
+            expect(Object.keys(clipboard.ui).length).toEqual(5);
+        });
+
+        it('has its own private modal instance', function () {
+            expect(clipboard.modal).toEqual(jasmine.any(Object));
+            // there's no reliable way to check if it's really modal,
+            // since Class.js has no instanceof, but this will suffice
+            expect(clipboard.modal.ui.modal).toEqual(jasmine.any(Object));
+        });
+
+        it('sets up events to open the modal (enabled)', function () {
+            expect(clipboard.ui.triggers).toHandle('click.cms.clipboard');
+
+            spyOn(clipboard.modal, 'open');
+            clipboard.ui.triggers.trigger('click.cms.clipboard');
+            expect(clipboard.modal.open).toHaveBeenCalledWith({
+                html: clipboard.ui.pluginsList,
+                title: 'Clipboard',
+                width: 400,
+                height: 117
+            });
+        });
+
+        it('sets up events to open the modal (disabled)', function () {
+            expect(clipboard.ui.triggers).toHandle('click.cms.clipboard');
+            spyOn(clipboard.modal, 'open');
+            clipboard.ui.triggers.parent().addClass('cms-toolbar-item-navigation-disabled');
+            clipboard.ui.triggers.trigger('click.cms.clipboard');
+            expect(clipboard.modal.open).not.toHaveBeenCalled();
+        });
+
+        it('sets up events to open the modal which trigger click on document', function (done) {
+            expect(clipboard.ui.triggers).toHandle('click.cms.clipboard');
+            spyOn(clipboard.modal, 'open');
+            $(document).on('click.cms.toolbar', function () {
+                $(this).off('click.cms.toolbar');
+                done();
+            });
+
+            clipboard.ui.triggers.trigger('click.cms.clipboard');
+        });
+
+
+        it('sets up events to clear the clipboard (enabled)', function () {
+            spyOn(clipboard, 'clear').and.callFake(function (callback) {
+                callback();
+            });
+            spyOn(clipboard.modal, 'close');
+            expect(clipboard.ui.triggerRemove).toHandle('click.cms.clipboard');
+            expect(clipboard.ui.triggers.parent()).not.toHaveClass('cms-toolbar-item-navigation-disabled');
+            expect(clipboard.ui.triggerRemove.parent()).not.toHaveClass('cms-toolbar-item-navigation-disabled');
+            var click = spyOnEvent(clipboard.ui.document, 'click.cms.toolbar');
+
+            clipboard.ui.triggerRemove.trigger('click');
+            expect(clipboard.clear).toHaveBeenCalled();
+            expect(clipboard.modal.close).toHaveBeenCalled();
+            expect(clipboard.ui.triggers.parent()).toHaveClass('cms-toolbar-item-navigation-disabled');
+            expect(clipboard.ui.triggerRemove.parent()).toHaveClass('cms-toolbar-item-navigation-disabled');
+            expect(click).toHaveBeenTriggered();
+        });
+
+        it('sets up events to clear the clipboard (disabled)', function () {
+            spyOn(clipboard, 'clear').and.callFake(function (callback) {
+                callback();
+            });
+            spyOn(clipboard.modal, 'close');
+            var click = spyOnEvent(clipboard.ui.document, 'click.cms.toolbar');
+
+            clipboard.ui.triggerRemove.parent().addClass('cms-toolbar-item-navigation-disabled');
+
+            clipboard.ui.triggerRemove.trigger('click');
+            expect(clipboard.clear).not.toHaveBeenCalled();
+            expect(clipboard.modal.close).not.toHaveBeenCalled();
+            expect(click).not.toHaveBeenTriggered();
+        });
     });
 
     describe('.clear()', function () {
