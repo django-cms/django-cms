2e487984633490d00ea42d6026c1cf673b1bb6bc
Iacopo Spalletti <i.spalletti@nephila.it>
2016-01-04 19:16:26 +0100

    Remove CMS_APP_NAME and implement the check of #4192 by checking the view names

--- cms/utils/conf.py
+++ cms/utils/conf.py
@@ -62,7 +62,6 @@ DEFAULTS = {
     'TOOLBAR_URL__BUILD': 'build',
     'TOOLBAR_URL__DISABLE': 'toolbar_off',
     'ADMIN_NAMESPACE': 'admin',
-    'APP_NAME': None,
     'TOOLBAR_HIDE': False,
     'WIZARD_DEFAULT_TEMPLATE': constants.TEMPLATE_INHERITANCE_MAGIC,
     'WIZARD_CONTENT_PLUGIN': 'TextPlugin',
