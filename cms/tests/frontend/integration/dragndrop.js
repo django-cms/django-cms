f1af020f0bb0593361f0ea347778165d9fc367e6
Vadim Sikora <vadim.sikora@divio.ch>
2016-01-07 14:44:12 +0100

    wip

--- cms/tests/frontend/integration/dragndrop.js
+++ cms/tests/frontend/integration/dragndrop.js
@@ -4,27 +4,41 @@
 // Drag'n'drop plugins
 
 var globals = require('./settings/globals');
-var cms = require('./helpers/cms')();
-
+var casperjs = require('casper');
+var cms = require('./helpers/cms')(casperjs);
 
 casper.test.setUp(function (done) {
     casper.start()
         .then(cms.login())
-        .then(cms.addPage({ title: 'First page' }))
+        // .then(cms.addPage({ title: 'First page' }))
+        // actually creates 3 plugins - row > col + col
+        .then(cms.addPlugin({
+            type: 'GridPlugin',
+            content: {
+                id_create: 2,
+                id_create_size: 12
+            }
+        }))
+        .then(cms.addPlugin({
+            type: 'TextPlugin',
+            content: {
+                id_body: 'Test text'
+            }
+        }))
         .run(done);
 });
 
 casper.test.tearDown(function (done) {
     casper.start()
         .then(cms.removePage())
-        .then(cms.logout())
+        // .then(cms.logout())
         .run(done);
 });
 
 casper.test.begin('Drag plugin to another placeholder', function (test) {
     casper.start(globals.editUrl)
         .then(function () {
-            casper.echo('Hi');
+            test.assertElementCount('.cms-structure .cms-draggable', 4);
         })
         .run(function () {
             test.done();
