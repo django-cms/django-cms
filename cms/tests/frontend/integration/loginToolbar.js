ee5f1a79fc33d020d068705adabfae0593179e86
Vadim Sikora <vadim.sikora@divio.ch>
2016-01-15 15:42:32 +0100

    check that login works on every screen

--- cms/tests/frontend/integration/loginToolbar.js
+++ cms/tests/frontend/integration/loginToolbar.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // #############################################################################
-// User login via the CMS toolbar
+// User login via the CMS toolbar (on all kinds of screens)
 
 var globals = require('./settings/globals');
 var cms = require('./helpers/cms')();
@@ -21,18 +21,27 @@ casper.test.tearDown(function (done) {
         .run(done);
 });
 
-casper.test.begin('User Login (via Toolbar)', function (test) {
-    casper
-        .start(globals.editUrl)
-        .waitUntilVisible('.cms-toolbar-expanded', function () {
-            test.assertExists('.cms-toolbar .cms-form-login', 'The toolbar login form is available');
+[
+    [320,  480],  // mobile vertical
+    [480,  320],  // mobile horizontal
+    [768,  1024], // tablet vertical
+    [1024, 768],  // tablet horizontal
+    [1280, 1024]  // standard - it's important that the last one resets
+].forEach(function (dimensions) {
+    casper.test.begin('User Login (via Toolbar) ' + JSON.stringify(dimensions), function (test) {
+        casper
+            .start(globals.editUrl)
+            .viewport(dimensions[0], dimensions[1])
+            .waitUntilVisible('.cms-toolbar-expanded', function () {
+                test.assertExists('.cms-toolbar .cms-form-login', 'The toolbar login form is available');
 
-            this.fill('.cms-form-login', globals.credentials, true);
-        })
-        .waitForSelector('.cms-ready', function () {
-            test.assertExists('.cms-toolbar-item-navigation', 'Login via the admin form done');
-        })
-        .run(function () {
-            test.done();
-        });
+                this.fill('.cms-form-login', globals.credentials, true);
+            })
+            .waitForSelector('.cms-ready', function () {
+                test.assertExists('.cms-toolbar-item-navigation', 'Login via the toolbar done');
+            })
+            .run(function () {
+                test.done();
+            });
+    });
 });
