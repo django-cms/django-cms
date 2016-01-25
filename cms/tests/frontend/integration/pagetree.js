961a5743d63ea5159cf62e07ab4ad9f26488cce9
Vadim Sikora <vadim.sikora@divio.ch>
2016-01-25 11:26:19 +0100

    add a test that pages can be nested

--- cms/tests/frontend/integration/pagetree.js
+++ cms/tests/frontend/integration/pagetree.js
@@ -156,3 +156,50 @@ casper.test.begin('Pages can be reordered', function (test) {
             test.done();
         });
 });
+
+casper.test.begin('Pages can be nested', function (test) {
+    casper
+        .then(cms.addPage({ title: 'Homepage' }))
+        .then(cms.addPage({ title: 'Second' }))
+        .thenOpen(globals.baseUrl)
+        .then(cms.openSideframe())
+        // switch to sideframe
+        .withFrame(0, function () {
+            var drop;
+            casper.waitUntilVisible('.cms-pagetree', function () {
+                test.assertExists(
+                    xPath('//li[./a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]]' +
+                          '/following-sibling::li[./a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    'Pages are in correct order initially'
+                );
+
+                // usually to drag stuff in the iframe you have to calculate the position of the frame
+                // and then the position of the thing inside frame, but here sideframe is opened at 0, 0
+                // so this should be enough
+                drop = this.getElementBounds(
+                    xPath('//a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]')
+                );
+
+                this.mouse.down(xPath('//a[contains(@class, "jstree-anchor")][contains(text(), "Second")]'));
+                this.mouse.move(drop.left + drop.width / 2, drop.top + drop.height / 2);
+            }).then(function () {
+                this.mouse.up(drop.left + drop.width / 2, drop.top + drop.height / 2);
+
+                test.assertExists(
+                    xPath('//a[contains(text(), "Homepage")]' +
+                          '/following-sibling::ul[contains(@class, "jstree-children")]' +
+                          '[./li/a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    'Second page is now nested into the Homepage'
+                );
+            }).waitForResource(/move-page/, function () {
+                test.assertExists('.cms-tree-node-success', 'Success icon showed up');
+            }).wait(1050, function () {
+                test.assertDoesntExist('.cms-tree-node-success', 'Success icon hides after some time');
+            });
+        })
+        .then(cms.removePage({ title: 'Homepage' }))
+        .then(cms.removePage({ title: 'Second' }))
+        .run(function () {
+            test.done();
+        });
+});
