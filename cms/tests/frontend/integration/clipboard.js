9658cc08593a388b527e0a4de8d6c29ed06a37b5
Vadim Sikora <vadim.sikora@divio.ch>
2016-01-27 10:32:45 +0100

    wip clipboard copy all from placeholder test

--- cms/tests/frontend/integration/clipboard.js
+++ cms/tests/frontend/integration/clipboard.js
@@ -12,30 +12,30 @@ var xPath = casperjs.selectXPath;
 casper.test.setUp(function (done) {
     casper.start()
         .then(cms.login())
-        .then(cms.addPage({ title: 'First page' }))
-        .then(cms.addPlugin({
-            type: 'TextPlugin',
-            content: {
-                id_body: 'Test text'
-            }
-        }))
-        .then(cms.addPlugin({
-            type: 'TextPlugin',
-            content: {
-                id_body: 'Another Test text'
-            }
-        }))
+        // .then(cms.addPage({ title: 'First page' }))
+        // .then(cms.addPlugin({
+        //     type: 'TextPlugin',
+        //     content: {
+        //         id_body: 'Test text'
+        //     }
+        // }))
+        // .then(cms.addPlugin({
+        //     type: 'TextPlugin',
+        //     content: {
+        //         id_body: 'Another Test text'
+        //     }
+        // }))
         .run(done);
 });
 
 casper.test.tearDown(function (done) {
     casper.start()
-        .then(cms.removePage())
+        // .then(cms.removePage())
         .then(cms.logout())
         .run(done);
 });
 
-casper.test.begin('Copy plugin from the structure board', function (test) {
+0 && casper.test.begin('Copy plugin from the structure board', function (test) {
     var contentNumber;
 
     casper
@@ -146,7 +146,141 @@ casper.test.begin('Copy plugin from the structure board', function (test) {
         });
 });
 
-casper.test.begin('Plugins with parent restriction cannot be pasted ' +
+casper.test.begin('Copy placeholder contents from the structure board', function (test) {
+    var contentNumber;
+
+    casper
+        .start(globals.editUrl)
+        // go to the Structure mode
+        .then(cms.switchTo('structure'))
+        .waitUntilVisible('.cms-structure', function () {
+            // save initial number of content plugins
+            contentNumber = this.evaluate(function () {
+                return document.querySelectorAll('.cms-structure-content .cms-draggable').length;
+            });
+            // click settings for last content plugin
+            this.click('.cms-structure .cms-dragarea:nth-child(2) .cms-submenu-settings');
+        })
+        // check that there is nothing in the clipboard
+        .then(function () {
+            test.assertElementCount('.cms-clipboard .cms-plugin', 0, 'No plugins in clipboard');
+        })
+        // try to copy contents of empty placeholder
+        .waitUntilVisible(
+            '.cms-structure .cms-dragarea:nth-child(2) .cms-submenu-item a[data-rel="copy"]',
+            function () {
+                this.click('.cms-structure .cms-dragarea:nth-child(2) .cms-submenu-item a[data-rel="copy"]');
+            }
+        )
+        .then(function () {
+            test.assertVisible('.cms-structure .cms-dragarea:nth-child(2) ' +
+                               '.cms-submenu-item a[data-rel="copy"]', 'Nothing happens');
+        })
+        .wait(2000, function () {
+            test.assertVisible('.cms-structure .cms-dragarea:nth-child(2) ' +
+                               '.cms-submenu-item a[data-rel="copy"]', 'Nothing happens for some time');
+        })
+        // try to copy contents of non-empty placeholder
+        .then(function () {
+            this.click('.cms-structure .cms-dragarea:nth-child(1) .cms-submenu-settings');
+        })
+        // check that there is still nothing in the clipboard
+        .then(function () {
+            test.assertElementCount('.cms-clipboard .cms-plugin', 0, 'No plugins in clipboard');
+        })
+        // try to copy contents of empty placeholder
+        .waitUntilVisible(
+            '.cms-structure .cms-dragarea:nth-child(1) .cms-submenu-item a[data-rel="copy"]',
+            function () {
+                this.click('.cms-structure .cms-dragarea:nth-child(1) .cms-submenu-item a[data-rel="copy"]');
+            }
+        )
+
+        .waitForResource(/copy-plugins/)
+
+        // check that there is something now in the clipboard
+        .then(function () {
+            test.assertElementCount('.cms-clipboard .cms-plugin', 1, '1 plugin in clipboard');
+        })
+
+        // .waitForSelector('.cms-toolbar-expanded')
+        // .then(function () {
+        //     // click on "Example.com" menu item
+        //     this.click('.cms-toolbar-item-navigation > li:nth-child(1) > a');
+        // })
+        // // opening "Clipboard" menu item
+        // .wait(10, function () {
+        //     this.click(
+        //         xPath('//a[.//span[text()[contains(.,"Clipboard")]]]')
+        //     );
+        // })
+        //
+        // // wait until clipboard modal is open
+        // .waitUntilVisible('.cms-modal-frame .cms-clipboard-containers', function () {
+        //     var placeholder = this.getElementBounds('.cms-dragarea:nth-child(2) .cms-draggables');
+        //
+        //     this.evaluate(function () {
+        //         // changing delay here because casper doesn't understand
+        //         // that we want to wait 100 ms after "picking up" the plugin
+        //         // for sortable to work
+        //         CMS.API.StructureBoard.ui.sortables.nestedSortable('option', 'delay', 0);
+        //     });
+        //
+        //     this.mouse.down('.cms-clipboard-containers .cms-draggable');
+        //     this.mouse.move(placeholder.left + placeholder.width / 2, placeholder.top + placeholder.height * 0);
+        // }).then(function () {
+        //     this.mouse.up('.cms-dragarea:nth-child(2) .cms-draggables');
+        //     // check before reload
+        //     test.assertElementCount(
+        //         '.cms-structure .cms-draggables .cms-draggable',
+        //         contentNumber + 1,
+        //         'Copy plugin successful'
+        //     );
+        // })
+        // .waitForResource(/move-plugin/)
+        // .then(function () {
+        //     return this.reload();
+        // })
+        // // check if number of content plugins has been increased (after reload)
+        // .then(function () {
+        //     test.assertElementCount(
+        //         '.cms-structure .cms-dragarea:nth-child(1) .cms-draggables .cms-draggable',
+        //         contentNumber,
+        //         'First placeholder has 2 plugins'
+        //     );
+        //
+        //     test.assertElementCount(
+        //         '.cms-structure .cms-dragarea:nth-child(2) .cms-draggables .cms-draggable',
+        //         1,
+        //         'Second placeholder has one plugin'
+        //     );
+        // })
+        //
+        // .then(function () {
+        //     // click on "Example.com" menu item
+        //     this.click('.cms-toolbar-item-navigation > li:nth-child(1) > a');
+        // })
+        // // opening "Clipboard" menu item
+        // .wait(10, function () {
+        //     this.click(
+        //         xPath('//a[.//span[text()[contains(.,"Clear clipboard")]]]')
+        //     );
+        // })
+        // .waitForResource(/clear/)
+        // .then(function () {
+        //     return this.reload();
+        // })
+        // // check that there is nothing now in the clipboard
+        // .then(function () {
+        //     test.assertElementCount('.cms-clipboard .cms-plugin', 0, 'Clipboard is now empty');
+        // })
+
+        .run(function () {
+            test.done();
+        });
+});
+
+0 && casper.test.begin('Plugins with parent restriction cannot be pasted ' +
                   'in incorrect parents (paste button)', function (test) {
     casper.start(globals.editUrl)
         .waitUntilVisible('.cms-toolbar-expanded')
@@ -265,7 +399,7 @@ casper.test.begin('Plugins with parent restriction cannot be pasted ' +
         });
 });
 
-casper.test.begin('Plugins with parent restriction cannot be pasted ' +
+0 && casper.test.begin('Plugins with parent restriction cannot be pasted ' +
                   'in incorrect parents (dragndrop)', function (test) {
     var column;
     var row;
@@ -407,7 +541,7 @@ casper.test.begin('Plugins with parent restriction cannot be pasted ' +
         });
 });
 
-casper.test.begin('Plugins with child restriction cannot accept other children (paste button)', function (test) {
+0 && casper.test.begin('Plugins with child restriction cannot accept other children (paste button)', function (test) {
     casper.start(globals.editUrl)
         .waitUntilVisible('.cms-toolbar-expanded')
         // creates 3 plugins - row > col + col
@@ -531,7 +665,7 @@ casper.test.begin('Plugins with child restriction cannot accept other children (
         });
 });
 
-casper.test.begin('Plugins with child restriction cannot accept other children (dragndrop)', function (test) {
+0 && casper.test.begin('Plugins with child restriction cannot accept other children (dragndrop)', function (test) {
     var row;
     var column;
 
