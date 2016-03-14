737d7aa19de09a7cdc093068b9eaedb98fcb8b54
Vadim Sikora <vadim.sikora@divio.ch>
2016-03-14 18:33:44 +0100

    use the function to generate XPath
    much more clear what's actually happening

--- cms/tests/frontend/integration/pagetree.js
+++ cms/tests/frontend/integration/pagetree.js
@@ -142,14 +142,8 @@ casper.test.begin('Pages can be added through the page tree', function (test) {
             .then(function () {
                 var pageId = cms.getPageId('Nested page');
                 test.assertExists(
-                    xPath('//a[contains(@class, "jstree-anchor")][contains(text(), "Nested page")]'),
-                    'Nested page was successfully added'
-                );
-                test.assertExists(
-                    xPath('//a[contains(text(), "Homepage")]' +
-                          '/following-sibling::ul[contains(@class, "jstree-children")]' +
-                          '[./li/a[contains(@class, "jstree-anchor")][contains(text(), "Nested page")]]'),
-                    'Newly created page is indeed nested'
+                    xPath(createJSTreeXPathFromTree([{ name: 'Homepage', children: [{ name: 'Nested page' }] }])),
+                    'Newly created page is added'
                 );
                 test.assertExists(
                     '.cms-tree-item-lang a[href*="' + pageId + '"] span.empty',
@@ -174,8 +168,10 @@ casper.test.begin('Pages can be reordered', function (test) {
             var drop;
             casper.waitUntilVisible('.cms-pagetree', function () {
                 test.assertExists(
-                    xPath('//li[./a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]]' +
-                          '/following-sibling::li[./a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        { name: 'Homepage' },
+                        { name: 'Second' }
+                    ])),
                     'Pages are in correct order initially'
                 );
 
@@ -191,9 +187,10 @@ casper.test.begin('Pages can be reordered', function (test) {
             }).then(function () {
                 this.mouse.up(drop.left + drop.width / 2, drop.top + drop.height - 3);
                 test.assertExists(
-                    xPath('//li[./a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]' +
-                          '/following-sibling::li' +
-                          '[./a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        { name: 'Second' },
+                        { name: 'Homepage' }
+                    ])),
                     'Pages are in correct order after move'
                 );
             }).waitForResource(/move-page/, function () {
@@ -204,9 +201,10 @@ casper.test.begin('Pages can be reordered', function (test) {
                 this.reload();
             }).waitUntilVisible('.cms-pagetree', function () {
                 test.assertExists(
-                    xPath('//li[./a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]' +
-                          '/following-sibling::li' +
-                          '[./a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        { name: 'Second' },
+                        { name: 'Homepage' }
+                    ])),
                     'Pages are in correct order after move'
                 );
             });
@@ -229,8 +227,10 @@ casper.test.begin('Pages can be nested / unnested', function (test) {
             var drop;
             casper.waitUntilVisible('.cms-pagetree', function () {
                 test.assertExists(
-                    xPath('//li[./a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]]' +
-                          '/following-sibling::li[./a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        { name: 'Homepage' },
+                        { name: 'Second' }
+                    ])),
                     'Pages are in correct order initially'
                 );
 
@@ -247,9 +247,14 @@ casper.test.begin('Pages can be nested / unnested', function (test) {
                 this.mouse.up(drop.left + drop.width / 2, drop.top + drop.height / 2);
 
                 test.assertExists(
-                    xPath('//a[contains(text(), "Homepage")]' +
-                          '/following-sibling::ul[contains(@class, "jstree-children")]' +
-                          '[./li/a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        {
+                            name: 'Homepage',
+                            children: [{
+                                name: 'Second'
+                            }]
+                        }
+                    ])),
                     'Second page is now nested into the Homepage'
                 );
             })
@@ -260,9 +265,14 @@ casper.test.begin('Pages can be nested / unnested', function (test) {
             })
             .waitUntilVisible('.cms-pagetree', function () {
                 test.assertExists(
-                    xPath('//a[contains(text(), "Homepage")]' +
-                          '/following-sibling::ul[contains(@class, "jstree-children")]' +
-                          '[./li/a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        {
+                            name: 'Homepage',
+                            children: [{
+                                name: 'Second'
+                            }]
+                        }
+                    ])),
                     'Second page is now nested into the Homepage after reload'
                 );
                 drop = this.getElementBounds(
@@ -274,8 +284,10 @@ casper.test.begin('Pages can be nested / unnested', function (test) {
             }).then(function () {
                 this.mouse.up(drop.left + 10, drop.top + drop.height - 1);
                 test.assertExists(
-                    xPath('//li[./a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]]' +
-                          '/following-sibling::li[./a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        { name: 'Homepage' },
+                        { name: 'Second' }
+                    ])),
                     'Pages are back in their initial order'
                 );
             })
@@ -286,8 +298,10 @@ casper.test.begin('Pages can be nested / unnested', function (test) {
             })
             .waitUntilVisible('.cms-pagetree', function () {
                 test.assertExists(
-                    xPath('//li[./a[contains(@class, "jstree-anchor")][contains(text(), "Homepage")]]' +
-                          '/following-sibling::li[./a[contains(@class, "jstree-anchor")][contains(text(), "Second")]]'),
+                    xPath(createJSTreeXPathFromTree([
+                        { name: 'Homepage' },
+                        { name: 'Second' }
+                    ])),
                     'Pages are back in their initial order after reload'
                 );
             });
