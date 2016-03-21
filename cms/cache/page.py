c089a988fb6195344c2bf4c20d8e90e305507b57
Martin Koistinen <mkoistinen@gmail.com>
2016-03-21 01:36:24 -0400

    Implement CMSPluginBase.get_expiration()

--- cms/cache/page.py
+++ cms/cache/page.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
+
 import hashlib
+
+from datetime import timedelta
+
 from django.conf import settings
-from django.utils.cache import add_never_cache_headers
+from django.utils.cache import add_never_cache_headers, patch_response_headers
 from django.utils.encoding import iri_to_uri, force_text
-from django.utils.timezone import get_current_timezone_name
+from django.utils.timezone import now, get_current_timezone_name
 
 from cms.cache import _get_cache_version, _set_cache_version, _get_cache_key
+from cms.constants import EXPIRE_NOW, MAX_EXPIRATION_TTL
 from cms.utils import get_cms_setting
 
 
@@ -29,41 +34,68 @@ def _page_cache_key(request):
 def set_page_cache(response):
     from django.core.cache import cache
 
-    if not get_cms_setting('PAGE_CACHE'):
-        return response
     request = response._request
-    save_cache = True
-    for placeholder in getattr(request, 'placeholders', []):
-        if not placeholder.cache_placeholder:
-            save_cache = False
-            break
-    if hasattr(request, 'toolbar'):
-        if request.toolbar.edit_mode or request.toolbar.show_toolbar:
-            save_cache = False
-    if request.user.is_authenticated():
-        save_cache = False
-    if not save_cache:
-        add_never_cache_headers(response)
-        return response
-    else:
-        version = _get_cache_version()
-        ttl = get_cms_setting('CACHE_DURATIONS')['content']
 
-        cache.set(
-            _page_cache_key(request),
-            (response.content, response._headers),
-            ttl,
-            version=version
-        )
-        # See note in invalidate_cms_page_cache()
-        _set_cache_version(version)
+    if get_cms_setting("PAGE_CACHE") and (
+            not hasattr(request, 'toolbar') or (
+                not request.toolbar.edit_mode and
+                not request.toolbar.show_toolbar and
+                not request.user.is_authenticated()
+            )):
+
+        # This *must* be TZ-aware
+        timestamp = now()
+
+        placeholders = request.current_page.get_placeholders()
+
+        # Checks if there's a plugin using the legacy "cache = False"
+        if all(ph.cache_placeholder for ph in placeholders):
+            placeholder_ttl_list = [
+                # get_expiration() always returns:
+                #     EXPIRE_NOW <= int <= MAX_EXPIRATION_IN_SECONDS
+                ph.get_expiration(request, timestamp)
+                for ph in placeholders
+            ]
+
+            if EXPIRE_NOW not in placeholder_ttl_list:
+                if placeholder_ttl_list:
+                    min_placeholder_ttl = min(x for x in placeholder_ttl_list)
+                else:
+                    # Should only happen when there are no placeholders at all
+                    min_placeholder_ttl = MAX_EXPIRATION_TTL
+                ttl = min(
+                    get_cms_setting('CACHE_DURATIONS')['content'],
+                    min_placeholder_ttl
+                )
+
+                if ttl > 0:
+                    # Adds expiration, etc. to headers
+                    patch_response_headers(response, cache_timeout=ttl)
+                    version = _get_cache_version()
+                    # We also store the absolute expiration timestamp to avoid
+                    # recomputing it on cache-reads.
+                    expires_datetime = timestamp + timedelta(seconds=ttl)
+                    cache.set(
+                        _page_cache_key(request),
+                        (
+                            response.content,
+                            response._headers,
+                            expires_datetime,
+                        ),
+                        ttl,
+                        version=version
+                    )
+                    # See note in invalidate_cms_page_cache()
+                    _set_cache_version(version)
+                    return response
+
+    add_never_cache_headers(response)
+    return response
 
 
 def get_page_cache(request):
     from django.core.cache import cache
-
-    return cache.get(_page_cache_key(request),
-                     version=_get_cache_version())
+    return cache.get(_page_cache_key(request), version=_get_cache_version())
 
 
 def get_xframe_cache(page):
