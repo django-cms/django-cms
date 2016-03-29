df60372a02bc542b4b693504bcf9498221e7d7ea
Vadim Sikora <vadim.sikora@divio.ch>
2016-03-29 10:08:55 +0200

    add more tests for toolbar

--- cms/tests/frontend/unit/cms.toolbar.test.js
+++ cms/tests/frontend/unit/cms.toolbar.test.js
@@ -631,4 +631,415 @@ describe('CMS.Toolbar', function () {
             expect(CMS.API.locked).toEqual(false);
         });
     });
+
+    describe('._events()', function () {
+        var toolbar;
+        beforeEach(function (done) {
+            fixture.load('toolbar.html');
+            CMS.config = {
+                lang: {
+                    publish: 'publish?'
+                }
+            };
+            CMS.settings = {
+                toolbar: 'expanded'
+            };
+            spyOn(CMS.Navigation.prototype, 'initialize').and.callFake(function () {
+                return {};
+            });
+            $(function () {
+                toolbar = new CMS.Toolbar();
+                done();
+            });
+        });
+
+        afterEach(function () {
+            fixture.cleanup();
+        });
+
+        it('attaches event handlers to toolbar trigger', function () {
+            toolbar.ui.toolbarTrigger.off(toolbar.pointerUp);
+            toolbar.ui.toolbarTrigger.off(toolbar.click);
+
+            toolbar._events();
+
+            expect(toolbar.ui.toolbarTrigger).toHandle(toolbar.pointerUp);
+            expect(toolbar.ui.toolbarTrigger).toHandle(toolbar.click);
+
+            spyOn(toolbar, 'toggle');
+
+            toolbar.ui.document.off(toolbar.click);
+            toolbar.ui.document.on(toolbar.click, function () {
+                expect(toolbar.toggle).toHaveBeenCalled();
+            });
+
+            toolbar.ui.toolbarTrigger.trigger(toolbar.pointerUp);
+            toolbar.ui.toolbarTrigger.trigger(toolbar.click);
+        });
+
+        it('attaches event handlers to navigation menu links', function () {
+            spyOn(toolbar, '_delegate');
+
+            var emptyLink = $(toolbar.ui.navigations[0]).find('a').eq(0);
+            var pagesLink = $(toolbar.ui.navigations[0]).find('a').eq(1);
+
+            emptyLink.trigger('click');
+            expect(toolbar._delegate).not.toHaveBeenCalled();
+
+            pagesLink.trigger('click');
+
+            expect(toolbar._delegate).toHaveBeenCalledTimes(1);
+            expect(toolbar._delegate).toHaveBeenCalledWith(pagesLink);
+        });
+
+        it('attaches event handlers to navigation menu links (to open in new window)', function () {
+            var fakeWindow = {
+                open: jasmine.createSpy()
+            };
+            spyOn(toolbar, '_delegate');
+            spyOn(CMS.API.Helpers, '_getWindow').and.callFake(function () {
+                return fakeWindow;
+            });
+
+            var emptyLink = $(toolbar.ui.navigations[0]).find('a').eq(0);
+            var pagesLink = $(toolbar.ui.navigations[0]).find('a').eq(1);
+
+            emptyLink.trigger($.Event('keydown', { keyCode: CMS.KEYS.CTRL }));
+            emptyLink.trigger('click');
+            expect(toolbar._delegate).not.toHaveBeenCalled();
+            expect(fakeWindow.open).not.toHaveBeenCalled();
+
+            emptyLink.trigger($.Event('keydown', { keyCode: CMS.KEYS.CTRL }));
+            pagesLink.trigger('click');
+            expect(toolbar._delegate).not.toHaveBeenCalledTimes(1);
+            expect(fakeWindow.open).toHaveBeenCalledWith(jasmine.stringMatching('cms/page'), '_blank');
+
+            pagesLink.trigger('keyup');
+        });
+
+        it('attaches event handlers to navigation menu lists', function () {
+            var firstMenuItem = $(toolbar.ui.navigations.find('> li')[0]);
+            var secondMenuItem = $(toolbar.ui.navigations.find('> li')[1]);
+            toolbar.ui.structureBoard = $('<div></div>');
+
+            expect(toolbar.ui.document).not.toHandle(toolbar.click);
+            expect(toolbar.ui.structureBoard).not.toHandle(toolbar.click);
+            expect(toolbar.ui.toolbar).not.toHandle(toolbar.click);
+            expect(toolbar.ui.window).not.toHandle(toolbar.resize + '.menu.reset');
+            firstMenuItem.trigger('click');
+            expect(toolbar.ui.document).toHandle(toolbar.click);
+            expect(toolbar.ui.structureBoard).toHandle(toolbar.click);
+            expect(toolbar.ui.toolbar).toHandle(toolbar.click);
+            expect(toolbar.ui.window).toHandle(toolbar.resize + '.menu.reset');
+        });
+
+
+        it('handles mousemove over top level toolbar items', function () {
+            var firstMenuItem = $(toolbar.ui.navigations.find('> li')[0]);
+            var secondMenuItem = $(toolbar.ui.navigations.find('> li')[1]);
+            var thirdMenuItem = $(toolbar.ui.navigations.find('> li')[2]);
+
+            firstMenuItem.trigger('click');
+
+            var clickFirstMenuItem = spyOnEvent(firstMenuItem, toolbar.click);
+            var clickSecondMenuItem = spyOnEvent(secondMenuItem, toolbar.click);
+            var clickThirdMenuItem = spyOnEvent(thirdMenuItem, toolbar.click);
+
+            firstMenuItem.trigger('mouseenter');
+            expect(clickFirstMenuItem).not.toHaveBeenTriggered();
+
+            secondMenuItem.trigger('mouseenter');
+            expect(clickSecondMenuItem).toHaveBeenTriggered();
+
+            thirdMenuItem.trigger('mouseenter');
+            expect(clickThirdMenuItem).toHaveBeenTriggered();
+        });
+
+        it('does not care for mouse over top level items if touch is enabled', function () {
+            var firstMenuItem = $(toolbar.ui.navigations.find('> li')[0]);
+            var secondMenuItem = $(toolbar.ui.navigations.find('> li')[1]);
+
+            firstMenuItem.find('a').trigger('touchstart');
+            firstMenuItem.trigger('click');
+
+            var clickFirstMenuItem = spyOnEvent(firstMenuItem, toolbar.click);
+            var clickSecondMenuItem = spyOnEvent(secondMenuItem, toolbar.click);
+
+            firstMenuItem.trigger('mouseenter');
+            expect(clickFirstMenuItem).not.toHaveBeenTriggered();
+            secondMenuItem.trigger('mouseenter');
+            expect(clickSecondMenuItem).not.toHaveBeenTriggered();
+        });
+
+        it('closes the menu item if it is open', function () {
+            var firstMenuItem = $(toolbar.ui.navigations.find('> li')[0]);
+            var secondMenuItem = $(toolbar.ui.navigations.find('> li')[1]);
+
+            firstMenuItem.trigger('click');
+            expect(firstMenuItem).toHaveClass('cms-toolbar-item-navigation-hover');
+
+            firstMenuItem.trigger('click');
+            expect(firstMenuItem).not.toHaveClass('cms-toolbar-item-navigation-hover');
+        });
+
+        it('handles mousemove over nested toolbar items', function () {
+            var menuItem = $(toolbar.ui.navigations.find('> li')[1]);
+            var subMenu = menuItem.find('> ul');
+            var childrenSubMenuItem = subMenu.find('> li').eq(0);
+            var disabledSubMenuItem = subMenu.find('> li').eq(1);
+            var normalSubMenuItem = subMenu.find('> li').eq(2);
+
+            spyOn($.fn, 'show').and.callThrough();
+
+            expect(childrenSubMenuItem.find('> ul')).not.toBeVisible();
+            childrenSubMenuItem.trigger('pointerover');
+            expect($.fn.show).toHaveBeenCalledTimes(1);
+            expect($.fn.show.calls.mostRecent().object).toEqual(childrenSubMenuItem.find('> ul'));
+        });
+
+        it('attaches event handlers to buttons with data-rel', function () {
+            spyOn(toolbar, '_delegate');
+            var createButton = toolbar.ui.buttons.eq(0).find('a');
+            createButton.trigger(toolbar.click);
+            expect(toolbar._delegate).toHaveBeenCalled();
+        });
+
+        it('attaches event handlers to buttons without data-rel', function () {
+            var createButton = toolbar.ui.buttons.eq(1).find('a:first');
+            spyOn($.Event.prototype, 'stopPropagation');
+            createButton.trigger(toolbar.click);
+            expect($.Event.prototype.stopPropagation).toHaveBeenCalled();
+        });
+
+        it('attaches a handler to publish page button', function () {
+            spyOn($, 'ajax');
+            spyOn($.Event.prototype, 'preventDefault');
+            spyOn(CMS.API.Helpers, 'secureConfirm').and.returnValues(false, true);
+
+            var publishButton = toolbar.ui.buttons.eq(3).find('.cms-publish-page');
+            publishButton.trigger(toolbar.click);
+            expect($.Event.prototype.preventDefault).toHaveBeenCalledTimes(2); // two handlers on same button
+            publishButton.trigger(toolbar.click);
+            expect($.Event.prototype.preventDefault).toHaveBeenCalledTimes(3); // one handler on same button
+        });
+
+        it('attaches a handler to publish button');
+    });
+
+    describe('._screenBlock()', function () {
+        var toolbar;
+        beforeEach(function (done) {
+            fixture.load('toolbar.html');
+            CMS.config = {};
+            CMS.settings = {
+                toolbar: 'expanded'
+            };
+            spyOn(CMS.Navigation.prototype, 'initialize').and.callFake(function () {
+                return {};
+            });
+            $(function () {
+                toolbar = new CMS.Toolbar();
+                toolbar.ui.window = $('<div></div>');
+                toolbar.ui.screenBlock = $('<div></div>');
+                spyOn($.fn, 'css').and.callThrough();
+                done();
+            });
+        });
+
+        afterEach(function () {
+            var timeoutId = setInterval(function () {
+            }, 10);
+
+            for (var i; i <= timeoutId; i++) {
+                clearInterval(i);
+            }
+            fixture.cleanup();
+        });
+
+        it('creates resize handler to resize the blocker element', function () {
+            expect(toolbar.ui.window).not.toHandle(toolbar.resize);
+            toolbar._screenBlock();
+            expect($.fn.css).toHaveBeenCalledWith({
+                width: 0,
+                height: 0
+            });
+            toolbar.ui.window.css({
+                width: 200,
+                height: 200
+            });
+
+            $.fn.css.calls.reset();
+            toolbar.ui.window.trigger(toolbar.resize);
+            expect($.fn.css).toHaveBeenCalledWith({
+                width: 200,
+                height: 200
+            });
+            expect(toolbar.ui.window).toHandle(toolbar.resize);
+        });
+        it('sets the interval to trigger resize', function () {
+            jasmine.clock().install();
+            toolbar._screenBlock();
+            expect($.fn.css).toHaveBeenCalledTimes(1);
+            jasmine.clock().tick(20);
+            expect($.fn.css).toHaveBeenCalledTimes(2);
+            jasmine.clock().tick(20);
+            expect($.fn.css).toHaveBeenCalledTimes(3);
+            jasmine.clock().uninstall();
+        });
+    });
+
+    describe('._delegate()', function () {
+        var toolbar;
+        var fakeWindow;
+        beforeEach(function (done) {
+            fixture.load('toolbar.html');
+            CMS.config = {};
+            CMS.settings = {
+                toolbar: 'expanded'
+            };
+            fakeWindow = {
+                location: {
+                    href: ''
+                }
+            };
+            spyOn(CMS.Navigation.prototype, 'initialize').and.callFake(function () {
+                return {};
+            });
+            $(function () {
+                toolbar = new CMS.Toolbar();
+                spyOn(CMS.Modal.prototype, 'initialize');
+                spyOn(CMS.Sideframe.prototype, 'initialize');
+                spyOn(toolbar, 'openAjax');
+                spyOn(CMS.API.Helpers, '_getWindow').and.returnValue(fakeWindow);
+                spyOn(CMS.API.Messages, 'open');
+                done();
+            });
+        });
+
+        afterEach(function () {
+            fixture.cleanup();
+        });
+
+        it('return false if item is disabled', function () {
+            expect(toolbar._delegate($('<div class="cms-btn-disabled"></div>'))).toEqual(false);
+        });
+        it('opens modal if item is "modal"', function () {
+            var modalOpen = jasmine.createSpy();
+            CMS.Modal.prototype.initialize.and.callFake(function (opts) {
+                expect(opts.onClose).toEqual('test');
+                return {
+                    open: modalOpen
+                };
+            });
+
+            toolbar._delegate($('<div href="href" data-name="modal" data-rel="modal" data-on-close="test"></div>'));
+
+            expect(modalOpen).toHaveBeenCalledWith({
+                url: 'href',
+                title: 'modal'
+            });
+        });
+
+        it('opens sideframe if item is "sideframe"', function () {
+            var sideframeOpen = jasmine.createSpy();
+            CMS.Sideframe.prototype.initialize.and.callFake(function (opts) {
+                expect(opts.onClose).toEqual('test2');
+                return {
+                    open: sideframeOpen
+                };
+            });
+
+            toolbar._delegate(
+                $('<div href="href2" data-name="modal" data-rel="sideframe" data-on-close="test2"></div>')
+            );
+
+            expect(sideframeOpen).toHaveBeenCalledWith({
+                url: 'href2',
+                animate: true
+            });
+        });
+
+        it('opens message if item is "message"', function () {
+            toolbar._delegate($('<div data-rel="message" data-text="message!"></div>'));
+
+            expect(CMS.API.Messages.open).toHaveBeenCalledWith({
+                message: 'message!'
+            });
+        });
+
+        it('opens ajax request if item is "ajax"', function () {
+            toolbar._delegate(
+                $('<div href="href" data-rel="ajax" ' +
+                  'data-post=\'{ "test": "shmest" }\' data-text="text" data-on-success="REFRESH"></div>')
+            );
+
+            expect(toolbar.openAjax).toHaveBeenCalledWith({
+                url: 'href',
+                post: '{"test":"shmest"}',
+                text: 'text',
+                onSuccess: 'REFRESH'
+            });
+        });
+
+        it('just redirects to the page if nothing else', function () {
+            expect(fakeWindow.location.href).toEqual('');
+            toolbar._delegate(
+                $('<div href="href"></div>')
+            );
+            expect(fakeWindow.location.href).toEqual('href');
+        });
+    });
+
+    describe('._debug()', function () {
+        var toolbar;
+        beforeEach(function (done) {
+            fixture.load('toolbar.html');
+            CMS.config = {
+                lang: {
+                    debug: 'DEBUG!'
+                }
+            };
+            CMS.settings = {
+                toolbar: 'expanded'
+            };
+            spyOn(CMS.Navigation.prototype, 'initialize').and.callFake(function () {
+                return {};
+            });
+            $(function () {
+                toolbar = new CMS.Toolbar();
+                toolbar.ui.container.append('<div class="cms-debug-bar"></div>');
+
+                spyOn(CMS.API.Messages, 'open');
+                jasmine.clock().install();
+                done();
+            });
+        });
+
+        afterEach(function () {
+            jasmine.clock().uninstall();
+            fixture.cleanup();
+        });
+
+        it('shows debug information after timeout', function () {
+            toolbar._debug();
+
+            toolbar.ui.container.find('.cms-debug-bar').trigger(toolbar.mouseEnter);
+            jasmine.clock().tick(10000);
+
+            expect(CMS.API.Messages.open).toHaveBeenCalledWith({
+                message: 'DEBUG!'
+            });
+        });
+
+        it('does not show debug information if not hovering after timeout', function () {
+            toolbar._debug();
+
+            toolbar.ui.container.find('.cms-debug-bar').trigger(toolbar.mouseEnter);
+            jasmine.clock().tick(500);
+            toolbar.ui.container.find('.cms-debug-bar').trigger(toolbar.mouseLeave);
+            jasmine.clock().tick(10000);
+
+            expect(CMS.API.Messages.open).not.toHaveBeenCalled();
+        });
+    });
 });
