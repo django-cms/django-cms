852c5062227aafb11d777db516ee90127fc79178
Vadim Sikora <vadim.sikora@divio.ch>
2016-04-19 11:19:46 +0200

    use `html` instead of `text` method in the message open
    modern browsers will use escaped version of the text, meaning
    &lt;script&gt;alert(1);&lt;/script&gt; would actually render script
    tag with an alert in the message

--- cms/static/cms/js/modules/cms.modal.js
+++ cms/static/cms/js/modules/cms.modal.js
@@ -904,7 +904,7 @@ var CMS = window.CMS || {};
                 messages = messageList.find('li');
                 if (messages.length) {
                     CMS.API.Messages.open({
-                        message: messages.eq(0).text()
+                        message: messages.eq(0).html()
                     });
                 }
                 messageList.remove();
