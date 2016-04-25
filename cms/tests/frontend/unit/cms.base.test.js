33882e3b2267a2dab60fd7bace202f57134200f1
Vadim Sikora <vadim.sikora@divio.ch>
2016-04-25 16:19:19 +0200

    fix unit tests

--- cms/tests/frontend/unit/cms.base.test.js
+++ cms/tests/frontend/unit/cms.base.test.js
@@ -23,7 +23,6 @@ describe('cms.base.js', function () {
         expect(CMS).toEqual(jasmine.any(Object));
         expect(CMS.API).toEqual(jasmine.any(Object));
         expect(CMS.KEYS).toEqual(jasmine.any(Object));
-        expect(CMS.BREAKPOINTS).toEqual(jasmine.any(Object));
         expect(CMS.$).toEqual(jQuery);
         expect(CMS.Class).toEqual(Class);
     });
