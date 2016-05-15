4eca42c98a68d755e306f9d4fc47a532768dccd0
Paulo <commonzenpython@gmail.com>
2016-05-15 18:41:56 -0400

    refactor menus to have a request aware manager

--- menus/templatetags/menu_tags.py
+++ menus/templatetags/menu_tags.py
@@ -128,7 +128,8 @@ class ShowMenu(InclusionTag):
             children = next_page.children
         else:
             # new menu... get all the data so we can save a lot of queries
-            nodes = menu_pool.get_nodes(request, namespace, root_id)
+            manager = menu_pool(request)
+            nodes = manager.get_nodes(namespace, root_id)
             if root_id:  # find the root id and cut the nodes
                 id_nodes = menu_pool.get_nodes_by_attribute(nodes, "reverse_id", root_id)
                 if id_nodes:
@@ -142,7 +143,7 @@ class ShowMenu(InclusionTag):
                 else:
                     nodes = []
             children = cut_levels(nodes, from_level, to_level, extra_inactive, extra_active)
-            children = menu_pool.apply_modifiers(children, request, namespace, root_id, post_cut=True)
+            children = manager.apply_modifiers(children, namespace, root_id, post_cut=True)
 
         try:
             context['children'] = children
@@ -205,6 +206,7 @@ class ShowSubMenu(InclusionTag):
             request = context['request']
         except KeyError:
             return {'template': 'menu/empty.html'}
+        manager = menu_pool(request)
         nodes = menu_pool.get_nodes(request)
         children = []
         # adjust root_level so we cut before the specified level, not after
@@ -231,9 +233,9 @@ class ShowSubMenu(InclusionTag):
                         # if root_level was 0 we need to give the menu the entire tree
                     # not just the children
                 if include_root:
-                    children = menu_pool.apply_modifiers([node], request, post_cut=True)
+                    children = manager.apply_modifiers([node], post_cut=True)
                 else:
-                    children = menu_pool.apply_modifiers(children, request, post_cut=True)
+                    children = manager.apply_modifiers(children, post_cut=True)
         context['children'] = children
         context['template'] = template
         context['from_level'] = 0
@@ -277,7 +279,9 @@ class ShowBreadcrumb(InclusionTag):
         except:
             only_visible = bool(only_visible)
         ancestors = []
-        nodes = menu_pool.get_nodes(request, breadcrumb=True)
+
+        manager = menu_pool(request)
+        nodes = manager.get_nodes(breadcrumb=True)
 
         # Find home
         home = None
