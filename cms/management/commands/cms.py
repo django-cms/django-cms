587cdbdf468c848b92850c730bb48b4f6669423c
Paulo <commonzenpython@gmail.com>
2016-05-24 12:11:50 -0400

    fixes regression in command options

--- cms/management/commands/cms.py
+++ cms/management/commands/cms.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 from collections import OrderedDict
 
+import cms
+
 from .subcommands.base import SubcommandsCommand
 from .subcommands.check import CheckInstallation
 from .subcommands.list import ListCommand
@@ -29,3 +31,10 @@ class Command(SubcommandsCommand):
     missing_args_message = 'one of the available sub commands must be provided'
 
     subcommand_dest = 'cmd'
+
+    def get_version(self):
+        return cms.__version__
+
+    def add_arguments(self, parser):
+        parser.add_argument('--version', action='version', version=self.get_version())
+        super(Command, self).add_arguments(parser)
