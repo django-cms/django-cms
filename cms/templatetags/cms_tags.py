fdfa970686235ce391206e6746fbd13fb028abd7
Iacopo Spalletti <i.spalletti@nephila.it>
2016-06-30 08:02:16 +0200

    Removed another deprecation

--- cms/templatetags/cms_tags.py
+++ cms/templatetags/cms_tags.py
@@ -13,7 +13,6 @@ from django.core.mail import mail_managers
 from django.core.urlresolvers import reverse
 from django.db.models import Model
 from django.middleware.common import BrokenLinkEmailsMiddleware
-from django.template import Context
 from django.template.defaultfilters import safe
 from django.template.loader import render_to_string
 from django.utils import six
