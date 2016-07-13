5e7f07c7b564d34065cb7e823503dcf1e8dd04ff
Vadim Sikora <vadim.sikora@divio.ch>
2016-07-13 16:39:18 +0200

    Created onPluginSave callback to get rid of unnecessary requests
    Modal has it's won mechanism for reloading when the plugin is
    saved. However when the plugin is saved - close_frame template is
    rendered where reloadBrowser is called again. So whenever the
    plugin is saved - server takes 2x time to process the response. We
    cannot remove the template since it can be used by other plugins
    for their own means (most notably djangocms-text-ckeditor).

--- cms/tests/frontend/unit/cms.base.test.js
+++ cms/tests/frontend/unit/cms.base.test.js
@@ -37,7 +37,7 @@ describe('cms.base.js', function () {
         it('exists', function () {
             expect(CMS.API.Helpers).toEqual(jasmine.any(Object));
             // this expectation is here so no one ever forgets to add a test
-            expect(Object.keys(CMS.API.Helpers).length).toEqual(17);
+            expect(Object.keys(CMS.API.Helpers).length).toEqual(18);
         });
 
         describe('.reloadBrowser()', function () {
@@ -319,6 +319,16 @@ describe('cms.base.js', function () {
                 expect(CMS.API.Helpers.reloadBrowser('/new-url', 50, false)).toEqual(undefined);
                 expect($.ajax).not.toHaveBeenCalled();
                 expect(win.parent.location.href).toEqual('/new-url');
+            });
+        });
+
+        describe('onPluginSave()', function () {
+            it('proxies to reloadBrowser', function () {
+                spyOn(CMS.API.Helpers, 'reloadBrowser');
+
+                CMS.API.Helpers.onPluginSave();
+                expect(CMS.API.Helpers.reloadBrowser).toHaveBeenCalledTimes(1);
+                expect(CMS.API.Helpers.reloadBrowser).toHaveBeenCalledWith(null, 300);
             });
         });
 
