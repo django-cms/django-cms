django-cms/django-cms

commit a44cf25d6d5fd63cd2e9bd2ad1b5e14ae15c7d7b

Changed JS bundling from simple concat to webpack based.
This enables smarter bundling, explicit dependencies, makes way for
future support for ES6, better libraries integration (e.g. jQuery
is no longer exposed to window, so no overlap with other libs).

This disables, however, the possibility for external apps to
directly use CMS modules, only the bundles (which is how it should
be).

However, for backwards compatibility cms.base has a typeof require
check for now.

cms/forms/widgets.py
@@ -24,10 +24,7 @@ class PageSelectWidget(MultiWidget):
 
     class Media:
         js = (
-            'cms/js/modules/jquery.noconflict.pre.js',
-            'cms/js/dist/bundle.admin.base.min.js',
             'cms/js/widgets/forms.pageselectwidget.js',
-            'cms/js/modules/jquery.noconflict.post.js'
         )
 
     def __init__(self, site_choices=None, page_choices=None, attrs=None):
@@ -126,7 +123,6 @@ class PageSmartLinkWidget(TextInput):
             )
         }
         js = (
-            'cms/js/dist/bundle.admin.base.min.js',
             'cms/js/select2/select2.js',
             'cms/js/widgets/forms.pagesmartlinkwidget.js',
         )
@@ -195,10 +191,7 @@ class AppHookSelect(Select):
 
     class Media:
         js = (
-            'cms/js/modules/jquery.noconflict.pre.js',
-            'cms/js/dist/bundle.admin.base.min.js',
             'cms/js/widgets/forms.apphookselect.js',
-            'cms/js/modules/jquery.noconflict.post.js'
         )
 
     def __init__(self, attrs=None, choices=(), app_namespaces={}):
@@ -251,10 +244,7 @@ class ApplicationConfigSelect(Select):
 
     class Media:
         js = (
-            'cms/js/modules/jquery.noconflict.pre.js',
-            'cms/js/dist/bundle.admin.base.min.js',
             'cms/js/widgets/forms.apphookselect.js',
-            'cms/js/modules/jquery.noconflict.post.js'
         )
 
     def __init__(self, attrs=None, choices=(), app_configs={}):

cms/static/cms/js/admin.base.js
@@ -1,0 +1,7 @@
+// polyfills
+require('./polyfills/function.prototype.bind.js');
+require('./libs/pep');
+
+var CMS = require('./modules/cms.base');
+
+window.CMS = CMS;

webpack.config.js
@@ -1,0 +1,82 @@
+var webpack = require('webpack');
+
+module.exports = function (opts) {
+    'use strict';
+
+    var PROJECT_PATH = opts.PROJECT_PATH;
+    var debug = opts.debug;
+
+    var baseConfig = {
+        devtool: false,
+        watch: !!opts.watch,
+        entry: {
+            'toolbar': PROJECT_PATH.js + '/toolbar.js',
+            'admin.base': PROJECT_PATH.js + '/admin.base.js',
+            'admin.pagetree': PROJECT_PATH.js + '/admin.pagetree.js',
+            'admin.changeform': PROJECT_PATH.js + '/admin.changeform.js'
+        },
+        output: {
+            path: PROJECT_PATH.js + '/dist/',
+            filename: 'bundle.[name].min.js',
+            jsonpFunction: 'cmsWebpackJsonp'
+        },
+        plugins: [
+            // this way admin.pagetree bundle won't
+            // include deps already required in admin.base bundle
+            new webpack.optimize.CommonsChunkPlugin({
+                name: 'admin.base',
+                chunks: ['admin.pagetree', 'admin.changeform']
+            })
+        ],
+        resolve: {
+            extensions: ['', '.js'],
+            alias: {
+                jquery: PROJECT_PATH.js + '/libs/jquery.min.js',
+                classjs: PROJECT_PATH.js + '/libs/class.min.js',
+                jstree: PROJECT_PATH.js + '/libs/jstree/jstree.min.js'
+            }
+        },
+        module: {
+            loaders: [
+                {
+                    test: /(modules\/jquery|libs\/pep)/,
+                    loaders: [
+                        'imports?jQuery=jquery'
+                    ]
+                },
+                {
+                    test: /class.min.js/,
+                    loaders: [
+                        'exports?Class'
+                    ]
+                }
+            ]
+        }
+    };
+
+    if (debug) {
+        baseConfig.devtool = 'inline-source-map';
+        baseConfig.plugins = baseConfig.plugins.concat([
+            new webpack.NoErrorsPlugin(),
+            new webpack.DefinePlugin({
+                __DEV__: 'true'
+            })
+        ]);
+    } else {
+        baseConfig.plugins = baseConfig.plugins.concat([
+            new webpack.DefinePlugin({
+                __DEV__: 'false'
+            }),
+            new webpack.optimize.OccurenceOrderPlugin(),
+            new webpack.optimize.DedupePlugin(),
+            new webpack.optimize.UglifyJsPlugin({
+                comments: false,
+                compressor: {
+                    drop_console: true // eslint-disable-line
+                }
+            })
+        ]);
+    }
+
+    return baseConfig;
+};

commit f527425d1b7a06d782f96b55a25df75a2de54732

Fixed a regression when standalone widgets wouldn't work
The regression happened because in
https://github.com/divio/django-cms/commit/a44cf25d6d5fd63cd2e9bd2ad1b5e14ae15c7d7b
and before that in 3.3.0 the files required for widgets were
removed from media js class on the assumption that the CMS provided
bundles would be loaded anyway. This commit changes the way
requirements are loaded for those widgets so they can be used
standalone and on CMS pages as well.

cms/forms/widgets.py
@@ -12,7 +12,7 @@ from django.utils.html import escape, escapejs
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext as _
 
-from cms.utils.urlutils import admin_reverse
+from cms.utils.urlutils import admin_reverse, static_with_version
 from cms.forms.utils import get_site_choices, get_page_choices
 from cms.models import Page, PageUser
 
@@ -24,7 +24,7 @@ class PageSelectWidget(MultiWidget):
 
     class Media:
         js = (
-            'cms/js/widgets/forms.pageselectwidget.js',
+            static_with_version('cms/js/dist/bundle.forms.pageselectwidget.min.js'),
         )
 
     def __init__(self, site_choices=None, page_choices=None, attrs=None):
@@ -99,10 +99,12 @@ class PageSelectWidget(MultiWidget):
                 final_attrs = dict(final_attrs, id='%s_%s' % (id_, i))
             output.append(widget.render(name + '_%s' % i, widget_value, final_attrs))
         output.append(r'''<script type="text/javascript">
-            CMS.$(function () {
-                new CMS.PageSelectWidget({
-                    name: '%(name)s'
-                });
+            var CMS = window.CMS || {};
+
+            CMS.Widgets = CMS.Widgets || {};
+            CMS.Widgets._pageSelectWidgets = CMS.Widgets._pageSelectWidgets || [];
+            CMS.Widgets._pageSelectWidgets.push({
+                name: '%(name)s'
             });
         </script>''' % {
             'name': name
@@ -123,8 +125,7 @@ class PageSmartLinkWidget(TextInput):
             )
         }
         js = (
-            'cms/js/select2/select2.js',
-            'cms/js/widgets/forms.pagesmartlinkwidget.js',
+            static_with_version('cms/js/dist/bundle.forms.pagesmartlinkwidget.min.js'),
         )
 
     def __init__(self, attrs=None, ajax_view=None):
@@ -144,13 +145,15 @@ class PageSmartLinkWidget(TextInput):
         id_ = final_attrs.get('id', None)
 
         output = [r'''<script type="text/javascript">
-            CMS.$(function () {
-                new CMS.PageSmartLinkWidget({
-                    id: '%(element_id)s',
-                    text: '%(placeholder_text)s',
-                    lang: '%(language_code)s',
-                    url: '%(ajax_url)s'
-                });
+            var CMS = window.CMS || {};
+
+            CMS.Widgets = CMS.Widgets || {};
+            CMS.Widgets._pageSmartLinkWidgets = CMS.Widgets._pageSmartLinkWidgets || [];
+            CMS.Widgets._pageSmartLinkWidgets.push({
+                id: '%(element_id)s',
+                text: '%(placeholder_text)s',
+                lang: '%(language_code)s',
+                url: '%(ajax_url)s'
             });
         </script>''' % {
             'element_id': id_,
@@ -191,7 +194,7 @@ class AppHookSelect(Select):
 
     class Media:
         js = (
-            'cms/js/widgets/forms.apphookselect.js',
+            static_with_version('cms/js/dist/bundle.forms.apphookselect.min.js'),
         )
 
     def __init__(self, attrs=None, choices=(), app_namespaces={}):
@@ -244,7 +247,7 @@ class ApplicationConfigSelect(Select):
 
     class Media:
         js = (
-            'cms/js/widgets/forms.apphookselect.js',
+            static_with_version('cms/js/dist/bundle.forms.apphookselect.min.js'),
         )
 
     def __init__(self, attrs=None, choices=(), app_configs={}):

cms/static/cms/js/admin.base.js
@@ -1,7 +1,6 @@
 // polyfills
 require('./polyfills/function.prototype.bind.js');
 require('./libs/pep');
-
 var CMS = require('./modules/cms.base');
 
 window.CMS = CMS;

cms/static/cms/js/modules/get-dist-path.js
@@ -1,0 +1,37 @@
+var getDistPath = function (scriptFileName) {
+    var fileNameReplaceRegExp = new RegExp(scriptFileName + '(\\?.*)?$', 'gi');
+
+    if (document.currentScript) {
+        return document.currentScript.src.replace(fileNameReplaceRegExp, '');
+    }
+    var scripts;
+    var scriptUrl;
+    var getSrc = function (listOfScripts, attr) {
+        var fileName;
+        var scriptPath;
+
+        for (var i = 0; i < listOfScripts.length; i++) {
+            scriptPath = null;
+            if (listOfScripts[i].getAttribute.length !== undefined) {
+                scriptPath = listOfScripts[i].getAttribute(attr, 2);
+            }
+            if (!scriptPath) {
+                continue; // eslint-disable-line
+            }
+            fileName = scriptPath;
+            fileName = fileName.split('?')[0].split('/').pop(); // get script filename
+            if (fileName === scriptFileName) {
+                return scriptPath;
+            }
+        }
+    };
+
+    scripts = document.getElementsByTagName('script');
+    scriptUrl = getSrc(scripts, 'src');
+    if (scriptUrl) {
+        return scriptUrl.replace(fileNameReplaceRegExp, '');
+    }
+    return '';
+};
+
+module.exports = getDistPath;

cms/static/cms/js/widgets/forms.apphookselect.js
@@ -2,15 +2,16 @@
  * Copyright https://github.com/divio/django-cms
  */
 
-// #############################################################################
-// NAMESPACES
-var CMS = window.CMS || {};
-var apphooks_configuration = window.apphooks_configuration || {};
+// this essentially makes sure that dynamically required bundles are loaded
+// from the same place
+// eslint-disable-next-line
+__webpack_public_path__ = require('../modules/get-dist-path')('bundle.forms.apphookselect.min.js');
 
 // #############################################################################
 // APP HOOK SELECT
-(function ($) {
-    'use strict';
+require.ensure([], function (require) {
+    var $ = require('jquery');
+    var apphooks_configuration = window.apphooks_configuration || {};
 
     // shorthand for jQuery(document).ready();
     $(function () {
@@ -91,4 +92,4 @@ var apphooks_configuration = window.apphooks_configuration || {};
         });
 
     });
-})(CMS.$);
+}, 'admin.widget');

cms/static/cms/js/widgets/forms.pageselectwidget.js
@@ -2,82 +2,86 @@
  * Copyright https://github.com/divio/django-cms
  */
 
-// #############################################################################
-// NAMESPACES
-/**
- * @module CMS
- */
-var CMS = window.CMS || {};
+// this essentially makes sure that dynamically required bundles are loaded
+// from the same place
+// eslint-disable-next-line
+__webpack_public_path__ = require('../modules/get-dist-path')('bundle.forms.pageselectwidget.min.js');
 
 // #############################################################################
 // PAGE SELECT WIDGET
 // cms/forms/widgets.py
-(function ($) {
-    'use strict';
+require.ensure([], function (require) {
+    var CMS = window.CMS || {};
+    var $ = require('jquery');
+    var Class = require('classjs');
 
-    // shorthand for jQuery(document).ready();
-    $(function () {
+    /**
+     * Manages the selection of two select fields. The first field
+     * sets the "Site" and the second the "Pagetree".
+     *
+     * @class PageSelectWidget
+     * @namespace CMS
+     */
+    CMS.PageSelectWidget = new Class({
+
+        initialize: function initialize(options) {
+            this.options = $.extend(true, {}, this.options, options);
+            // load functionality
+            this._setup(options);
+        },
+
         /**
-         * Manages the selection of two select fields. The first field
-         * sets the "Site" and the second the "Pagetree".
+         * Setup internal functions and events.
          *
-         * @class PageSelectWidget
-         * @namespace CMS
+         * @private
+         * @method _setup
+         * @param {Object} options
+         * @param {String} options.name
          */
-        CMS.PageSelectWidget = new CMS.Class({
+        _setup: function _setup(options) {
+            var group0 = $('#id_' + options.name + '_0');
+            var group1 = $('#id_' + options.name + '_1');
+            var group2 = $('#id_' + options.name + '_2');
+            var tmp;
 
-            initialize: function initialize(options) {
-                this.options = $.extend(true, {}, this.options, options);
-                // load functionality
-                this._setup(options);
-            },
+            // handles the change event on the first select "site"
+            // that controls the display of the second select "pagetree"
+            group0.on('change', function () {
+                tmp = $(this).children(':selected').text();
 
-            /**
-             * Setup internal functions and events.
-             *
-             * @private
-             * @method _setup
-             * @param {Object} options
-             * @param {String} options.name
-             */
-            _setup: function _setup(options) {
-                var group0 = $('#id_' + options.name + '_0');
-                var group1 = $('#id_' + options.name + '_1');
-                var group2 = $('#id_' + options.name + '_2');
-                var tmp;
+                group1.find('optgroup').remove();
+                group1.append(
+                    group2.find('optgroup[label="' + tmp + '"]').clone()
+                ).change();
 
-                // handles the change event on the first select "site"
-                // that controls the display of the second select "pagetree"
-                group0.on('change', function () {
-                    tmp = $(this).children(':selected').text();
+                // refresh second select
+                setTimeout(function () {
+                    group1.trigger('change');
+                }, 0);
+            }).trigger('change');
 
-                    group1.find('optgroup').remove();
-                    group1.append(
-                        group2.find('optgroup[label="' + tmp + '"]').clone()
-                    ).change();
+            // sets the correct value
+            group1.on('change', function () {
+                tmp = $(this).find('option:selected').val();
 
-                    // refresh second select
-                    setTimeout(function () {
-                        group1.trigger('change');
-                    }, 0);
-                }).trigger('change');
+                if (tmp) {
+                    group2.find('option').prop('selected', false);
+                    group2.find('option[value="' + tmp + '"]').prop('selected', true);
+                } else if (group2.length) {
+                    group2.find('option[value=""]').prop('selected', true);
+                }
+            });
 
-                // sets the correct value
-                group1.on('change', function () {
-                    tmp = $(this).find('option:selected').val();
-
-                    if (tmp) {
-                        group2.find('option').prop('selected', false);
-                        group2.find('option[value="' + tmp + '"]').prop('selected', true);
-                    } else if (group2.length) {
-                        group2.find('option[value=""]').prop('selected', true);
-                    }
-                });
-
-                // don't allow to add another page from in here
-                $('#add_id_' + options.name).hide();
-            }
-        });
-
+            // don't allow to add another page from in here
+            $('#add_id_' + options.name).hide();
+        }
     });
-})(CMS.$);
+
+
+    // init
+    $(function () {
+        CMS.Widgets._pageSelectWidgets.forEach(function (widget) {
+            new CMS.PageSelectWidget(widget);
+        });
+    });
+}, 'admin.widget');

cms/static/cms/js/widgets/forms.pagesmartlinkwidget.js
@@ -2,95 +2,99 @@
  * Copyright https://github.com/divio/django-cms
  */
 
-// #############################################################################
-// NAMESPACES
-/**
- * @module CMS
- */
-var CMS = window.CMS || {};
+// this essentially makes sure that dynamically required bundles are loaded
+// from the same place
+// eslint-disable-next-line
+__webpack_public_path__ = require('../modules/get-dist-path')('bundle.forms.pagesmartlinkwidget.min.js');
 
 // #############################################################################
 // PAGE SMART LINK WIDGET
 // cms/forms/widgets.py used for redirects in admin/cms/page/advanced-settings
-(function ($) {
-    'use strict';
+require.ensure([], function (require) {
+    var CMS = window.CMS || {};
+    var $ = require('jquery');
+    var Class = require('classjs');
 
-    // shorthand for jQuery(document).ready();
-    $(function () {
+    require('../select2/select2');
+
+    /**
+     * Creates a select field using jquery.select2 to filter through
+     * available pages or set a custom url.
+     *
+     * @class PageSmartLinkWidget
+     * @namespace CMS
+     */
+    CMS.PageSmartLinkWidget = new Class({
+
+        initialize: function initialize(options) {
+            this.options = $.extend(true, {}, this.options, options);
+            // load functionality
+            this._setup(options);
+        },
+
         /**
-         * Creates a select field using jquery.select2 to filter through
-         * available pages or set a custom url.
+         * Setup internal functions and events.
          *
-         * @class PageSmartLinkWidget
-         * @namespace CMS
+         * @private
+         * @method _setup
+         * @param {Object} options
+         * @param {String} options.id
+         * @param {String} options.url
          */
-        CMS.PageSmartLinkWidget = new CMS.Class({
-
-            initialize: function initialize(options) {
-                this.options = $.extend(true, {}, this.options, options);
-                // load functionality
-                this._setup(options);
-            },
-
-            /**
-             * Setup internal functions and events.
-             *
-             * @private
-             * @method _setup
-             * @param {Object} options
-             * @param {String} options.id
-             * @param {String} options.url
-             */
-            _setup: function _setup(options) {
-                $('#' + options.id).select2({
-                    placeholder: options.text,
-                    allowClear: true,
-                    multiple: false,
-                    ajax: {
-                        url: options.url,
-                        dataType: 'json',
-                        data: function (term) {
-                            return {
-                                q: term,
-                                language_code: options.lang
-                            };
-                        },
-                        // default search output, will be overridden if no results map
-                        results: function (data) {
-                            return {
-                                more: false,
-                                results: $.map(data, function (item) {
-                                    return {
-                                        id: item.redirect_url,
-                                        text: item.title + ' (/' + item.path + ')'
-                                    };
-                                })
-                            };
-                        }
+        _setup: function _setup(options) {
+            $('#' + options.id).select2({
+                placeholder: options.text,
+                allowClear: true,
+                multiple: false,
+                ajax: {
+                    url: options.url,
+                    dataType: 'json',
+                    data: function (term) {
+                        return {
+                            q: term,
+                            language_code: options.lang
+                        };
                     },
-                    // create fallback entry if no choices are found
-                    createSearchChoice: function (term, data) {
-                        if ($(data).filter(
-                            function () {
-                                return this.text.localeCompare(term) === 0;
-                            }).length === 0
-                        ) {
-                            return {
-                                id: term,
-                                text: term
-                            };
-                        }
-                    },
-                    // ensures initial selection is loaded
-                    initSelection: function (element, callback) {
-                        callback({
-                            id: element.val(),
-                            text: element.val()
-                        });
+                    // default search output, will be overridden if no results map
+                    results: function (data) {
+                        return {
+                            more: false,
+                            results: $.map(data, function (item) {
+                                return {
+                                    id: item.redirect_url,
+                                    text: item.title + ' (/' + item.path + ')'
+                                };
+                            })
+                        };
                     }
-                });
-            }
-        });
-
+                },
+                // create fallback entry if no choices are found
+                createSearchChoice: function (term, data) {
+                    if ($(data).filter(
+                        function () {
+                            return this.text.localeCompare(term) === 0;
+                        }).length === 0
+                    ) {
+                        return {
+                            id: term,
+                            text: term
+                        };
+                    }
+                },
+                // ensures initial selection is loaded
+                initSelection: function (element, callback) {
+                    callback({
+                        id: element.val(),
+                        text: element.val()
+                    });
+                }
+            });
+        }
     });
-})(CMS.$);
+
+    $(function () {
+        CMS.Widgets._pageSmartLinkWidgets.forEach(function (widget) {
+            new CMS.PageSmartLinkWidget(widget);
+        });
+    });
+}, 'admin.widget');

webpack.config.js
@@ -10,14 +10,22 @@ module.exports = function (opts) {
         devtool: false,
         watch: !!opts.watch,
         entry: {
+            // CMS frontend
             'toolbar': PROJECT_PATH.js + '/toolbar.js',
+            // CMS admin
             'admin.base': PROJECT_PATH.js + '/admin.base.js',
             'admin.pagetree': PROJECT_PATH.js + '/admin.pagetree.js',
-            'admin.changeform': PROJECT_PATH.js + '/admin.changeform.js'
+            'admin.changeform': PROJECT_PATH.js + '/admin.changeform.js',
+            // CMS widgets
+            // they will load the on-demand bundle called admin.widget
+            'forms.pageselectwidget': PROJECT_PATH.js + '/widgets/forms.pageselectwidget.js',
+            'forms.pagesmartlinkwidget': PROJECT_PATH.js + '/widgets/forms.pagesmartlinkwidget.js',
+            'forms.apphookselect': PROJECT_PATH.js + '/widgets/forms.apphookselect.js'
         },
         output: {
             path: PROJECT_PATH.js + '/dist/',
             filename: 'bundle.[name].min.js',
+            chunkFilename: 'bundle.[name].min.js',
             jsonpFunction: 'cmsWebpackJsonp'
         },
         plugins: [
@@ -25,7 +33,10 @@ module.exports = function (opts) {
             // include deps already required in admin.base bundle
             new webpack.optimize.CommonsChunkPlugin({
                 name: 'admin.base',
-                chunks: ['admin.pagetree', 'admin.changeform']
+                chunks: [
+                    'admin.pagetree',
+                    'admin.changeform'
+                ]
             })
         ],
         resolve: {
@@ -39,7 +50,7 @@ module.exports = function (opts) {
         module: {
             loaders: [
                 {
-                    test: /(modules\/jquery|libs\/pep)/,
+                    test: /(modules\/jquery|libs\/pep|select2\/select2)/,
                     loaders: [
                         'imports?jQuery=jquery'
                     ]