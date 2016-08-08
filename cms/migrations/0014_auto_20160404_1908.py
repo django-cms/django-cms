eca00bac1e5af4ecf5b4322f5f4a1bbf4b8db964
Paulo <commonzenpython@gmail.com>
2016-08-08 11:44:35 -0400

    Fixes #5589 -- Use pk explicitly when filtering relations in migrations (#5592)

--- cms/migrations/0014_auto_20160404_1908.py
+++ cms/migrations/0014_auto_20160404_1908.py
@@ -8,10 +8,23 @@ def forwards(apps, schema_editor):
     ContentType = apps.get_model('contenttypes', 'ContentType')
     Permission = apps.get_model('auth', 'Permission')
     page_model = apps.get_model('cms', 'Page')
-    page_ctype = ContentType.objects.get_for_model(page_model)
-    Permission.objects.filter(
-        name='',
-        codename='change_page', content_type=page_ctype).update(name='Can change page')
+    page_opts = page_model._meta
+
+    # Calling get_for_model directly causes Django to create
+    # the Content Type for Page if it does not exist.
+    # See django-cms#5589 & django#2342
+    content_type_exists = ContentType.objects.filter(
+        app_label=page_opts.app_label,
+        model=page_opts.model_name,
+    ).exists()
+
+    if content_type_exists:
+        page_ctype = ContentType.objects.get_for_model(page_model)
+        Permission.objects.filter(
+            name='',
+            codename='change_page',
+            content_type_id=page_ctype.pk,
+        ).update(name='Can change page')
 
 
 def backwards(apps, schema_editor):
