41d262032aad816b4176742415e6c42daf8f9727
Florian Verdet <fvgoto@users.noreply.github.com>
2016-09-13 16:25:48 +0200

    fix CMS_TEMPLATES_DIR without __init__.py (py2 OK)
    Now compatible with Python 2 and 3.
    reverted to if/else instead try/catch; added isfile check.

--- cms/utils/conf.py
+++ cms/utils/conf.py
@@ -146,10 +146,10 @@ def get_templates():
         config_path = os.path.join(tpldir, '__init__.py')
         # Try to load templates list and names from the template module
         # If module file is not present skip configuration and just dump the filenames as templates
-        try:
+        if os.path.isfile(config_path):
             template_module = load_from_file(config_path)
             templates = [(os.path.join(prefix, data[0].strip()), data[1]) for data in template_module.TEMPLATES.items()]
-        except FileNotFoundError:
+        else:
             templates = list((os.path.join(prefix, tpl), tpl) for tpl in os.listdir(tpldir))
     else:
         templates = list(getattr(settings, 'CMS_TEMPLATES', []))
