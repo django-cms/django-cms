bc32c15a9555464c492dcaec74156ee709bca610
Paulo Alvarado <commonzenpython@gmail.com>
2017-01-03 13:42:11 -0500

    Fixed #5830 -- Added django-reversion >=2 support (#5833)

--- cms/utils/reversion_hacks.py
+++ cms/utils/reversion_hacks.py
@@ -1,28 +1,37 @@
 # -*- coding: utf-8 -*-
 
-# This large try / except is to account for reversion 1.10 (top) / 1.8 (bottom) support
-# This will go away in CMS 3.3 when only reversion 1.10 will be supported
+from reversion.revisions import RegistrationError
+
 try:
-    from reversion import revisions as reversion
-    from reversion.admin import VersionAdmin as ModelAdmin, RollBackRevisionView  # NOQA  # nopyflakes
-    from reversion.models import Revision, Version  # NOQA  # nopyflakes
-    from reversion.revisions import create_revision, RegistrationError, VersionAdapter  # NOQA  # nopyflakes
-    from reversion.signals import post_revision_commit  # NOQA  # nopyflakes
-
-    revision_manager = reversion.default_revision_manager
-    revision_context = reversion.revision_context_manager
+    # django-reversion >= 2
+    from reversion.revisions import _VersionOptions as VersionAdapter
 except ImportError:
+    # django-reversion < 2
+    from reversion.revisions import VersionAdapter
+
+try:
+    # 1.10 >= django-reversion < 2
+    from reversion import revisions as reversion
+    revision_manager = reversion.default_revision_manager
+except ImportError:
+    # django-reversion < 1.10
     import reversion
-    from reversion import create_revision  # NOQA  # nopyflakes
-    from reversion.admin import VersionAdmin as ModelAdmin  # NOQA  # nopyflakes
-    from reversion.models import Revision, Version, post_revision_commit  # NOQA  # nopyflakes
-    from reversion.revisions import RegistrationError, VersionAdapter  # NOQA  # nopyflakes
-
     revision_manager = reversion.revision
-    revision_context = reversion.revision_context_manager
+except AttributeError:
+    # django-reversion >= 2
+    from reversion import revisions
 
-    class RollBackRevisionView(Exception):
-        pass
+    class revision_manager(object):
+
+        _registered_models = revisions._registered_models
+
+        @staticmethod
+        def _registration_key_for_model(*args, **kwargs):
+            return revisions._get_registration_key(*args, **kwargs)
+
+        @staticmethod
+        def is_registered(*args, **kwargs):
+            return revisions.is_registered(*args, **kwargs)
 
 
 def register_draft_only(model_class, fields, follow, format):
@@ -51,10 +60,20 @@ def register_draft_only(model_class, fields, follow, format):
 
     # Register the generated registration information.
     follow = tuple(follow)
-    registration_info = VersionAdapter(model_class)
-    registration_info.fields = fields
-    registration_info.follow = follow
-    registration_info.format = format
+
+    try:
+        registration_info = VersionAdapter(
+            fields=fields,
+            follow=follow,
+            format=format,
+            for_concrete_model=True,
+            ignore_duplicates=False,
+        )
+    except TypeError:
+        registration_info = VersionAdapter(model_class)
+        registration_info.fields = fields
+        registration_info.follow = follow
+        registration_info.format = format
     if hasattr(revision_manager, '_registration_key_for_model'):
         model_key = revision_manager._registration_key_for_model(model_class)
     else:
