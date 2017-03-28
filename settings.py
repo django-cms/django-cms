d1172b7c0fdee310da3ea75bd43266fc91722bc3
Angelo Dini <angelo.dini@divio.ch>
2017-03-28 08:18:10 +0000

    install aldryn-addons 1.0.2

--- settings.py
+++ settings.py
@@ -2,6 +2,7 @@
 
 INSTALLED_ADDONS = [
     # <INSTALLED_ADDONS>  # Warning: text inside the INSTALLED_ADDONS tags is auto-generated. Manual changes will be overwritten.
+    'aldryn-addons',
     'aldryn-django',
     'aldryn-sso',
     # </INSTALLED_ADDONS>
