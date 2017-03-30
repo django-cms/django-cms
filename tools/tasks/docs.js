1a847d724596551209fc3d6daa59615a1cc5353d
Angelo Dini <angelo.dini@divio.ch>
2017-03-30 19:58:12 +0200

    remove components that are not required anymore

--- tools/tasks/docs.js
+++
@@ -1,9 +0,0 @@
-var yuidoc = require('gulp-yuidoc');
-
-module.exports = function (gulp, opts) {
-    return function () {
-        gulp.src(opts.PROJECT_PATTERNS.js)
-            .pipe(yuidoc())
-            .pipe(gulp.dest(opts.PROJECT_PATH.docs));
-    };
-};
