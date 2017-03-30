9ab199b88e295e8a92e68e3f7f9ffdf22aa1a4d9
Angelo Dini <angelo.dini@divio.ch>
2017-03-30 11:44:19 +0200

    add new style sass implementation

---
+++ tools/tasks/sass/inline.js
@@ -0,0 +1,14 @@
+const gulpif = require('gulp-if');
+const gutil = require('gulp-util');
+const checkFileSize = require('gulp-check-filesize');
+
+
+module.exports = function (gulp, opts) {
+    return function () {
+        return gulp.src(opts.PROJECT_PATH.css + '/*-critical.css')
+            .pipe(checkFileSize({
+                fileSizeLimit: '14336'
+            }));
+            // TODO add inline script, prevent if debug param is passed
+    };
+};
