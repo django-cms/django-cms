71483ca8fa69997eb3f8d050e87f15c79ca4c2c8
Andras Gyömrey <lgandras@gmail.com>
2017-10-29 12:52:34 +0100

    Fixed #6045 -- Validate IP address on toolbar IP check (#6046)

--- cms/middleware/toolbar.py
+++ cms/middleware/toolbar.py
@@ -2,7 +2,9 @@
 """
 Edit Toolbar middleware
 """
+from django import forms
 from django.contrib.admin.models import LogEntry, ADDITION, CHANGE
+from django.core.exceptions import ValidationError
 from django.core.urlresolvers import resolve
 from django.http import HttpResponse
 
@@ -27,8 +29,13 @@ class ToolbarMiddleware(MiddlewareMixin):
 
         if internal_ips:
             client_ip = get_request_ip(request)
-            if client_ip not in internal_ips:
+            try:
+                client_ip = forms.GenericIPAddressField().clean(client_ip)
+            except ValidationError:
                 return False
+            else:
+                if client_ip not in internal_ips:
+                    return False
 
         if not toolbar_hide:
             return True
