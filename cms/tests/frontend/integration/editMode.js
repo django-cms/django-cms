9d5c2020d06486ad46cedb370e49d352ec0976a8
Vadim Sikora <vadim.sikora@divio.ch>
2017-11-09 16:31:22 +0800

    Fixed "Expand all / Collapse all" not reflecting real state of the tree

--- cms/tests/frontend/integration/editMode.js
+++ cms/tests/frontend/integration/editMode.js
@@ -3,10 +3,11 @@
 // #############################################################################
 // Users managed via the admin panel
 
+var casperjs = require('casper');
 var helpers = require('djangocms-casper-helpers');
 var globals = helpers.settings;
 var content = globals.content.page;
-var cms = helpers();
+var cms = helpers(casperjs);
 
 casper.test.setUp(function(done) {
     casper.start().then(cms.login()).then(cms.addPage({ title: content.title })).run(done);
@@ -47,6 +48,50 @@ casper.test.begin('Opening Page in Edit Mode', function(test) {
                 '.cms-toolbar-item-switch-save-edit',
                 'The page is in edit mode after clicking Page -> Edit this Page'
             );
+        })
+        .run(function() {
+            test.done();
+        });
+});
+
+casper.test.begin('Expand / collapse all', function(test) {
+    casper
+        // opening the page in edit off mode
+        .start(globals.editUrl)
+        .then(
+            cms.addPlugin({
+                type: 'MultiColumnPlugin',
+                content: {
+                    id_create: 2
+                }
+            })
+        )
+        .then(
+            cms.addPlugin({
+                type: 'TextPlugin',
+                content: {
+                    id_body: 'Test text'
+                }
+            })
+        )
+        .then(cms.switchTo('structure'))
+        .then(function() {
+            this.mouse.click(
+                '.cms-dragarea:first-child > .cms-draggables > .cms-draggable > .cms-dragitem-collapsable'
+            );
+        })
+        .wait(100)
+        .then(function() {
+            test.assertExists('.cms-dragarea:first-child .cms-dragbar-title-expanded');
+        })
+        .wait(100, function() {
+            this.mouse.click(
+                '.cms-dragarea:first-child > .cms-draggables > .cms-draggable > .cms-dragitem-collapsable'
+            );
+        })
+        .wait(100)
+        .then(function() {
+            test.assertDoesntExist('.cms-dragarea:first-child .cms-dragbar-title-expanded');
         })
         .run(function() {
             test.done();
