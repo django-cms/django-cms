e88005093ab48d94fb4399027a3e51499c643982
Olivier Tabone <olivier.tabone@ripplemotion.fr>
2017-11-10 18:04:59 +0100

    fix #6035 Editing advanced settings through change view raises AttributeError

--- cms/admin/forms.py
+++ cms/admin/forms.py
@@ -301,6 +301,7 @@ class AdvancedSettingsForm(forms.ModelForm):
                     # for application_configs to avoid the field data from
                     # being validated by the field itself
                     try:
+                        self.data = self.data.copy()  # self.data is usually immutable. Copy it to make it mutable.
                         del self.data['application_configs']
                     except KeyError:
                         pass
