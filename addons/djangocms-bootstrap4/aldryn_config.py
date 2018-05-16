70746b492be087afd04d9837a50c36451aedfdee
aldrynbot <aldrynbot@1744d0880826>
2018-05-16 12:15:00 +0000

    upgrade djangocms-bootstrap4 0.0.1.4 to 1.0.0

--- addons/djangocms-bootstrap4/aldryn_config.py
+++ addons/djangocms-bootstrap4/aldryn_config.py
@@ -4,5 +4,20 @@ from aldryn_client import forms
 
 class Form(forms.BaseForm):
 
+    grid_size = forms.NumberField(
+        'Maximum columns to support, default is 12.',
+        required=False
+    )
+    enable_icons = forms.CheckboxField(
+        'Enable icon support',
+        required=False,
+        initial=True,
+    )
+
     def to_settings(self, data, settings):
+        if data['grid_size']:
+            settings['DJANGOCMS_BOOTSTRAP4_GRID_SIZE'] = int(data['grid_size'])
+        if data['enable_icons']:
+            settings['DJANGOCMS_BOOTSTRAP4_USE_ICONS'] = int(data['enable_icons'])
+
         return settings
