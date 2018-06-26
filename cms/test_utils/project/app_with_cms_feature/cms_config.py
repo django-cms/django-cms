2055642fc7c2d0ee46583cd7f20a7366b2a5ff4d
Paulo Alvarado <commonzenpython@gmail.com>
2018-06-26 16:01:27 -0400

    Minor cleanup

--- cms/test_utils/project/app_with_cms_feature/cms_config.py
+++ cms/test_utils/project/app_with_cms_feature/cms_config.py
@@ -1,18 +1,6 @@
 from cms.app_base import CMSAppExtension
 
 
-def some_function(*args, **kwargs):
-    # this should not be called by autodiscover
-    raise Exception('some_function called')
-
-
-class SomeClass(object):
-    # this should not be picked up by autodiscover
-
-    def __init__(self, *args, **kwargs):
-        raise Exception('SomeClass instantiated')
-
-
 class CMSSomeFeatureConfig(CMSAppExtension):
 
     def __init__(self, *args, **kwargs):
@@ -22,10 +10,3 @@ class CMSSomeFeatureConfig(CMSAppExtension):
     def configure_app(self, cms_config):
         self.num_configured_apps += 1
         self.configured_apps.append(cms_config.app_config.label)
-
-
-class SomeOtherClass(object):
-    # this should not be picked up by autodiscover
-
-    def __init__(self, *args, **kwargs):
-        raise Exception('SomeOtherClass instantiated')
