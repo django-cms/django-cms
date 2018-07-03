1949123b85cba5f9bd06d8d72574bf079f298c6a
Andrew Aikman <a.aikman@ic.ac.uk>
2018-07-03 17:12:48 +0100

    Clean the migration fo rthe pages and placeholders

--- cms/migrations/0023_title_placeholders_data_migration.py
+++ cms/migrations/0023_title_placeholders_data_migration.py
@@ -8,42 +8,23 @@ from django.db import migrations, models
 
 def forwards(apps, schema_editor):
 
-    Placeholder = apps.get_model('cms', 'Placeholder')
-    Plugin = apps.get_model('cms', 'CMSPlugin')
     Page = apps.get_model('cms', 'Page')
-    Title = apps.get_model('cms', 'Title')
-
     # 1. Create a placeholder for each language the page is tied to (title_set)
     # 2. Move all plugins for each language into their respective placeholder
-
     page_list = Page.objects.all().prefetch_related('title_set')
 
-    page_count = page_list.count()
-
     for page in page_list:
-
-        # Add each placeholder to the title / language
         # Get all titles registered to the page
         title_set = page.title_set.all()
-        # For each title add the placeholders
+        # Add each placeholder on the page template to the title
         for title in title_set:
-
-            title.placeholders.add(page.placeholders.all())
-
-
-    """
-    # for each page language
-    for page_title in page.title_set:
-        new_title = page_title
-    """
-
-
-
+            placeholders = page.placeholders.all()
+            title.placeholders.add(*list(placeholders))
 
     #except Exception:
     #    warnings.warn(u'Placeholder migration failure.')
 
-    raise os.sys.exit(u'Placeholder migration failure.')
+    #raise os.sys.exit(u'Placeholder migration failure.')
 
 
 def backwards(apps, schema_editor):
