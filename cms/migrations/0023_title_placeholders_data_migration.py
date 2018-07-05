194402b6cb9ebe43f5cdfc6e782dc346fe602d45
Andrew Aikman <a.aikman@ic.ac.uk>
2018-07-05 17:18:16 +0100

    Cleaned the migrations

--- cms/migrations/0023_title_placeholders_data_migration.py
+++ cms/migrations/0023_title_placeholders_data_migration.py
@@ -6,7 +6,7 @@ import warnings, os, sys
 from django.db import migrations, models
 
 """
-
+# Current / Previous Placeholder structure
 Title id 1
 Title id 2
 Page id 1
@@ -17,14 +17,15 @@ Page id 1
         - Plugin 3 lang 1
         - Plugin 4 lang 1
 
-Title id 1
-    - Page 1
+# New Title Placeholder structure
+Page 1
+    - Title id 1
         - Placeholder 1
             - Plugin 1 lang 1
         - Placeholder 2
             - Plugin 3 lang 1
             - Plugin 4 lang 1
-    - Page 2
+    - Title id 2
         - Placeholder 1
             - Plugin 5 lang 2
 """
@@ -34,16 +35,12 @@ def forwards(apps, schema_editor):
 
     Page = apps.get_model('cms', 'Page')
     Plugin = apps.get_model('cms', 'CMSPlugin')
-    Placeholder = apps.get_model('cms', 'Placeholder')
+
     # 1. Create a placeholder for each language the page is tied to (title_set)
     # 2. Move all plugins for each language into their respective placeholder
-    # 3. Clean away the existing placeholders???
+    # 3. Clean away the existing placeholders
 
     page_list = Page.objects.all().prefetch_related('title_set')
-
-    #TODO: 1 Replicate placeholder structure for each title
-    #TODO: 2 Move placeholder plugins to the new placeholder, use language here to filter by title!!
-    #TODO: 3 Delete previous placeholders
 
     for page in page_list:
         # Get all titles registered to the page
@@ -54,29 +51,24 @@ def forwards(apps, schema_editor):
         for title in title_set:
             # Get a list of the plugins attached to this placeholder for this language
             for placeholder in placeholders:
-
                 # Get all of the plugins for this placeholder
                 placeholder_plugins = Plugin.objects.filter(placeholder_id=placeholder.pk, language=title.language)
-
                 # Clone the placeholder
-                placeholder.pk=None
+                placeholder.pk = None
                 placeholder.save()
-
+                # Create a link to the new placeholder
                 title.placeholders.add(placeholder)
 
-                # Move the plugins to the relevant placeholder
+                # Move the plugins to the relevant / new title -> placeholder
                 for plugin in placeholder_plugins:
                     plugin.placeholder_id = placeholder.pk
                     plugin.save()
 
+    # TODO: Delete previous placeholders!! None should have any plugins
+    # TODO: Handle exceptions!!
     #except Exception:
     #    warnings.warn(u'Placeholder migration failure.')
-
     #raise os.sys.exit(u'Placeholder migration failure.')
-
-
-def backwards(apps, schema_editor):
-    pass
 
 
 class Migration(migrations.Migration):
