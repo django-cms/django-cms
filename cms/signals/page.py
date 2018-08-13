1318c224f9391c3fb7b53d086d5697308a1fa449
Jacob Rief <jacob.rief@gmail.com>
2018-08-13 18:12:28 +0200

    fix reviiew #145557349 of PR #6473

--- cms/signals/page.py
+++ cms/signals/page.py
@@ -3,11 +3,10 @@ from cms.cache.permissions import clear_permission_cache
 from cms.signals.apphook import set_restart_trigger
 
 
-def pre_save_page(instance, raw=False, **kwargs):
-    if instance.publisher_is_draft:
-        if raw is False:
-            instance.clear_cache(menu=True)
-            clear_permission_cache()
+def pre_save_page(instance, **kwargs):
+    if instance.publisher_is_draft and not kwargs.get('raw'):
+        instance.clear_cache(menu=True)
+        clear_permission_cache()
 
 
 def pre_delete_page(instance, **kwargs):
