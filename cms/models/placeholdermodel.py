88f6439a32152db50a4b0a1b9ce083db2c3b509f
Krzysztof Socha <k.socha@merixstudio.com>
2018-09-03 14:55:47 +0200

    Add placeholder checks

--- cms/models/placeholdermodel.py
+++ cms/models/placeholdermodel.py
@@ -186,6 +186,19 @@ class Placeholder(models.Model):
                 return False
         return True
 
+    def _get_source_remote_field(self):
+        if self.source is None:
+            return
+        return next(
+            f for f in self.source._meta.get_fields()
+            if f.related_model == Placeholder
+        )
+
+    def check_source(self, user):
+        remote_field = self._get_source_remote_field()
+        if remote_field:
+            return remote_field.run_checks(user, self)
+
     def _get_related_objects(self):
         fields = self._meta._get_fields(
             forward=False, reverse=True,
