7f1993dc377d2c2438993a87f983846b6fe1a734
Filipe Waitman <filwaitman@gmail.com>
2018-09-19 17:53:55 +0200

    Support Django 1.11

--- project/cms_plugins.py
+++ project/cms_plugins.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
@@ -17,9 +17,9 @@ class PandadocDocumentSenderCMSPlugin(CMSPluginBase):
     cache = False
 
     def get_plugin_urls(self):
-        return patterns('',
+        return [
             url(r'^send-document/$', views.send_pandadoc_document, name='pandadoc-send-document'),
-        )
+        ]
 
     def render(self, context, instance, placeholder):
         context = super(PandadocDocumentSenderCMSPlugin, self).render(context, instance, placeholder)
