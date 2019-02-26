ee562295edf0e990e9377aa19048f0dfe1ef93ba
Angelo Dini <angelo.dini@divio.ch>
2019-02-26 09:28:04 +0100

    re-added meta

--- private/js/addons/intercom.js
+++ private/js/addons/intercom.js
@@ -34,8 +34,6 @@ export function initIntercom() {
         el.on(data.trigger || 'click', () => {
             clearTimeout(timeout);
             timeout = setTimeout(() => {
-                console.log('trackEvent', data.track, data.meta || {});
-                console.log(window.Intercom)
                 window.Intercom && window.Intercom('trackEvent', data.track, data.meta || {});
             }, data.timeout || 0);
         });
