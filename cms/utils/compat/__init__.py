9591aec2623d604dbe81258ef9e2dc383bb39251
vThaian <b.plociennik@merixstudio.com>
2019-03-12 10:18:17 +0100

    Restored Django flags in compat file

--- cms/utils/compat/__init__.py
+++ cms/utils/compat/__init__.py
@@ -8,6 +8,9 @@ DJANGO_VERSION = django.get_version()
 PYTHON_VERSION = platform.python_version()
 
 # These means "less than or equal to DJANGO_FOO_BAR"
+DJANGO_1_8 = LooseVersion(DJANGO_VERSION) < LooseVersion('1.9')
+DJANGO_1_9 = LooseVersion(DJANGO_VERSION) < LooseVersion('1.10')
+DJANGO_1_10 = LooseVersion(DJANGO_VERSION) < LooseVersion('1.11')
 DJANGO_1_11 = LooseVersion(DJANGO_VERSION) < LooseVersion('2.0')
 DJANGO_2_0 = LooseVersion(DJANGO_VERSION) < LooseVersion('2.1')
 DJANGO_2_1 = LooseVersion(DJANGO_VERSION) < LooseVersion('2.2')
