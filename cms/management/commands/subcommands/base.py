209fed58a111b36a66416892c44876cd2c767c04
vThaian <b.plociennik@merixstudio.com>
2019-03-26 12:45:31 +0100

    Fixed issue with force_color

--- cms/management/commands/subcommands/base.py
+++ cms/management/commands/subcommands/base.py
@@ -5,7 +5,7 @@ import os
 from collections import OrderedDict
 
 from django.core.management.base import BaseCommand, CommandParser
-from django.core.management.color import no_style
+from django.core.management.color import no_style, color_style
 
 from cms.utils.compat import DJANGO_2_0
 
@@ -37,6 +37,8 @@ def add_builtin_arguments(parser):
         help='Raise on CommandError exceptions')
     parser.add_argument('--no-color', action='store_true', dest='no_color', default=False,
         help="Don't colorize the command output.")
+    parser.add_argument('--force-color', action='store_true', dest='force_color', default=False,
+        help="Colorize the command output.")
 
 
 class SubcommandsCommand(BaseCommand):
@@ -85,6 +87,9 @@ class SubcommandsCommand(BaseCommand):
             if options.get('no_color'):
                 command.style = no_style()
                 command.stderr.style_func = None
+            # if options.get('force_color'):
+            #     command.style = color_style()
+            #     command.stderr.style_func = None
             if options.get('stdout'):
                 command.stdout._out = options.get('stdout')
             if options.get('stderr'):
