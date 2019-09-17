ff16c4648c57a3bf246f03fe0b7c6282289f46c1
Zdeněk Böhm <zdenek.bohm@nic.cz>
2019-09-17 14:58:42 +0200

    Add COLLATE "C" into database table column cms_cmsplugin.path.

---
+++ cms/migrations/0035_cms_cmsplugin_path_collage.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cms', '0034_remove_pagecontent_placeholders'),
+    ]
+
+    operations = []
+    if settings.DATABASES['default']['ENGINE'] in (
+            'django.db.backends.postgresql_psycopg2', 'django.db.backends.postgresql'):
+        operations.append(migrations.RunSQL(
+            'ALTER TABLE cms_cmsplugin ALTER COLUMN path SET DATA TYPE varchar(255) COLLATE "C"'))
+    elif settings.DATABASES['default']['ENGINE'] == 'django.db.backends.mysql':
+        operations.append(migrations.RunSQL(
+            'ALTER TABLE cms_cmsplugin MODIFY path VARCHAR(255) COLLATE ascii_general_ci'))
+    elif settings.DATABASES['default']['ENGINE'] == 'django.db.backends.oracle':
+        operations.append(migrations.RunSQL(
+            'ALTER TABLE cms_cmsplugin MODIFY (path VARCHAR2(255) COLLATE BINARY_CI)'))
