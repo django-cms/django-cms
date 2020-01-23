1e83a17a951439dab13fcd0ab25c1b800345e0f0
Angelo Dini <angelo.dini@divio.ch>
2020-01-23 11:01:21 +0100

    update test framework

--- setup.py
+++ setup.py
@@ -19,11 +19,13 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
     'Framework :: Django',
     'Framework :: Django :: 1.11',
     'Framework :: Django :: 2.0',
     'Framework :: Django :: 2.1',
     'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
 ]
 
 INSTALL_REQUIREMENTS = [
