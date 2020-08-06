742d76e88669cfd01e060978ac6366e5c5b4fc4f
Angelo Dini <angelo.dini@divio.ch>
2020-08-06 11:59:46 +0200

    moved from django.forms.fields.EMPTY_VALUES to django.core.validators.EMPTY_VALUES

--- cms/forms/fields.py
+++ cms/forms/fields.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django import forms
 from django.contrib.admin.widgets import RelatedFieldWidgetWrapper
-from django.forms.fields import EMPTY_VALUES
+from django.core.validators import EMPTY_VALUES
 from django.utils.translation import ugettext_lazy as _
 
 from cms.forms.utils import get_site_choices, get_page_choices
