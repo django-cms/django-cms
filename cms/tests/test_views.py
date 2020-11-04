92edd837f0f19df3f48dd3a35588cfe8cc82b401
Adam Murray <adam-murray@hotmail.co.uk>
2020-11-04 11:14:28 +0000

    Added missing import

--- cms/tests/test_views.py
+++ cms/tests/test_views.py
@@ -2,6 +2,7 @@ import re
 import sys
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Permission
 from django.core.cache import cache
 from django.http import Http404
