c263577cb9723258c6b21875d841d19b0819b59e
Django Doctor <help@django.doctor>
2021-01-20 06:14:43 +0000

    Fix some django anti-patterns

--- cms/migrations/0019_set_pagenode.py
+++ cms/migrations/0019_set_pagenode.py
@@ -53,8 +53,8 @@ class Migration(IrreversibleMigration):
     ]
 
     operations = [
-        migrations.RunPython(unpublish_never_published_pages),
-        migrations.RunPython(set_page_nodes),
+        migrations.RunPython(unpublish_never_published_pages, migrations.RunPython.noop),
+        migrations.RunPython(set_page_nodes, migrations.RunPython.noop),
     ]
 
     def apply(self, project_state, schema_editor, collect_sql=False):
