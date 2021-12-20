0b8e9c19b932f6e91cb452d00093d4b89d2339fe
Andrew Aikman <andrew.aikman@eliga.services>
2021-12-20 18:08:44 +0000

    Replaced dict has attr with in

--- cms/forms/fields.py
+++ cms/forms/fields.py
@@ -50,7 +50,7 @@ class PageSelectFormField(forms.MultiValueField):
         # Remove the unexpected blank kwarg if it's supplied,
         # causes an error where the MultiValueField doesn't expect it
         # https://github.com/django/django/commit/da79ee472d803963dc3ea81ee67767dc06068aac
-        if hasattr(kwargs, 'blank'):
+        if 'blank' in kwargs:
             del(kwargs['blank'])
 
         super().__init__(fields, *args, **kwargs)
