e707f6aaae911a38f88a21b28885176648779db5
Mark Walker <theshow@gmail.com>
2022-04-04 21:02:17 +0100

    Fix for django 2.2 in middleware [#7290] (#7293)
    * Fix for django 2.2 in middleware [#7290]
    
    * Address isort concern

--- cms/middleware/language.py
+++ cms/middleware/language.py
@@ -2,6 +2,8 @@ from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.translation import LANGUAGE_SESSION_KEY, get_language
 
+from cms.utils.compat import DJANGO_3_0
+
 
 class LanguageCookieMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
@@ -18,14 +20,21 @@ class LanguageCookieMiddleware(MiddlewareMixin):
         # To ensure support of very old browsers, Django processed automatically "expires" according to max_age value.
         # https://docs.djangoproject.com/en/3.2/ref/request-response/#django.http.HttpResponse.set_cookie
 
+        cookie_kwargs = {
+            'value': language,
+            'domain': settings.LANGUAGE_COOKIE_DOMAIN,
+            'max_age': settings.LANGUAGE_COOKIE_AGE or 365 * 24 * 60 * 60,  # 1 year
+            'path': settings.LANGUAGE_COOKIE_PATH,
+        }
+        if DJANGO_3_0:
+            cookie_kwargs.update({
+                'httponly': settings.LANGUAGE_COOKIE_HTTPONLY,
+                'samesite': settings.LANGUAGE_COOKIE_SAMESITE,
+                'secure': settings.LANGUAGE_COOKIE_SECURE,
+            })
+
         response.set_cookie(
             settings.LANGUAGE_COOKIE_NAME,
-            value=language,
-            domain=settings.LANGUAGE_COOKIE_DOMAIN,
-            max_age=settings.LANGUAGE_COOKIE_AGE or 365 * 24 * 60 * 60,  # 1 year
-            httponly=settings.LANGUAGE_COOKIE_HTTPONLY,
-            path=settings.LANGUAGE_COOKIE_PATH,
-            samesite=settings.LANGUAGE_COOKIE_SAMESITE,
-            secure=settings.LANGUAGE_COOKIE_SECURE,
+            **cookie_kwargs
         )
         return response
