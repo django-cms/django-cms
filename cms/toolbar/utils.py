4750bc298e6bd7228c61a55edbfe33290a6a28d3
Adam Murray <adam-murray@hotmail.co.uk>
2022-07-13 15:39:19 +0100

    feat: Add live-url url query parameter to PageContent cms Preview and Edit endpoints (#7359) (#7362)
    A new feature added to v4 Preview and Edit endpoints. Currently there’s no way of knowing what the current pages live url is like you used to be able to with 3.x using ?edit, this new feature (off by default) adds a query string parameter to the preview and edit endpoints.

--- cms/toolbar/utils.py
+++ cms/toolbar/utils.py
@@ -6,6 +6,8 @@ from django.utils.encoding import force_str
 from django.utils.translation import get_language, override as force_language, gettext
 
 from cms.constants import PLACEHOLDER_TOOLBAR_JS, PLUGIN_TOOLBAR_JS
+from cms.models import PageContent
+from cms.utils.conf import get_cms_setting
 from cms.utils.urlutils import admin_reverse
 
 
@@ -117,6 +119,28 @@ def get_toolbar_from_request(request):
     return getattr(request, 'toolbar', EmptyToolbar(request))
 
 
+def add_live_url_querystring_param(obj, url, language=None):
+    """
+    Append a live url to a given Page url using a supplied url parameter configured
+    by the setting: CMS_ENDPOINT_LIVE_URL_QUERYSTRING_PARAM
+
+    :param obj: Placeholder source object
+    :param url: Url string
+    :param language: The current language code or None
+    :returns: A url string
+    """
+    url_param = get_cms_setting('ENDPOINT_LIVE_URL_QUERYSTRING_PARAM')
+    if not isinstance(obj, PageContent):
+        return url
+    live_url = obj.page.get_absolute_url(language=language)
+    url_fragments = url.split('?')
+    if len(url_fragments) > 1:
+        url += f'&{url_param}={live_url}'
+    else:
+        url += f'?{url_param}={live_url}'
+    return url
+
+
 def get_object_edit_url(obj, language=None):
     content_type = ContentType.objects.get_for_model(obj)
 
@@ -124,7 +148,10 @@ def get_object_edit_url(obj, language=None):
         language = get_language()
 
     with force_language(language):
-        return admin_reverse('cms_placeholder_render_object_edit', args=[content_type.pk, obj.pk])
+        url = admin_reverse('cms_placeholder_render_object_edit', args=[content_type.pk, obj.pk])
+    if get_cms_setting('ENDPOINT_LIVE_URL_QUERYSTRING_PARAM_ENABLED'):
+        url = add_live_url_querystring_param(obj, url, language)
+    return url
 
 
 def get_object_preview_url(obj, language=None):
@@ -134,7 +161,10 @@ def get_object_preview_url(obj, language=None):
         language = get_language()
 
     with force_language(language):
-        return admin_reverse('cms_placeholder_render_object_preview', args=[content_type.pk, obj.pk])
+        url = admin_reverse('cms_placeholder_render_object_preview', args=[content_type.pk, obj.pk])
+    if get_cms_setting('ENDPOINT_LIVE_URL_QUERYSTRING_PARAM_ENABLED'):
+        url = add_live_url_querystring_param(obj, url, language)
+    return url
 
 
 def get_object_structure_url(obj, language=None):
