35a46b5e38468852651f3999725b2f4788a2a758
Mark Walker <mark.walker@realbuzz.com>
2022-09-06 22:18:53 +0100

    ci: Extend the ignored files by flake8

--- cms/extensions/__init__.py
+++ cms/extensions/__init__.py
@@ -2,7 +2,6 @@ from .admin import PageExtensionAdmin, TitleExtensionAdmin
 from .extension_pool import extension_pool
 from .models import PageExtension, TitleExtension
 
-
 __all__ = [
     'extension_pool',
     'PageExtension',
