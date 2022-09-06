fa0af0c24d3b1629ca9aa82454f1c6949a367327
Mark Walker <mark.walker@realbuzz.com>
2022-09-06 01:26:21 +0100

    ci: tidy up E129

--- cms/test_utils/testcases.py
+++ cms/test_utils/testcases.py
@@ -464,8 +464,7 @@ class BaseCMSTestCase:
             self.fail("No warnings emitted")
         first = warningsShown[0]
         for other in warningsShown[1:]:
-            if ((other.message, other.category)
-                != (first.message, first.category)):
+            if (other.message, other.category) != (first.message, first.category):
                 self.fail("Can't handle different warnings")
         self.assertEqual(first.message, message)
         self.assertTrue(first.category is category)
