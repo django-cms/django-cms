0f71a87c3da61a5a5742aac65e65e294d5a7181c
Fabian Braun <fsbraun@gmx.de>
2022-09-27 08:51:55 +0200

    support: Django 4.1

--- cms/utils/compat/__init__.py
+++ cms/utils/compat/__init__.py
@@ -1,4 +1,4 @@
-from distutils.version import LooseVersion
+from packaging.version import Version
 from platform import python_version
 
 from django import get_version
@@ -7,7 +7,8 @@ DJANGO_VERSION = get_version()
 PYTHON_VERSION = python_version()
 
 # These means "less than or equal to DJANGO_FOO_BAR"
-DJANGO_2_2 = LooseVersion(DJANGO_VERSION) < LooseVersion('3.0')
-DJANGO_3_0 = LooseVersion(DJANGO_VERSION) < LooseVersion('3.1')
-DJANGO_3_1 = LooseVersion(DJANGO_VERSION) < LooseVersion('3.2')
-DJANGO_3_2 = LooseVersion('3.2') <= LooseVersion(DJANGO_VERSION) and  LooseVersion(DJANGO_VERSION) < LooseVersion('3.3')
+DJANGO_2_2 = Version(DJANGO_VERSION) < Version('3.0')
+DJANGO_3_0 = Version(DJANGO_VERSION) < Version('3.1')
+DJANGO_3_1 = Version(DJANGO_VERSION) < Version('3.2')
+DJANGO_3_2 = Version(DJANGO_VERSION) < Version('4.0')
+DJANGO_4_1 = Version(DJANGO_VERSION) < Version('4.2')
