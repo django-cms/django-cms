b0638844a8cc5cb62512f71dada0f18b97a505fb
Fabian Braun <fsbraun@gmx.de>
2022-09-30 16:29:53 +0200

    Disentangle pr

--- cms/models/placeholdermodel.py
+++ cms/models/placeholdermodel.py
@@ -292,7 +292,7 @@ class Placeholder(models.Model):
         Returns language objects for every language for which the placeholder
         has plugins.
 
-        This is not cached as it's meant to be used in the frontend editor.
+        This is not cached as it's meant to eb used in the frontend editor.
         """
 
         languages = []
@@ -602,9 +602,12 @@ class Placeholder(models.Model):
             )
         else:
             # moving to empty placeholder
-            # Mover out behind  last source position
             source_offset = source_last_plugin.position
 
+        # Shift all plugins whose position is greater than or equal to
+        # the plugin being moved. This includes the plugin itself.
+        # This is to create enough space in-between for the squashing
+        # to work without conflicts.
         self._shift_plugin_positions(
             plugin.language,
             start=plugin.position,
