c59b756a019981713670e7494a5fdb5fa0456a40
Fabian Braun <fsbraun@gmx.de>
2022-10-21 11:31:34 +0200

    Update models.py

--- cms/test_utils/project/emailuserapp/models.py
+++ cms/test_utils/project/emailuserapp/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import (
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
-from django.utils.http import urlquote
 
 
 class EmailUserManager(BaseUserManager):
