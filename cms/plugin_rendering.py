aa308c1a9e39da862c709d6eb31322c1fcd96794
Fabian Braun <fsbraun@gmx.de>
2022-10-30 09:49:50 +0100

    Use triple-quoted string for warning html

--- cms/plugin_rendering.py
+++ cms/plugin_rendering.py
@@ -496,14 +496,15 @@ class ContentRenderer(BaseRenderer):
     def render_exception(self, action, instance, context, placeholder, editable):
         if editable:
             exc, value, traceback = context['exc_info']
-            return f'<div style="display: inline-block; border: none; background: rgba(0, 0, 0, 0.5); ' \
-                   f'padding: 0.6em 0.3em; cursor: not-allowed; min-height: 2em; width:100%; vertical-align:middle; ' \
-                   f'text-align:center;">' \
-                   f'  <span style="font-weight: bold; color: darkred;"> ' \
-                   f'   Exception when {action} in {instance.plugin_type} (id={instance.id})<br />' \
-                   f'   {exc.__name__}: {value}' \
-                   f'  </span>' \
-                   f'</div>'
+            return f'''
+                <div style="display: inline-block; border: none; background: rgba(0, 0, 0, 0.5);
+                   padding: 0.6em 0.3em; cursor: not-allowed; min-height: 2em; width:100%; vertical-align:middle;
+                   text-align:center;">
+                    <span style="font-weight: bold; color: darkred;">
+                      Exception when {action} in {instance.plugin_type} (id={instance.id})<br />
+                      {exc.__name__}: {value}
+                    </span>
+                </div>'''
         return ''
 
     def render_plugins(self, placeholder, language, context, editable=False, template=None):
