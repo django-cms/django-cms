007e29fb22c1f43c8a35993bfbe8b56290537254
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2023-02-16 11:16:20 +0530

    fix: review feedback

--- cms/utils/compat/forms.py
+++ cms/utils/compat/forms.py
@@ -9,33 +9,16 @@ if settings.AUTH_USER_MODEL != 'auth.User':  # pragma: no cover
     user_app_name = settings.AUTH_USER_MODEL.split('.')[0]
     app = apps.get_app_config(user_app_name).models_module
 
-    try:
-        custom_admin = importlib.import_module(app.__name__[:-6] + "admin")
+    custom_admin = importlib.import_module(app.__name__[:-6] + "admin")
 
-        if hasattr(custom_admin, 'UserAdmin'):
-            UserAdmin = custom_admin.UserAdmin
-        else:
-            from django.contrib.auth.admin import UserAdmin
-    except ImportError:
-        from django.contrib.auth.admin import UserAdmin  # noqa
+    if hasattr(custom_admin, 'UserAdmin'):
+        UserAdmin = custom_admin.UserAdmin
 
     # user form classes
-    try:
-        custom_forms = importlib.import_module(app.__name__[:-6] + "forms")
+    custom_forms = importlib.import_module(app.__name__[:-6] + "forms")
 
-        if hasattr(custom_forms, 'UserCreationForm'):
-            UserCreationForm = custom_forms.UserCreationForm
-        else:
-            from django.contrib.auth.forms import UserCreationForm
+    if hasattr(custom_forms, 'UserCreationForm'):
+        UserCreationForm = custom_forms.UserCreationForm
 
-        if hasattr(custom_forms, 'UserChangeForm'):
-            UserChangeForm = custom_forms.UserChangeForm
-        else:
-            from django.contrib.auth.forms import UserChangeForm
-    except ImportError:
-        from django.contrib.auth.forms import UserChangeForm  # noqa
-        from django.contrib.auth.forms import UserCreationForm  # noqa
-else:
-    from django.contrib.auth.admin import UserAdmin  # noqa
-    from django.contrib.auth.forms import UserChangeForm  # noqa
-    from django.contrib.auth.forms import UserCreationForm  # noqa
+    if hasattr(custom_forms, 'UserChangeForm'):
+        UserChangeForm = custom_forms.UserChangeForm
