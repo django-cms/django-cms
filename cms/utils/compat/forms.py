520dea5d1312f6a80801ab5e7bc956537aa66e1c
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2023-02-16 13:00:43 +0530

    fix: broken ci tests

--- cms/utils/compat/forms.py
+++ cms/utils/compat/forms.py
@@ -4,21 +4,40 @@ from django.apps import apps
 from django.conf import settings
 
 # override with custom classes if they exist
+# noqa is used to avoid flake8 errors, as this is not used in
+# the same file but imported in other files
 if settings.AUTH_USER_MODEL != 'auth.User':  # pragma: no cover
     # UserAdmin class
     user_app_name = settings.AUTH_USER_MODEL.split('.')[0]
     app = apps.get_app_config(user_app_name).models_module
 
-    custom_admin = importlib.import_module(app.__name__[:-6] + "admin")
+    try:
+        custom_admin = importlib.import_module(app.__name__[:-6] + "admin")
 
-    if hasattr(custom_admin, 'UserAdmin'):
-        UserAdmin = custom_admin.UserAdmin
+        if hasattr(custom_admin, 'UserAdmin'):
+            UserAdmin = custom_admin.UserAdmin
+        else:
+            from django.contrib.auth.admin import UserAdmin
+    except ImportError:
+        from django.contrib.auth.admin import UserAdmin  # noqa
 
     # user form classes
-    custom_forms = importlib.import_module(app.__name__[:-6] + "forms")
+    try:
+        custom_forms = importlib.import_module(app.__name__[:-6] + "forms")
 
-    if hasattr(custom_forms, 'UserCreationForm'):
-        UserCreationForm = custom_forms.UserCreationForm
+        if hasattr(custom_forms, 'UserCreationForm'):
+            UserCreationForm = custom_forms.UserCreationForm
+        else:
+            from django.contrib.auth.forms import UserCreationForm
 
-    if hasattr(custom_forms, 'UserChangeForm'):
-        UserChangeForm = custom_forms.UserChangeForm
+        if hasattr(custom_forms, 'UserChangeForm'):
+            UserChangeForm = custom_forms.UserChangeForm
+        else:
+            from django.contrib.auth.forms import UserChangeForm # noqa
+    except ImportError:
+        from django.contrib.auth.forms import UserChangeForm  # noqa
+        from django.contrib.auth.forms import UserCreationForm  # noqa
+else:
+    from django.contrib.auth.admin import UserAdmin  # noqa
+    from django.contrib.auth.forms import UserChangeForm  # noqa
+    from django.contrib.auth.forms import UserCreationForm  # noqa
