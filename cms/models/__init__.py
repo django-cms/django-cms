46ef2ac9ab1fe2b3af0184676333f63910cde4da
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2023-04-08 01:32:19 +0530

    fix: ignore isort for this file as it causes circular import issues

--- cms/models/__init__.py
+++ cms/models/__init__.py
@@ -1,13 +1,13 @@
-# must be last
-from cms import signals as s_import  # nopyflakes
-
-from .aliaspluginmodel import *  # nopyflakes
-from .apphooks_reload import *  # nopyflakes
+# isort: skip_file
+from .settingmodels import *  # nopyflakes
 from .pagemodel import *  # nopyflakes
 from .permissionmodels import *  # nopyflakes
 from .placeholdermodel import *  # nopyflakes
-from .placeholderpluginmodel import *  # nopyflakes
 from .pluginmodel import *  # nopyflakes
-from .settingmodels import *  # nopyflakes
-from .static_placeholder import *  # nopyflakes
 from .titlemodels import *  # nopyflakes
+from .placeholderpluginmodel import *  # nopyflakes
+from .static_placeholder import *  # nopyflakes
+from .aliaspluginmodel import *  # nopyflakes
+from .apphooks_reload import *  # nopyflakes
+# must be last
+from cms import signals as s_import  # nopyflakes
