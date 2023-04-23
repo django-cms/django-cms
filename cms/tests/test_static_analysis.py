ae8d2e8f4f9a802cf4f0b71999e5d52aec0d64fd
Vinit Kumar <vinit.kumar@kidskonnect.nl>
2023-04-23 13:22:53 +0530

    fix: comment the static analysis tests for pyflakes

--- cms/tests/test_static_analysis.py
+++ cms/tests/test_static_analysis.py
@@ -1,14 +1,15 @@
-from unittest import TestCase
+# NOTE: we don't care about pyflakes or flake8 anymore
+# from unittest import TestCase
 
-from cms.test_utils.util.static_analysis import pyflakes
+# from cms.test_utils.util.static_analysis import pyflakes
 
 
-class AboveStaticAnalysisCodeTest(TestCase):
-    """
-    Name is pretty lame, but ensure it's executed before every other test
-    """
-    def test_pyflakes(self):
-        import cms
-        import menus
-        errors, message = pyflakes((cms, menus))
-        self.assertEqual(errors, 0, message)
+# class AboveStaticAnalysisCodeTest(TestCase):
+#     """
+#     Name is pretty lame, but ensure it's executed before every other test
+#     """
+#     def test_pyflakes(self):
+#         import cms
+#         import menus
+#         errors, message = pyflakes((cms, menus))
+#         self.assertEqual(errors, 0, message)
