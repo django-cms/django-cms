2e827ddfed4cc599f2fb05bb14b78abc032537f7
Fabian Braun <fsbraun@gmx.de>
2023-05-23 23:22:45 +0200

    fix eslint error

--- cms/admin/forms.py
+++ cms/admin/forms.py
@@ -10,8 +10,7 @@ from django.forms.widgets import HiddenInput
 from django.template.defaultfilters import slugify
 from django.utils import timezone
 from django.utils.encoding import force_str
-from django.utils.functional import cached_property
-from django.utils.translation import gettext
+from django.utils.translation import get_language, gettext
 from django.utils.translation import gettext_lazy as _
 
 from cms import api
