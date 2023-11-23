b973530adaa53937cc768f2925fcb94826626e25
Fabian Braun <fsbraun@gmx.de>
2023-11-23 16:32:03 +0100

    Skip test if not internet

--- cms/tests/test_djangocms_command.py
+++ cms/tests/test_djangocms_command.py
@@ -1,13 +1,27 @@
+import socket
 import subprocess
 import sys
 from tempfile import TemporaryDirectory
-from unittest import TestCase
+from unittest import TestCase, skipIf
+
+
+def has_no_internet():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(5)
+        s.connect(('4.4.4.2', 80))
+        s.send(b"hello")
+    except socket.error:  # no internet
+        return True
+    return False
+
 
 class DjangocmsCommandTest(TestCase):
-    """
-    Name is pretty lame, but ensure it's executed before every other test
-    """
+    @skipIf(has_no_internet(), "No internet")
     def test_djangocms_command(self):
         with TemporaryDirectory() as dir:
-            result = subprocess.run([sys.executable, "-m", "cms", "mysite", dir,  "--noinput"])
-        self.assertEqual(result.returncode, 0)
+            result = subprocess.run(
+                [sys.executable, "-m", "cms", "mysite", dir,  "--noinput"],
+                env={"DJANGOCMS_ALLOW_PIP_INSTALL": "True"},
+            )
+            self.assertEqual(result.returncode, 0)
