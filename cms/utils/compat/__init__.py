0950714e86d40d5a24d4270b39362a32b0021b37
Leonardo Cavallucci <leo.cavallucci@gmail.com>
2023-12-21 19:41:07 +0100

    feat: django 5 support for cms 3.11 (#7724)
    * feat: django 5 support (#7648)
    
    * Support for Django 5.0
    
    * Fix: test.yml and django Promise handling
    
    * Shorten github action names
    
    * Update test.yml
    
    * Update _cms.scss
    
    * Update setup.py
    
    * fix: django 5's choice widget is not lazy (#7707)
    
    * Support for Django 5.0
    
    * Fix: test.yml and django Promise handling
    
    * Shorten github action names
    
    * Update test.yml
    
    * Update _cms.scss
    
    * Update setup.py
    
    * Fix: Django 5 choice widget is not lazy either
    
    * Add test
    
    * Fix: Swapped underscore
    
    * Deprecate SuperLazyIterator and LazyChoiceField
    
    * Fix toolbar tests
    
    * Fix import sorting
    
    * Remove Django 2.2 as it was before
    
    * Remove useless DJANGO_5_0 compat variable
    
    * Pin correct Django 5.0 version
    
    Co-authored-by: Fabian Braun <fsbraun@gmx.de>
    
    * Do not deprecate LazyChoiceField
    
    Co-authored-by: Fabian Braun <fsbraun@gmx.de>
    
    * Do not deprecate SuperLazyIterator
    
    Co-authored-by: Fabian Braun <fsbraun@gmx.de>
    
    * Remove unused warning import
    
    ---------
    
    Co-authored-by: Fabian Braun <fsbraun@gmx.de>

--- cms/utils/compat/__init__.py
+++ cms/utils/compat/__init__.py
@@ -13,3 +13,4 @@ DJANGO_3_1 = Version(DJANGO_VERSION) < Version('3.2')
 DJANGO_3_2 = Version(DJANGO_VERSION) < Version('4.0')
 DJANGO_3 = DJANGO_3_2
 DJANGO_4_1 = Version(DJANGO_VERSION) < Version('4.2')
+DJANGO_4_2 = Version(DJANGO_VERSION) < Version('4.3')
