5925f9d81b85c3c2c8be791c83d7ee0a36b2a9da
Josh Peng Yu <joshyu@users.noreply.github.com>
2024-03-08 03:34:03 +0000

    fix: remove useless

--- cms/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-import django
-
-
-if django.VERSION < (4, 2):  # TODO: remove when dropping support for Django < 4.2
-    from django.test.testcases import TransactionTestCase
-
-    TransactionTestCase.assertQuerySetEqual = TransactionTestCase.assertQuerysetEqual
