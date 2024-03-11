3c7241f8307566d4807c64bc90d90ebff86c3fda
Fabian Braun <fsbraun@gmx.de>
2024-03-11 11:11:37 +0100

    Add update of PageContent Meta options to squashed migration

--- cms/migrations/0035_auto_20230822_2208_squashed_0036_auto_20240311_1028.py
+++ cms/migrations/0035_auto_20230822_2208_squashed_0036_auto_20240311_1028.py
@@ -12,4 +12,12 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AlterModelOptions(
+            name='pagecontent',
+            options={'default_permissions': [], 'verbose_name': 'page content', 'verbose_name_plural': 'page contents'},
+        ),
+        migrations.AlterModelOptions(
+            name='page',
+            options={'default_permissions': ('add', 'change', 'delete'), 'permissions': (('view_page', 'Can view page'), ('publish_page', 'Can publish page'), ('edit_static_placeholder', 'Can edit static placeholders')), 'verbose_name': 'page', 'verbose_name_plural': 'pages'},
+        ),
     ]
