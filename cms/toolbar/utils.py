600f85a72b741e46a4a5e816f778f0e4c9d884bc
Fabian Braun <fsbraun@gmx.de>
2024-03-21 21:13:35 +0100

    Fix linting

--- cms/toolbar/utils.py
+++ cms/toolbar/utils.py
@@ -16,6 +16,7 @@ from django.utils.translation import (
 from cms.constants import PLACEHOLDER_TOOLBAR_JS, PLUGIN_TOOLBAR_JS
 from cms.models import PageContent
 from cms.utils import get_language_list
+from cms.utils.compat.warnings import RemovedInDjangoCMS43Warning
 from cms.utils.conf import get_cms_setting
 from cms.utils.urlutils import admin_reverse
 
@@ -64,8 +65,12 @@ def get_plugin_toolbar_js(plugin, children=None, parents=None):
 
 
 def get_plugin_tree_as_json(request, plugins):
-    assert False, ("This function is deprecated. Use get_plugin_tree instead.")
+    import warnings
+
+    warnings.warn("get_plugin_tree_as_json is deprecated. Use get_plugin_tree instead.",
+                  RemovedInDjangoCMS43Warning, stacklevel=2)
     return json.dumps(get_plugin_tree(request, plugins))
+
 
 def get_plugin_tree(request, plugins):
     from cms.utils.plugins import downcast_plugins, get_plugin_restrictions
