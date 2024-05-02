61e4cefea93d33aaa6ead9abed4de78c464ee98e
Fabian Braun <fsbraun@gmx.de>
2024-05-02 16:56:54 +0200

    Fix: Set toolbar direction based on toolbar language

--- cms/toolbar/toolbar.py
+++ cms/toolbar/toolbar.py
@@ -168,6 +168,7 @@ class CMSToolbarBase(BaseToolbar):
         self.is_staff = None
         self.clipboard = None
         self.toolbar_language = None
+        self.toolbar_language_bidi = None
         self.show_toolbar = True
         self.init_toolbar(request, request_path=request_path)
         # Internal attribute to track whether we can cache
@@ -252,6 +253,7 @@ class CMSToolbarBase(BaseToolbar):
                 user_settings.language = self.request_language
                 user_settings.save()
             self.clipboard = user_settings.clipboard
+        self.toolbar_language_bidi = self.toolbar_language in settings.LANGUAGES_BIDI
 
         if hasattr(self, 'toolbars'):
             for key, toolbar in self.toolbars.items():
