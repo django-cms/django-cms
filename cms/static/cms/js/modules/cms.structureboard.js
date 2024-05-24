django-cms/django-cms

commit 9fbd71d4b58aba3eb6ad5be350f52eac270b27a9

fix: Only switch to structure endpoint on viewport breakpoint if in edit mode (#7920)

cms/static/cms/js/modules/cms.structureboard.js
@@ -181,7 +181,7 @@ class StructureBoard {
             const width = this.ui.window[0].innerWidth;
             const BREAKPOINT = 1024;
 
-            if (width > BREAKPOINT && !this.condensed) {
+            if (width > BREAKPOINT && !this.condensed && CMS.settings.mode === 'edit') {
                 this._makeCondensed();
             }
 
@@ -683,7 +683,7 @@ class StructureBoard {
 
         if (CMS.settings.mode === 'structure') {
             history.replaceState({}, '', CMS.config.settings.structure);
-            this.ui.html.addClass('cms-overflow');
+            $('html.cms-structure-mode-structure').addClass('cms-overflow');
         }
 
         this.ui.container.css('right', 0);

commit 201236a62d9a52cbbcef81e865810300aa8380db

fix: Correct regression introduced in #7920 (structure board breakpoint) (#7928)
* Revert "Removed default plugin creation for placeholders (#6468)"

This reverts commit eef5cbbfe3b4875b9b00db60611171d700c29ced.

* Fix: Only change structure board endpoint in edit mode

* Fix typo

* Revert "Revert "Removed default plugin creation for placeholders (#6468)""

This reverts commit 71f0d7fed35217ca374346a926c57494e01ae63d.

* fix: Correct patch

* Update cms.structureboard.js

cms/static/cms/js/modules/cms.structureboard.js
@@ -175,13 +175,13 @@ class StructureBoard {
 
     _events() {
         this.ui.window.on('resize.cms.structureboard', () => {
-            if (!this._loadedContent) {
+            if (!this._loadedContent || CMS.config.mode !== 'draft') {
                 return;
             }
             const width = this.ui.window[0].innerWidth;
             const BREAKPOINT = 1024;
 
-            if (width > BREAKPOINT && !this.condensed && CMS.settings.mode === 'edit') {
+            if (width > BREAKPOINT && !this.condensed) {
                 this._makeCondensed();
             }
 