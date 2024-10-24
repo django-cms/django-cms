769e6f3a365af7129937d7bb79bb9d117538482a
theShinigami <salimabdellanuru@gmail.com>
2024-10-24 19:24:19 +0300

    refactor: remove unused imports

--- cms/tests/test_class_properties.py
+++ cms/tests/test_class_properties.py
@@ -1,8 +1,7 @@
 from django.contrib.admin.utils import flatten_fieldsets
 
-from cms.admin.forms import AdvancedSettingsForm, ChangePageForm
+from cms.admin.forms import ChangePageForm
 from cms.models.contentmodels import PageContent
-from cms.models.pagemodel import Page
 from cms.test_utils.testcases import CMSTestCase
 
 
