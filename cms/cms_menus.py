234024d5e3acf9bda97ff6e954cd30148b573d9e
Github Release Action <info@django-cms.org>
2024-10-30 19:07:41 +0100

    Fix: Inconsistent page content models crashed menus

--- cms/cms_menus.py
+++ cms/cms_menus.py
@@ -140,8 +140,8 @@ def get_menu_node_for_page(renderer, page, language, fallbacks=None, endpoint=Fa
     for lang in [language] + fallbacks:
         translation = page.page_content_cache.get(lang)
 
-        if translation:
-            page_url = page.urls_cache[translation.language]
+        if translation and lang in page.urls_cache:
+            page_url = page.urls_cache[lang]
             # Do we have a redirectURL?
             attr["redirect_url"] = translation.redirect  # save redirect URL if any
 
