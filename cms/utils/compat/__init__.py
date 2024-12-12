02bbd78e899114965e3a34f724d6cc0542f0b4c8
Github Release Action <info@django-cms.org>
2024-12-12 13:52:53 +0100

    Keep compatibility flags for unsupported Django versions

--- cms/utils/compat/__init__.py
+++ cms/utils/compat/__init__.py
@@ -7,5 +7,11 @@ DJANGO_VERSION = get_version()
 PYTHON_VERSION = python_version()
 
 # These means "less than or equal to DJANGO_FOO_BAR"
+DJANGO_2_2 = Version(DJANGO_VERSION) < Version('3.0')
+DJANGO_3_0 = Version(DJANGO_VERSION) < Version('3.1')
+DJANGO_3_1 = Version(DJANGO_VERSION) < Version('3.2')
+DJANGO_3_2 = Version(DJANGO_VERSION) < Version('3.3')
+DJANGO_3 = Version(DJANGO_VERSION) < Version('4.0')
+DJANGO_4_1 = Version(DJANGO_VERSION) < Version('4.2')
 DJANGO_4_2 = Version(DJANGO_VERSION) < Version('4.3')
 DJANGO_5_1 = Version(DJANGO_VERSION) < Version('5.2.dev')  # To allow testing against django's main branch
