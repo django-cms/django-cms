840e536fbabcc9df5a8812cd0f64dae6e15594c6
Fabian Braun <fsbraun@gmx.de>
2025-01-23 08:17:03 +0100

    Remove inline script from data bridge

--- cms/static/cms/js/admin.base.js
+++ cms/static/cms/js/admin.base.js
@@ -2,7 +2,7 @@
 import './polyfills/function.prototype.bind.js';
 import './libs/pep';
 
-import { Helpers, KEYS } from './modules/cms.base';
+import {Helpers, KEYS} from './modules/cms.base';
 import $ from 'jquery';
 import Class from 'classjs';
 
@@ -15,6 +15,23 @@ const CMS = {
     KEYS
 };
 
-// in case some data is already attached to the CMS global
-// we shoud not override it
+// in case some data is already attached to the global CMS
+// we must not override it
 window.CMS = CMS.$.extend(window.CMS || {}, CMS);
+
+// Serve the data bridge:
+// We have a special case here cause the CMS namespace
+// can be either inside the current window or the parent
+if (document.querySelector('body.cms-close-frame script#data-bridge')) {
+    (function (Window) {
+        // the dataBridge is used to access plugin information from different resources
+        // Do NOT move this!!!
+        Window.CMS.API.Helpers.dataBridge = JSON.parse(document.getElementById('data-bridge').textContent);
+        // make sure we're doing after the "modal" mechanism kicked in
+        setTimeout(function () {
+            // save current plugin
+            Window.CMS.API.Helpers.onPluginSave();
+        }, 100);
+    })(window.parent || window);
+};
+
