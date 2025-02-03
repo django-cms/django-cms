63a38367beffb2aaf313c8960e7c58a71cbe5ff0
Jacob Rief <jacob.rief@gmail.com>
2025-02-03 08:59:29 +0100

    fix: #7904 - `manage.py cms fixtree` did not fix PageUrl model (#7905)
    * add management command to fix PageUrl model
    
    * add management command to fix PageUrl model
    
    * fix ruff complains
    
    * Update tree.py
    
    * Update tree.py
    
    ---------
    
    Co-authored-by: Vinit Kumar <vinit.kumar@kidskonnect.nl>
    Co-authored-by: Fabian Braun <fsbraun@gmx.de>

--- cms/tests/test_management.py
+++ cms/tests/test_management.py
@@ -73,7 +73,7 @@ class ManagementTestCase(CMSTestCase):
         page1.save()
         out = StringIO()
         management.call_command('cms', 'fix-tree', interactive=False, stdout=out)
-        self.assertEqual(out.getvalue(), 'fixing page tree\nall done\n')
+        self.assertEqual(out.getvalue(), 'fixing page tree\nfixing page URLs\nall done\n')
         page1 = page1.reload()
         self.assertEqual(page1.path, "0002")
         self.assertEqual(page1.depth, 1)
