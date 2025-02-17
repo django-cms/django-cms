ffe3427685ae8028b8d7d4afbb0e47257b358079
Fabian Braun <fsbraun@gmx.de>
2025-02-17 11:23:24 +0100

    fix: Remove MiddlewareMixin from middlewares

--- cms/middleware/user.py
+++ cms/middleware/user.py
@@ -3,13 +3,15 @@ permission system.
 
 This middleware is required only when CMS_PERMISSION = True.
 """
-from django.utils.deprecation import MiddlewareMixin
 
-
-class CurrentUserMiddleware(MiddlewareMixin):
+class CurrentUserMiddleware:
     async_capable = False
 
-    def process_request(self, request):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
         from cms.utils.permissions import set_current_user
 
         set_current_user(getattr(request, 'user', None))
+        return self.get_response(request)
