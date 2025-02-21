c9692ed6a562c9c98b225c3cd06a27433a20babf
Fabian Braun <fsbraun@gmx.de>
2025-02-21 21:15:24 +0100

    fix: Default value not necessary

--- cms/plugin_base.py
+++ cms/plugin_base.py
@@ -219,7 +219,7 @@ class CMSPluginBase(admin.ModelAdmin, metaclass=CMSPluginBaseMetaclass):
 
     #: The plugin does not modify the context or request and its rendering is not influenced by its parent
     #: plugins.
-    is_local = not get_cms_setting("ALWAYS_REFRESH_CONTENT", False)
+    is_local = not get_cms_setting("ALWAYS_REFRESH_CONTENT")
 
     # Warning: setting these to False, may have a serious performance impact,
     # because their child-parent-relation must be recomputed each
