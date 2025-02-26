24f38ca06000e4dd934cd6a49127cb09b7dca648
Fabian Braun <fsbraun@gmx.de>
2025-02-26 20:11:11 +0100

    feat: Run new javascript when updating the full content

--- cms/static/cms/js/modules/cms.structureboard.js
+++ cms/static/cms/js/modules/cms.structureboard.js
@@ -1112,7 +1112,12 @@ class StructureBoard {
             $(`script[data-cms-plugin]#cms-plugin-${id}`).remove();
         });
         this._updateSekizai(data, 'css');
-        this._updateSekizai(data, 'js');
+        if (this._updateSekizai(data, 'js') === 0) {
+            // No scripts need to be loaded - content update is done
+            Helpers._getWindow().document.dispatchEvent(new Event('DOMContentLoaded'));
+            Helpers._getWindow().dispatchEvent(new Event('load'));
+            $(Helpers._getWindow()).trigger('cms-content-refresh');    
+        };
 
         this._contentChanged(data.messages);
         return false;
@@ -1168,31 +1173,19 @@ class StructureBoard {
 
         // Collect deferred scripts to ensure firing
         this.scriptReferenceCount = 0;
-        const scriptLoaded = () => {
-            if (--this.scriptReferenceCount < 1) {
-                Helpers._getWindow().dispatchEvent(new Event('load'));
-                $(Helpers._getWindow()).trigger('cms-content-refresh');
-            }
-        };
 
         for (const element of newElements.querySelectorAll(selector)) {
-            if (this._elementPresent(current, element)) {
+            if (StructureBoard._elementPresent(current, element)) {
                 element.remove();
             } else {
                 if (element.hasAttribute('src')) {
                     this.scriptReferenceCount++;
-                    element.onerror = element.onload = scriptLoaded;
+                    element.onload = element.onerror = this._scriptLoaded.bind(this);
                 }
                 location.appendChild(element);
             }
         }
         return this.scriptReferenceCount > 0;
-    }
-
-    _elementPresent(current, element) {
-        const markup = element.outerHTML;
-
-        return [...current].some(el => el.outerHTML === markup);
     }
 
     _loadToolbar() {
@@ -1393,7 +1386,7 @@ class StructureBoard {
         }
         const headDiff = dd.diff(document.head, nodeToObj(newDoc.head));
 
-        StructureBoard._replaceBodyWithHTML(newDoc.body);
+        this._replaceBodyWithHTML(newDoc.body);
         dd.apply(document.head, headDiff);
 
         toolbar.prependTo(document.body);
@@ -1406,9 +1399,6 @@ class StructureBoard {
 
     _contentChanged(messages) {
         Plugin._refreshPlugins();
-
-        Helpers._getWindow().dispatchEvent(new Event('load'));
-        $(Helpers._getWindow()).trigger('cms-content-refresh');
         if (messages) {
             CMS.API.Messages.close();
             if (messages.length) {
@@ -1604,15 +1594,90 @@ class StructureBoard {
         });
     }
 
-    static _replaceBodyWithHTML(body, forceFullReplace = true) {
-        if (forceFullReplace) {
-            // Resets all events etc.
-            document.body.innerHTML = body.innerHTML;
-        } else {
-            // Mostly small changes to the body, so we can just diff and apply
-            const bodyDiff = dd.diff(document.body, body);
+    /**
+     * Replaces the current document body with the provided HTML content.
+     * 
+     * This method removes all existing script elements from the document body,
+     * replaces the body content with the new HTML, and then re-inserts new script
+     * elements to ensure they are executed.
+     * 
+     * @param {HTMLElement} body - The new HTML content to replace the current body.
+     * 
+     * @private
+     */
+    _replaceBodyWithHTML(body) {
+        const oldScripts = document.body.querySelectorAll('script:not([type="application/json"])');  // get old scripts
+        for (const script of oldScripts) {
+            script.remove();  // detach them
+        }
+        console.log(oldScripts);
+        // Resets all events etc.
+        document.body.innerHTML = body.innerHTML;
 
-            dd.apply(document.body, bodyDiff);
+        const newScripts = document.body.querySelectorAll('script:not([type="application/json"])');
+        console.log(newScripts);
+
+        // Collect deferred scripts to ensure firing
+        this.scriptReferenceCount = 0;
+
+        for (const script of newScripts) {
+            if (!StructureBoard._elementPresent(oldScripts, script)) {
+                // Rewrite script to DOM to force execution
+                console.log("new script", script.outerHTML);
+                const newScript = document.createElement('script');
+                // Copy all attributes
+                for (const attr of script.attributes) {
+                    newScript.setAttribute(attr.name, attr.value);
+                }
+                if (script.src) {
+                    newScript.src = script.src;
+                    this.scriptReferenceCount++;
+                    newScript.onload = newScript.onerror = this._scriptLoaded.bind(this);
+                } else {
+                    newScript.textContent = script.textContent;
+                }
+                // Insert the new script at the same position
+                script.parentNode.insertBefore(newScript, script.nextSibling);
+                script.remove();
+            }
+        }
+
+        if (this.scriptReferenceCount === 0) {
+            console.log("no scripts to load - refresh");
+            // No scripts need to be loaded - content update is done
+            Helpers._getWindow().document.dispatchEvent(new Event('DOMContentLoaded'));
+            Helpers._getWindow().dispatchEvent(new Event('load'));
+            $(Helpers._getWindow()).trigger('cms-content-refresh');    
+        }
+    }
+
+    /**
+     * Checks if a given element is present in the current set of elements.
+     *
+     * @param {NodeList} current - The current set of elements to check against.
+     * @param {Element} element - The element to check for presence.
+     * @returns {boolean} - Returns true if the element is present in the current set, otherwise false.
+     * @private
+     */
+    static _elementPresent(current, element) {
+        const markup = element.outerHTML;
+
+        return [...current].some(el => el.outerHTML === markup);
+    }
+
+    /**
+     * Handles the event when a script is loaded.
+     * Decrements the script reference count and, if it reaches zero,
+     * dispatches a 'load' event on the window and triggers a 'cms-content-refresh' event.
+     *
+     * @private
+     */
+    _scriptLoaded() {
+        if (--this.scriptReferenceCount < 1) {
+            console.log("All scripts loaded - refresh");
+            Helpers._getWindow().document.dispatchEvent(new Event('DOMContentLoaded'));
+            Helpers._getWindow().dispatchEvent(new Event('load'));
+            $(Helpers._getWindow()).trigger('cms-content-refresh');    
         }
     }
 
