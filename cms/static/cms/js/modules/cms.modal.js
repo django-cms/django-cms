044915e3af42fcbe017a605ee1e6361fe5fd52d4
Fabian Braun <fsbraun@gmx.de>
2025-03-20 09:50:03 +0100

    Remove need for timeout to close modal

--- cms/static/cms/js/modules/cms.modal.js
+++ cms/static/cms/js/modules/cms.modal.js
@@ -1019,22 +1019,19 @@ class Modal {
                         true
                     );
                 } else {
-                    setTimeout(function() {
-                        if (that.justDeleted && (that.justDeletedPlugin || that.justDeletedPlaceholder)) {
-                            CMS.API.StructureBoard.invalidateState(
-                                that.justDeletedPlaceholder ? 'CLEAR_PLACEHOLDER' : 'DELETE',
-                                {
-                                    plugin_id: that.justDeletedPlugin,
-                                    placeholder_id: that.justDeletedPlaceholder,
-                                    deleted: true
-                                }
-                            );
-                        }
+                    // Serve the data bridge:
+                    // We have a special case here cause the CMS namespace
+                    // can be either inside the current window or the parent
+                    const dataBridge = body[0].querySelector('script#data-bridge');
+                    if (dataBridge) {
                         // hello ckeditor
                         Helpers.removeEventListener('modal-close.text-plugin');
                         that.close();
-                    // must be more than 100ms
-                    }, 150); // eslint-disable-line
+                        // the dataBridge is used to access plugin information from different resources
+                        // Do NOT move this!!!
+                        CMS.API.Helpers.dataBridge = JSON.parse(dataBridge.textContent);
+                        CMS.API.Helpers.onPluginSave();
+                    }
                 }
             } else {
                 if (that.ui.modal.hasClass('cms-modal-open')) {
