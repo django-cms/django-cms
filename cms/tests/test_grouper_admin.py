b89f5edf3f77ee4d562f7a14c6d4ce6b9d73a08b
Fabian Braun <fsbraun@gmx.de>
2025-03-31 11:49:14 +0200

    Update cms/tests/test_grouper_admin.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- cms/tests/test_grouper_admin.py
+++ cms/tests/test_grouper_admin.py
@@ -246,7 +246,7 @@ class SimpleGrouperChangeListTestCase(SimpleSetupMixin, CMSTestCase):
         random_content = self.createContentInstance("en")
         with self.login_user_context(self.admin_user):
             # Act
-            response = self.client.get(self.change_url + "?language=en", follow=True)
+            response = self.client.get(f"{self.change_url}?language=en", follow=True)
             # Assert
             # Contains relation to grouper as hidden input
             self.assertContains(
