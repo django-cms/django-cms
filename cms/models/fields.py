eb5b165e7301d405cbe82ab3f79e6807f0afd0d5
Fabian Braun <fsbraun@gmx.de>
2025-05-21 09:34:53 +0200

    Update cms/models/fields.py

--- cms/models/fields.py
+++ cms/models/fields.py
@@ -43,7 +43,6 @@ class PlaceholderField(models.ForeignKey):
     See documentation of :class:`~cms.models.fields.PlaceholderRelationField` for how to replace the code.
     """
     def __init__(self, slotname, *args, **kwargs):
-    def __init__(self, slotname, *args, **kwargs):
         kwargs.update({'null': True})  # always allow Null
         kwargs.update({'editable': False})  # never allow edits in admin
         # We hard-code the `to` argument for ForeignKey.__init__
