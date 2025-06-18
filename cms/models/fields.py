7eb2223b762d44d98a1a6a7dd9cd05db07f25949
Fabian Braun <fsbraun@gmx.de>
2025-06-18 08:43:54 +0200

    fix: add `system_check_removed_details` message to `PlaceholderField`

--- cms/models/fields.py
+++ cms/models/fields.py
@@ -42,6 +42,14 @@ class PlaceholderField(models.ForeignKey):
     the built-in migrations will automatically take care of the replacement.
     See documentation of :class:`~cms.models.fields.PlaceholderRelationField` for how to replace the code.
     """
+    system_check_removed_details = {
+        "msg": (
+            "PlaceholderField is for django CMS versions below 4 only. It may only be used inside migrations."
+        ),
+        "hint": "Use PlaceholderRelationField instead.",
+        "id": "cms.E001",
+    }
+
     def __init__(self, slotname, *args, **kwargs):
         kwargs.update({'null': True})  # always allow Null
         kwargs.update({'editable': False})  # never allow edits in admin
