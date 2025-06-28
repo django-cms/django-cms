6c8704e16613e6b441f15158bb55eca002e9c8f3
Fabian Braun <fsbraun@gmx.de>
2025-06-28 16:10:46 +0200

    fix: Wizard pool depr message

--- cms/wizards/wizard_pool.py
+++ cms/wizards/wizard_pool.py
@@ -1,7 +1,7 @@
 from django.apps import apps
 from django.utils.translation import gettext as _
 
-from cms.utils.compat.warnings import RemovedInDjangoCMS51Warning
+from cms.utils.compat.warnings import RemovedInDjangoCMS60Warning
 from cms.wizards.helpers import get_entries, get_entry  # noqa: F401
 from cms.wizards.wizard_base import Wizard, entry_choices  # noqa: F401
 
@@ -44,7 +44,7 @@ class WizardPool:
 
         warnings.warn(
             "Using wizard_pool is deprecated. Use the cms_config instead.",
-            RemovedInDjangoCMS51Warning,
+            RemovedInDjangoCMS60Warning,
             stacklevel=2,
         )
         assert isinstance(entry, Wizard), "entry must be an instance of Wizard"
