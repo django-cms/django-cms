60444929e28c77650aa7311c3249fc3c427d477b
Fabian Braun <fsbraun@gmx.de>
2025-07-09 17:03:35 +0200

    fix: Allow lazy wizard initialization (#8266) (#8283)
    * fix: Allow lazy wizard initialization (#8266)
    
    * fix: Allow lazy wizard initialization (#8265)
    
    * fix: Allow lazy wizard initialization
    
    * Update cms/cms_toolbars.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update cms/cms_config.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * feat: add "clear_wizard_cache"
    
    * fix: Wizard pool depr message
    
    * Update wizard_base.py
    
    * fix  linting
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * Update changelog
    
    * fix merge error
    
    * consistent deprecation warning
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- cms/wizards/wizard_base.py
+++ cms/wizards/wizard_base.py
@@ -15,15 +15,14 @@ from cms.toolbar.utils import (
 
 def get_entries():
     """
-    Returns a list of (wizard.id, wizard) tuples (for all registered
-    wizards) ordered by weight
+    Returns a list of Wizard objects (for all registered wizards) ordered by weight
 
     ``get_entries()`` is useful if it is required to have a list of all registered
     wizards. Typically, this is used to iterate over them all. Note that they will
     be returned in the order of their ``weight``: smallest numbers for weight are
     returned first.::
 
-        for wizard_id, wizard in get_entries():
+        for wizard in get_entries():
             # do something with a wizard...
     """
     wizards = apps.get_app_config('cms').cms_extension.wizards
@@ -39,12 +38,21 @@ def get_entry(entry_key):
 
 def entry_choices(user, page):
     """
-    Yields a list of wizard entries that the current user can use based on their
-    permission to add instances of the underlying model objects.
+    Yields a list of wizard entry tuples of the form (wizard.id, wizard.title) that
+    the current user can use based on their permission to add instances of the
+    underlying model objects.
     """
     for entry in get_entries():
         if entry.user_has_add_permission(user, page=page):
             yield (entry.id, entry.title)
+
+
+def clear_wizard_cache():
+    """
+    Clears the wizard cache. This is useful if you have added or removed wizards
+    and want to ensure that the changes are reflected immediately.
+    """
+    del apps.get_app_config('cms').cms_extension.wizards
 
 
 class WizardBase:
