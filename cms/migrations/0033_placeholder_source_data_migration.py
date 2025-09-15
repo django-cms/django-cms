c7c0b7a3540c2b6a8828ee957182ba2f26dc5437
Fabian Braun <fsbraun@gmx.de>
2025-09-15 16:37:10 +0200

    fix: Migration 0033 failed when empty palceholder objects were not present in the db (#8340)

--- cms/migrations/0033_placeholder_source_data_migration.py
+++ cms/migrations/0033_placeholder_source_data_migration.py
@@ -39,7 +39,7 @@ def forwards(apps, schema_editor):
                             content_type_id=cur_ct_obj.pk,
                             object_id=obj.pk,
                         )
-                    else:
+                    elif obj_placeholder_field:
                         obj_placeholder_field.content_type_id = cur_ct_obj.pk
                         obj_placeholder_field.object_id = obj.pk
                         obj_placeholder_field.save()
