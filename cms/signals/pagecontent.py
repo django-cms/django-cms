0fabc742c0eed074a2574410b78714accb16fb94
[Aaditya1273] <aadityaiitian1515@gmail.com>
2025-10-21 16:57:23 +0530

    fix: correct import and resolve linting issues

--- cms/signals/pagecontent.py
+++ cms/signals/pagecontent.py
@@ -16,20 +16,22 @@ logger = logging.getLogger(__name__)
 def handle_pagecontent_post_save(sender, instance, created, **kwargs):
     """
     Handle PageContent post_save signal.
-    
+
     When a new PageContent (language variant) is created for a page that has
     an apphook, we need to trigger URL reloading to ensure the apphook URLs
     are available for the new language.
-    
+
     This fixes the issue where creating a new language variant for a page
-    with an apphook causes NoReverseMatch exceptions until the server is restarted.
+    with an apphook causes NoReverseMatch exceptions until the server is
+    restarted.
     """
     if created and instance.page.application_urls:
         # A new language variant was created for a page with an apphook
-        # Trigger URL reloading to make apphook URLs available for this language
+        # Trigger URL reloading to make apphook URLs available for this
+        # language
         logger.debug(
             f"New language variant '{instance.language}' created for page "
-            f"'{instance.page}' with apphook '{instance.page.application_urls}'. "
-            f"Triggering URL reload."
+            f"'{instance.page}' with apphook "
+            f"'{instance.page.application_urls}'. Triggering URL reload."
         )
         set_restart_trigger()
--- cms/tests/test_apphook_reload_language_variants.py
+++ cms/tests/test_apphook_reload_language_variants.py
@@ -6,23 +6,23 @@ for pages with apphooks causes NoReverseMatch exceptions.
 """
 from unittest.mock import patch
 
-from django.test import TestCase, override_settings
+from django.test import override_settings
 
 from cms.api import create_page, create_page_content
 from cms.models import UrlconfRevision
 from cms.test_utils.testcases import CMSTestCase
 from cms.test_utils.project.sampleapp.cms_apps import SampleApp
-from cms.utils.setup import apphooks
+from cms.test_utils.util.context_managers import apphooks
 
 
 @override_settings(
     MIDDLEWARE=['cms.middleware.utils.ApphookReloadMiddleware'] + [
-        m for m in __import__('django.conf', fromlist=['settings']).settings.MIDDLEWARE
+        m for m in __import__('django.conf', fromlist=['settings']).settings.MIDDLEWARE  # noqa: E501
         if m != 'cms.middleware.utils.ApphookReloadMiddleware'
     ]
 )
 class ApphookReloadLanguageVariantTests(CMSTestCase):
-    """Test that URL reloading is triggered when creating language variants for apphook pages."""
+    """Test URL reloading when creating language variants for apphook pages."""
 
     def test_new_language_variant_triggers_url_reload(self):
         """
@@ -38,10 +38,10 @@ class ApphookReloadLanguageVariantTests(CMSTestCase):
                 application_urls="SampleApp",
                 application_namespace="test_app"
             )
-            
+
             # Get the initial URL revision
             initial_revision, _ = UrlconfRevision.get_or_create_revision()
-            
+
             # Create a new language variant (German)
             # This should trigger URL reloading
             create_page_content(
@@ -49,19 +49,20 @@ class ApphookReloadLanguageVariantTests(CMSTestCase):
                 title="Test Seite",
                 page=page
             )
-            
+
             # Check that the URL revision has changed
             new_revision, _ = UrlconfRevision.get_or_create_revision()
             self.assertNotEqual(
-                initial_revision, 
+                initial_revision,
                 new_revision,
-                "URL revision should change when creating a language variant for an apphook page"
+                "URL revision should change when creating a language variant "
+                "for an apphook page"
             )
 
     def test_new_language_variant_without_apphook_no_reload(self):
         """
-        Test that creating a new language variant for a page without an apphook
-        does NOT trigger URL reloading.
+        Test that creating a new language variant for a page without an
+        apphook does NOT trigger URL reloading.
         """
         # Create a page without an apphook in English
         page = create_page(
@@ -69,10 +70,10 @@ class ApphookReloadLanguageVariantTests(CMSTestCase):
             template="nav_playground.html",
             language="en"
         )
-        
+
         # Get the initial URL revision
         initial_revision, _ = UrlconfRevision.get_or_create_revision()
-        
+
         # Create a new language variant (German)
         # This should NOT trigger URL reloading
         create_page_content(
@@ -80,20 +81,22 @@ class ApphookReloadLanguageVariantTests(CMSTestCase):
             title="Test Seite",
             page=page
         )
-        
+
         # Check that the URL revision has NOT changed
         new_revision, _ = UrlconfRevision.get_or_create_revision()
         self.assertEqual(
-            initial_revision, 
+            initial_revision,
             new_revision,
-            "URL revision should NOT change when creating a language variant for a non-apphook page"
+            "URL revision should NOT change when creating a language variant "
+            "for a non-apphook page"
         )
 
     @patch('cms.signals.pagecontent.set_restart_trigger')
-    def test_signal_handler_called_for_apphook_page(self, mock_set_restart_trigger):
+    def test_signal_handler_called_for_apphook_page(self,
+                                                    mock_set_restart_trigger):
         """
-        Test that the signal handler is called when creating a language variant
-        for a page with an apphook.
+        Test that the signal handler is called when creating a language
+        variant for a page with an apphook.
         """
         with apphooks(SampleApp):
             # Create a page with an apphook
@@ -104,25 +107,26 @@ class ApphookReloadLanguageVariantTests(CMSTestCase):
                 application_urls="SampleApp",
                 application_namespace="test_app"
             )
-            
+
             # Reset the mock to ignore the call from page creation
             mock_set_restart_trigger.reset_mock()
-            
+
             # Create a new language variant
             create_page_content(
                 language="de",
                 title="Test Seite",
                 page=page
             )
-            
+
             # Verify that set_restart_trigger was called
             mock_set_restart_trigger.assert_called_once()
 
     @patch('cms.signals.pagecontent.set_restart_trigger')
-    def test_signal_handler_not_called_for_non_apphook_page(self, mock_set_restart_trigger):
+    def test_signal_handler_not_called_for_non_apphook_page(
+            self, mock_set_restart_trigger):
         """
-        Test that the signal handler is NOT called when creating a language variant
-        for a page without an apphook.
+        Test that the signal handler is NOT called when creating a language
+        variant for a page without an apphook.
         """
         # Create a page without an apphook
         page = create_page(
@@ -130,25 +134,26 @@ class ApphookReloadLanguageVariantTests(CMSTestCase):
             template="nav_playground.html",
             language="en"
         )
-        
+
         # Reset the mock to ignore any previous calls
         mock_set_restart_trigger.reset_mock()
-        
+
         # Create a new language variant
         create_page_content(
             language="de",
             title="Test Seite",
             page=page
         )
-        
+
         # Verify that set_restart_trigger was NOT called
         mock_set_restart_trigger.assert_not_called()
 
     @patch('cms.signals.pagecontent.set_restart_trigger')
-    def test_signal_handler_not_called_for_existing_content_update(self, mock_set_restart_trigger):
+    def test_signal_handler_not_called_for_existing_content_update(
+            self, mock_set_restart_trigger):
         """
-        Test that the signal handler is NOT called when updating existing PageContent
-        (only when creating new ones).
+        Test that the signal handler is NOT called when updating existing
+        PageContent (only when creating new ones).
         """
         with apphooks(SampleApp):
             # Create a page with an apphook
@@ -159,14 +164,14 @@ class ApphookReloadLanguageVariantTests(CMSTestCase):
                 application_urls="SampleApp",
                 application_namespace="test_app"
             )
-            
+
             # Reset the mock to ignore the call from page creation
             mock_set_restart_trigger.reset_mock()
-            
+
             # Update the existing page content (not creating new)
             page_content = page.get_content_obj("en")
             page_content.title = "Updated Title"
             page_content.save()
-            
+
             # Verify that set_restart_trigger was NOT called for updates
             mock_set_restart_trigger.assert_not_called()
