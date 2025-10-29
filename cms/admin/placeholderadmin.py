5298a431e672190ef4ba19bd420b811313a46fd8
Fabian Braun <fsbraun@gmx.de>
2025-10-29 13:05:20 +0100

    fix: Copying x-language lead to unique constraint violation (#8366)
    * fix: Copying x-language lead to unique constraint violation
    
    * Update cms/tests/test_plugins.py
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * fix: test
    
    * fix: import errors
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- cms/admin/placeholderadmin.py
+++ cms/admin/placeholderadmin.py
@@ -769,7 +769,7 @@ class PlaceholderAdmin(admin.ModelAdmin):
             target_parent_id=target_parent_id,
         )
 
-        target_last_plugin = target_placeholder.get_last_plugin(plugin.language)
+        target_last_plugin = target_placeholder.get_last_plugin(target_language)
 
         if target_last_plugin:
             target_offset = target_last_plugin.position + len(plugins)
