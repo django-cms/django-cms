9e00f694ee0afe637521177a139d69a02d752f7f
Fabian Braun <fsbraun@gmx.de>
2025-11-06 22:20:59 +0100

    fix: Ensure edit endpoint language selection when admin is not using i18n_patterns (#8367) (#8390)
    * fix: Select correct language for edit endpoints when there are no i18n admin urls
    
    * Add tests

--- cms/toolbar/utils.py
+++ cms/toolbar/utils.py
@@ -12,6 +12,7 @@ from django.urls import NoReverseMatch
 from django.utils.encoding import force_str
 from django.utils.translation import (
     get_language,
+    get_language_from_path,
     gettext,
     override as force_language,
 )
@@ -250,6 +251,8 @@ def get_object_edit_url(obj: models.Model, language: str = None) -> str:
 
     with force_language(language):
         url = admin_reverse('cms_placeholder_render_object_edit', args=[content_type.pk, obj.pk])
+        if not get_language_from_path(url):
+            url += f'?language={language}'
     if get_cms_setting('ENDPOINT_LIVE_URL_QUERYSTRING_PARAM_ENABLED'):
         url = add_live_url_querystring_param(obj, url, language)
     return url
@@ -270,6 +273,8 @@ def get_object_preview_url(obj: models.Model, language: str = None) -> str:
 
     with force_language(language):
         url = admin_reverse('cms_placeholder_render_object_preview', args=[content_type.pk, obj.pk])
+        if not get_language_from_path(url):
+            url += f'?language={language}'
     if get_cms_setting('ENDPOINT_LIVE_URL_QUERYSTRING_PARAM_ENABLED'):
         url = add_live_url_querystring_param(obj, url, language)
     return url
@@ -290,8 +295,10 @@ def get_object_structure_url(obj: models.Model, language: str = None) -> str:
         language = get_language()
 
     with force_language(language):
-        return admin_reverse('cms_placeholder_render_object_structure', args=[content_type.pk, obj.pk])
-
+        url = admin_reverse('cms_placeholder_render_object_structure', args=[content_type.pk, obj.pk])
+        if not get_language_from_path(url):
+            url += f'?language={language}'
+    return url
 
 def get_object_for_language(obj: models.Model, language: str, latest: bool = False) -> models.Model | None:
     """
