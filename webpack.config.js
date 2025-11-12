12b8c6dbd0c25bc40c5cddf52ae9c8fe9b82a0fa
Fabian Braun <fsbraun@gmx.de>
2025-11-12 12:07:20 +0100

    fix: Include licenses of bundled open source libraries

--- webpack.config.js
+++ webpack.config.js
@@ -72,14 +72,76 @@ module.exports = function(opts) {
         },
         plugins: [
             new LicenseWebpackPlugin({
-                outputFilename: 'LICENSE-BUNDLES',
+                outputFilename: '../../../../../../LICENSE',
                 perChunkOutput: false,
                 addBanner: false,
                 renderLicenses: (modules) => {
-                    return modules.map(module => {
+                    const copyright = `Copyright (c) 2008-present, Batiste Bieler
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of the author nor the names of other
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+This software bundles Javascript open source software using WebPack.
+Please find their copyright notices here:
+
+================================================================================
+
+Package: class.min.js
+Version: 1.0
+Author: Angelo Dini
+License: BSD License
+Copyright: Distributed under the BSD License.
+
+${'='.repeat(80)}
+
+Package: jQuery
+Version: 1.11.3
+License: MIT License
+Copyright: (c) 2005, 2015 jQuery Foundation, Inc.
+License URL: jquery.org/license
+
+${'='.repeat(80)}
+
+Package: jsTree
+Version: 3.3.0 (2016-04-25)
+License: MIT License
+License URL: https://github.com/vakata/jstree/blob/master/LICENSE-MIT
+Copyright: (c) jsTree project
+Note: THIS FILE HAS BEEN MANUALLY PATCHED TO ALLOW COPYING INTO SELF/CHILDREN
+Source: https://github.com/FinalAngel/jstree
+
+${'='.repeat(80)}
+
+`;
+                    const licenses = modules.map(module => {
                         const licenseText = module.licenseText || 'License text not found';
                         return `Package: ${module.packageJson.name || module.name}\nVersion: ${module.packageJson.version || 'N/A'}\nLicense: ${module.packageJson.license || 'N/A'}\n\n${licenseText}\n\n${'='.repeat(80)}\n`;
                     }).join('\n');
+                    return copyright + licenses;
                 }
             })
         ],
