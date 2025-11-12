103ef0565954541c202030cbacc01f8712911200
Fabian Braun <fsbraun@gmx.de>
2025-11-12 12:18:44 +0100

    fix: improve license formatting

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,72 @@ var path = require('path');
 var TerserPlugin = require('terser-webpack-plugin');
 var LicenseWebpackPlugin = require('license-webpack-plugin').LicenseWebpackPlugin;
 
+const cmsCopyright = `Copyright (c) 2008-present, Batiste Bieler
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of the author nor the names of other
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+Files bundled as node modules and located in vendor directories are
+externally maintained libraries used by this software which have their own
+licenses; we recommend you read them, as their terms may differ from the
+terms above.
+
+================================================================================
+
+Package: class.min.js
+Version: 1.0
+Author: Angelo Dini
+License: BSD License
+Copyright: Distributed under the BSD License.
+
+${'='.repeat(80)}
+
+Package: jQuery
+Version: 1.11.3
+License: MIT License
+Copyright: (c) 2005, 2015 jQuery Foundation, Inc.
+License URL: jquery.org/license
+
+${'='.repeat(80)}
+
+Package: jsTree
+Version: 3.3.0 (2016-04-25)
+License: MIT License
+License URL: https://github.com/vakata/jstree/blob/master/LICENSE-MIT
+Copyright: (c) jsTree project
+Note: THIS FILE HAS BEEN MANUALLY PATCHED TO ALLOW COPYING INTO SELF/CHILDREN
+Source: https://github.com/FinalAngel/jstree
+
+${'='.repeat(80)}
+
+`;
+
+
 module.exports = function(opts) {
     'use strict';
 
@@ -76,72 +142,11 @@ module.exports = function(opts) {
                 perChunkOutput: false,
                 addBanner: false,
                 renderLicenses: (modules) => {
-                    const copyright = `Copyright (c) 2008-present, Batiste Bieler
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are
-met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials provided
-      with the distribution.
-    * Neither the name of the author nor the names of other
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-This software bundles Javascript open source software using WebPack.
-Please find their copyright notices here:
-
-================================================================================
-
-Package: class.min.js
-Version: 1.0
-Author: Angelo Dini
-License: BSD License
-Copyright: Distributed under the BSD License.
-
-${'='.repeat(80)}
-
-Package: jQuery
-Version: 1.11.3
-License: MIT License
-Copyright: (c) 2005, 2015 jQuery Foundation, Inc.
-License URL: jquery.org/license
-
-${'='.repeat(80)}
-
-Package: jsTree
-Version: 3.3.0 (2016-04-25)
-License: MIT License
-License URL: https://github.com/vakata/jstree/blob/master/LICENSE-MIT
-Copyright: (c) jsTree project
-Note: THIS FILE HAS BEEN MANUALLY PATCHED TO ALLOW COPYING INTO SELF/CHILDREN
-Source: https://github.com/FinalAngel/jstree
-
-${'='.repeat(80)}
-
-`;
                     const licenses = modules.map(module => {
                         const licenseText = module.licenseText || 'License text not found';
                         return `Package: ${module.packageJson.name || module.name}\nVersion: ${module.packageJson.version || 'N/A'}\nLicense: ${module.packageJson.license || 'N/A'}\n\n${licenseText}\n\n${'='.repeat(80)}\n`;
                     }).join('\n');
-                    return copyright + licenses;
+                    return cmsCopyright + licenses;
                 }
             })
         ],
