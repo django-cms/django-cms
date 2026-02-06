9a97a41b7bbaf02b4899ed02bc1d58b600002a68
Fabian Braun <fsbraun@gmx.de>
2026-02-06 23:24:57 +0100

    simplify

--- menus/utils.py
+++ menus/utils.py
@@ -270,25 +270,25 @@ class DefaultLanguageChanger:
             self.request.toolbar.obj
         ):
             # Toolbar object
+            obj = self.request.toolbar.obj
             if self.request.toolbar.edit_mode_active:
-                lang_obj = get_object_for_language(self.request.toolbar.obj, lang, latest=True)
+                lang_obj = get_object_for_language(obj, lang, latest=True)
                 return '' if lang_obj is None else get_object_edit_url(lang_obj, language=lang)
             if self.request.toolbar.preview_mode_active:
-                lang_obj = get_object_for_language(self.request.toolbar.obj, lang, latest=True)
+                lang_obj = get_object_for_language(obj, lang, latest=True)
                 return '' if lang_obj is None else get_object_preview_url(lang_obj, language=lang)
-            if hasattr(self.request.toolbar.obj, 'get_absolute_url'):
+            if hasattr(obj, 'get_absolute_url'):
                 try:
                     # First see, if object can get language-specific absolute urls (like PageContent)
-                    return self.request.toolbar.obj.get_absolute_url(language=lang)
+                    return obj.get_absolute_url(language=lang)
                 except (TypeError, NoReverseMatch):
                     # Object's get_absolute_url does not accept language parameter, set the language
                     with force_language(lang):
                         try:
-                            url = self.request.toolbar.obj.get_absolute_url()
+                            return obj.get_absolute_url()
                         except NoReverseMatch:
-                            url = None
-                    if url:
-                        return url
+                            # Fall back to view
+                            pass
         if view and view.url_name not in ('pages-details-by-slug', 'pages-root'):
             view_name = view.url_name
             if view.namespace:
