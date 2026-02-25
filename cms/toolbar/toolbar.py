4813a6aaf8af77c90313ab89b3a14e9e89d4bfb8
Petr Klus <petr.klus@framewurk.io>
2026-02-25 17:51:47 +0100

    fix: Avoid mutable default args

--- cms/toolbar/toolbar.py
+++ cms/toolbar/toolbar.py
@@ -415,8 +415,9 @@ class CMSToolbarBase(BaseToolbar):
             return self.obj.pk
         return ''
 
-    @property
-    def request_extra_params(self, remove_params=['language']):
+    def get_request_extra_params(self, remove_params=None):
+        if remove_params is None:
+            remove_params = ('language',)
         params = self.request.GET.copy()  # makes it mutable
         for param in remove_params:
             params.pop(param, None)
@@ -424,17 +425,17 @@ class CMSToolbarBase(BaseToolbar):
 
     def get_object_preview_url(self):
         if self.obj:
-            return get_object_preview_url(self.obj, language=self.request_language, params=self.request_extra_params)
+            return get_object_preview_url(self.obj, language=self.request_language, params=self.get_request_extra_params())
         return ''
 
     def get_object_edit_url(self):
         if self.obj:
-            return get_object_edit_url(self.obj, language=self.request_language, params=self.request_extra_params)
+            return get_object_edit_url(self.obj, language=self.request_language, params=self.get_request_extra_params())
         return ''
 
     def get_object_structure_url(self):
         if self.obj:
-            return get_object_structure_url(self.obj, language=self.request_language, params=self.request_extra_params)
+            return get_object_structure_url(self.obj, language=self.request_language, params=self.get_request_extra_params())
         return ''
 
     def object_is_editable(self, obj=None):
