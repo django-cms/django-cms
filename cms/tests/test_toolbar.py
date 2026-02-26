1d851a96e23d8ee5b7b25bc8962bfaa3b35d38b2
Django CMS Release <info@django-cms.org>
2026-02-26 16:46:14 +0100

    fix: add missing import

--- cms/tests/test_toolbar.py
+++ cms/tests/test_toolbar.py
@@ -8,6 +8,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.models import AnonymousUser, Permission
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import Site
 from django.http import HttpResponse, QueryDict
 from django.template.defaultfilters import truncatewords
 from django.test import TestCase
