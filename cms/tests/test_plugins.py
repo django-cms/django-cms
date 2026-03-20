4912128f0bb25ffb1c0ea55600e7aded645c9198
Fabian Braun <fsbraun@gmx.de>
2026-03-20 16:00:01 +0100

    fix: Chick size was missing for plugin queryset.iterator() (#8532) (#8533)
    * fix: Chick size was missing for plugin queryset.iterator() (#8532)
    
    Co-authored-by: Django CMS Release <info@django-cms.org>
    
    * fix: Add import for mock
    
    ---------
    
    Co-authored-by: Django CMS Release <info@django-cms.org>

--- cms/tests/test_plugins.py
+++ cms/tests/test_plugins.py
@@ -2,7 +2,7 @@ import datetime
 import pickle
 import warnings
 from contextlib import contextmanager
-from unittest import skipIf
+from unittest import mock, skipIf
 
 from django import http
 from django.conf import settings
@@ -1039,6 +1039,37 @@ class PluginsTestCase(PluginsTestBaseCase):
             ancestors = plugins[-1].get_ancestors()
 
         self.assertEqual([ancestor.pk for ancestor in ancestors], [plugin.pk for plugin in plugins[:-1]])
+
+    def test_iterator_chunk_size_in_get_bound_plugins(self):
+        """iterator() must pass PLUGIN_ITERATOR_CHUNK_SIZE so that
+        querysets with prefetch_related (from custom managers) don't
+        crash on Django 5.0+."""
+        from cms.constants import PLUGIN_ITERATOR_CHUNK_SIZE
+        from cms.utils.plugins import get_bound_plugins
+
+        placeholder = self.get_placeholder()
+        with register_plugins(DumbFixturePlugin):
+            plugin = api.add_plugin(placeholder, DumbFixturePlugin, "en")
+            plugins = CMSPlugin.objects.filter(pk=plugin.pk)
+
+            with mock.patch(
+                "cms.utils.plugins.PLUGIN_ITERATOR_CHUNK_SIZE", 999
+            ):
+                # Patch the queryset's iterator to inspect the chunk_size kwarg
+                original_iterator = None
+
+                def patched_iterator(chunk_size=None):
+                    self.assertEqual(chunk_size, 999)
+                    return original_iterator(chunk_size=chunk_size)
+
+                qs = DumbFixturePlugin.model.objects.filter(pk__in=[plugin.pk])
+                original_iterator = qs.iterator
+
+                with mock.patch.object(
+                    DumbFixturePlugin.model.objects, "filter", return_value=qs
+                ):
+                    with mock.patch.object(qs, "iterator", side_effect=patched_iterator):
+                        list(get_bound_plugins(plugins))
 
 
 class PluginManyToManyTestCase(PluginsTestBaseCase):
