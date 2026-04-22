6e9d5947c83074ede7366204595a77b4bdadc25b
Fabian Braun <fsbraun@gmx.de>
2026-04-22 15:07:36 +0200

    Update cms/admin/utils.py
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- cms/admin/utils.py
+++ cms/admin/utils.py
@@ -387,9 +387,9 @@ class GrouperModelAdmin(ChangeListActionsMixin, ModelAdmin):
     def can_change_content(self, request, content_obj):
         opts = self.content_model._meta
         perm = f"{opts.app_label}.{get_permission_codename('change' if content_obj else 'add', opts)}"
-        has_permissions = request.user.has_perm(perm, content_obj)
-        is_editable = getattr(content_obj, "is_editable", lambda *_: True)(request)
-        return has_permissions and is_editable
+        if not request.user.has_perm(perm, content_obj):
+            return False
+        return getattr(content_obj, "is_editable", lambda *_: True)(request)
 
     def get_queryset(self, request: HttpRequest) -> models.QuerySet:
         """Annotates content fields with the name "content__{field_name}" to the grouper queryset if
