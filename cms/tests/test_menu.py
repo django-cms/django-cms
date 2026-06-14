d067a700ee9ed6c7e7bcc9140e4b9d367581dae0
Fabian Braun <fsbraun@gmx.de>
2026-06-14 11:57:54 +0200

    Apply suggestion from @fsbraun

--- cms/tests/test_menu.py
+++ cms/tests/test_menu.py
@@ -503,7 +503,6 @@ class FixturesMenuTests(MenusFixture, BaseMenuTest):
 
         prev_cache = menu_pool.cache
         menu_pool.cache = get_menu_cache()
-        
         cms_page = self.get_page(1)
         context = self.get_context(path=cms_page.get_absolute_url(), page=cms_page)
         request = context["request"]
