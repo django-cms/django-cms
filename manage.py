7dcc65c4e6618de24e53bc4b68cc6486a83a579b
Fabian Braun <fsbraun@gmx.de>
2026-06-22 16:00:59 +0200

    chore: Remove CMS_CONFIRM_VERSION4 setting for CMS 5.1 (#8686)
    * chore: Remove CMS_CONFIRM_VERSION4 setting for CMS 5.1
    
    * fix linting
    
    * Update docs/reference/configuration.rst
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    * fix typos in docs
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- manage.py
+++ manage.py
@@ -380,7 +380,6 @@ if __name__ == "__main__":
             TEST_RUNNER="django.test.runner.DiscoverRunner",
             MIGRATION_MODULES=MIGRATION_MODULES,
             X_FRAME_OPTIONS="SAMEORIGIN",
-            CMS_CONFIRM_VERSION4=True,
             TEXT_INLINE_EDITING=False,  # Do not pollute toolbar for tests
             **dynamic_configs,
         )
